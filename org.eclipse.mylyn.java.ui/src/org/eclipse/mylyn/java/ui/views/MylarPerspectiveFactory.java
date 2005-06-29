03e2e31bfd8c9a037551a7a1e5d77189bbd365ad
sminto <>
2005-06-29 18:35:06 +0000

    Created Mylar Java perspective

--- org.eclipse.mylyn.java.ui/src/org/eclipse/mylyn/java/ui/views/MylarPerspectiveFactory.java
+++ org.eclipse.mylyn.java.ui/src/org/eclipse/mylyn/java/ui/views/MylarPerspectiveFactory.java
@@ -11,26 +11,15 @@
 
 package org.eclipse.mylar.java.ui.views;
 
-import org.eclipse.jdt.ui.JavaUI;
-import org.eclipse.search.ui.NewSearchUI;
+import org.eclipse.jdt.internal.ui.JavaPerspectiveFactory;
 import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
-import org.eclipse.ui.IPerspectiveFactory;
-import org.eclipse.ui.console.IConsoleConstants;
 
 
-/**
- * Based on @see org.eclipse.jdt.internal.ui.JavaPerspectiveFactory
- */
-public class MylarPerspectiveFactory implements IPerspectiveFactory {
+public class MylarPerspectiveFactory extends JavaPerspectiveFactory {
 
-	public static String ID_PROBLEM_VIEW = "org.eclipse.mylar.java.ui.views.MylarProblemView"; 
     public static String ID_RELATED_ELEMENTS_VIEW = "org.eclipse.mylar.ui.views.RelatedElementsView"; 
-	public static String ID_PACKAGE_EXPLORER_VIEW = JavaUI.ID_PACKAGES; 
-    public static String ID_TASKSCAPE_MODEL_VIEW = "org.eclipse.mylar.java.ui.views.TaskscapeTreeView"; 
-    public static String ID_TASK_LIST_VIEW = "org.eclipse.mylar.tasklist.views.TaskListView"; 
-//	public static String ID_USAGE_STATISTICS_VIEW = "org.eclipse.mylar.ui.views.UsageStatisticsView";  
-//	public static final String ID_PROGRESS_VIEW = "org.eclipse.ui.views.ProgressView"; // see bug 63563  //$NON-NLS-1$
+    public static String ID_TASK_LIST_VIEW = "org.eclipse.mylar.tasks.ui.views.TaskListView"; 
 	  
 	/**
 	 * Constructs a new Default layout engine.
@@ -40,57 +29,9 @@ public class MylarPerspectiveFactory implements IPerspectiveFactory {
 	}
 
 	public void createInitialLayout(IPageLayout layout) {
- 		String editorArea = layout.getEditorArea();
-		
-		IFolderLayout folder= layout.createFolder("left", IPageLayout.LEFT, (float)0.25, editorArea); //$NON-NLS-1$
-		folder.addView(ID_PACKAGE_EXPLORER_VIEW);
-		folder.addView(JavaUI.ID_TYPE_HIERARCHY);
-		folder.addPlaceholder(IPageLayout.ID_RES_NAV);
-		
-		IFolderLayout outputfolder= layout.createFolder("bottom", IPageLayout.BOTTOM, (float)0.75, editorArea); //$NON-NLS-1$
-		outputfolder.addView(ID_PROBLEM_VIEW);
-        outputfolder.addView(ID_RELATED_ELEMENTS_VIEW);
-//		outputfolder.addView(IMylarLayout.ID_MONITOR_VIEW);
-//		outputfolder.addView(JavaUI.ID_SOURCE_VIEW);
-		outputfolder.addPlaceholder(NewSearchUI.SEARCH_VIEW_ID);
-		outputfolder.addPlaceholder(IConsoleConstants.ID_CONSOLE_VIEW);
-		outputfolder.addPlaceholder(IPageLayout.ID_BOOKMARKS);
-
-		
-		IFolderLayout rightFolder = layout.createFolder("right", IPageLayout.RIGHT, (float)0.75, editorArea); //$NON-NLS-1$
-		rightFolder.addView(ID_TASK_LIST_VIEW); 
-        rightFolder.addView(IPageLayout.ID_OUTLINE);
-		rightFolder.addView(ID_TASKSCAPE_MODEL_VIEW); 
-		
-//		layout.addActionSet(IDebugUIConstants.LAUNCH_ACTION_SET);
-		layout.addActionSet(JavaUI.ID_ACTION_SET);
-		layout.addActionSet(JavaUI.ID_ELEMENT_CREATION_ACTION_SET);
-		layout.addActionSet(IPageLayout.ID_NAVIGATE_ACTION_SET);
-		
-		// views - java
-		layout.addShowViewShortcut(JavaUI.ID_PACKAGES);
-		layout.addShowViewShortcut(JavaUI.ID_TYPE_HIERARCHY);
-		layout.addShowViewShortcut(JavaUI.ID_SOURCE_VIEW);
-		layout.addShowViewShortcut(JavaUI.ID_JAVADOC_VIEW);
-
-		// views - search
-		layout.addShowViewShortcut(NewSearchUI.SEARCH_VIEW_ID);
-		
-		// views - debugging
-		layout.addShowViewShortcut(IConsoleConstants.ID_CONSOLE_VIEW);
-
-		// views - standard workbench
-		layout.addShowViewShortcut(IPageLayout.ID_OUTLINE);
-		layout.addShowViewShortcut(IPageLayout.ID_PROBLEM_VIEW);
-		layout.addShowViewShortcut(IPageLayout.ID_RES_NAV);
-				
-		// new actions - Java project creation wizard
-		layout.addNewWizardShortcut("org.eclipse.jdt.ui.wizards.NewPackageCreationWizard"); //$NON-NLS-1$
-		layout.addNewWizardShortcut("org.eclipse.jdt.ui.wizards.NewClassCreationWizard"); //$NON-NLS-1$
-		layout.addNewWizardShortcut("org.eclipse.jdt.ui.wizards.NewInterfaceCreationWizard"); //$NON-NLS-1$
-		layout.addNewWizardShortcut("org.eclipse.jdt.ui.wizards.NewSourceFolderCreationWizard");	 //$NON-NLS-1$
-		layout.addNewWizardShortcut("org.eclipse.jdt.ui.wizards.NewSnippetFileCreationWizard"); //$NON-NLS-1$
-		layout.addNewWizardShortcut("org.eclipse.ui.wizards.new.folder");//$NON-NLS-1$
-		layout.addNewWizardShortcut("org.eclipse.ui.wizards.new.file");//$NON-NLS-1$
+		super.createInitialLayout(layout);
+	
+		IFolderLayout rightFolder = layout.createFolder("right", IPageLayout.TOP, (float)0.4, IPageLayout.ID_OUTLINE);
+		rightFolder.addView(ID_TASK_LIST_VIEW);
 	}
 }
