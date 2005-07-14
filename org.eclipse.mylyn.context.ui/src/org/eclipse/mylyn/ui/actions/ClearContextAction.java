b7c124088562d58deb917fdcf480653c9bc80f1a
mkersten <>
2005-07-14 20:55:56 +0000

    Made warning pop up

--- org.eclipse.mylyn.context.ui/src/org/eclipse/mylyn/ui/actions/ClearContextAction.java
+++ org.eclipse.mylyn.context.ui/src/org/eclipse/mylyn/ui/actions/ClearContextAction.java
@@ -42,13 +42,13 @@ public class ClearContextAction extends Action {
 	public void run() {
 	    Object selectedObject = ((IStructuredSelection)this.view.getViewer().getSelection()).getFirstElement();
 	    if (selectedObject != null && selectedObject instanceof ITask) {
-//	    	ITask task = (ITask)selectedObject;
-//    		if (task.isActive() || task.getOrCreateCorrespondingTask().isActive()) {
-//	    		MessageDialog.openError(Workbench.getInstance()
-//						.getActiveWorkbenchWindow().getShell(), "Clear context failed",
-//						"Task must be deactivated before clearing task context.");
-//				return;
-//	    	}
+	    	ITask task = (ITask)selectedObject;
+    		if (task.isActive() || task.getOrCreateCorrespondingTask().isActive()) {
+	    		MessageDialog.openError(Workbench.getInstance()
+						.getActiveWorkbenchWindow().getShell(), "Clear context failed",
+						"Task must be deactivated before clearing task context.");
+				return;
+	    	}
 	    	boolean deleteConfirmed = MessageDialog.openQuestion(
 		            Workbench.getInstance().getActiveWorkbenchWindow().getShell(),
 		            "Confirm clear context", 
