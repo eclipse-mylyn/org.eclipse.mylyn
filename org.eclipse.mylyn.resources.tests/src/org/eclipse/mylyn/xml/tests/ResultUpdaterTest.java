3b18afc7d32f66e363b0192c33d33b26c1306dad
sminto <>
2005-08-10 18:13:32 +0000

    added tests for search result invalidation

---
+++ org.eclipse.mylyn.resources.tests/src/org/eclipse/mylyn/xml/tests/ResultUpdaterTest.java
@@ -0,0 +1,125 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.mylar.xml.tests;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.mylar.core.IMylarContextNode;
+import org.eclipse.mylar.core.MylarPlugin;
+import org.eclipse.mylar.core.internal.CompositeContext;
+import org.eclipse.mylar.core.internal.MylarContext;
+import org.eclipse.mylar.core.internal.MylarContextEdge;
+import org.eclipse.mylar.core.search.IMylarSearchOperation;
+import org.eclipse.mylar.core.tests.support.ResourceHelper;
+import org.eclipse.mylar.core.tests.support.WorkspaceSetupHelper;
+import org.eclipse.mylar.core.tests.support.search.ISearchPluginTest;
+import org.eclipse.mylar.core.tests.support.search.SearchPluginTestHelper;
+import org.eclipse.mylar.core.tests.support.search.SearchTaskscapeNotifier;
+import org.eclipse.mylar.java.JavaStructureBridge;
+import org.eclipse.mylar.xml.XmlReferencesProvider;
+
+public class ResultUpdaterTest extends TestCase implements ISearchPluginTest{
+	private IType type1;
+	private IFile plugin1;
+	private IJavaProject jp1;
+	private static final String SOURCE_ID = "XMLSearchResultUpdaterTest";
+	private SearchPluginTestHelper helper;
+
+	@Override
+    protected void setUp() throws Exception {
+    	//TODO: clear the relationship providers?
+    	WorkspaceSetupHelper.setupWorkspace();
+    	jp1 = WorkspaceSetupHelper.getProject1();
+    	type1 = WorkspaceSetupHelper.getType(jp1, "org.eclipse.mylar.tests.project1.views.SampleView");
+    	plugin1 = WorkspaceSetupHelper.getFile(jp1, "plugin.xml");
+    	
+    	MylarContext t = WorkspaceSetupHelper.getTaskscape();
+    	MylarPlugin.getContextManager().taskActivated(t.getId(), t.getId());
+    	helper = new SearchPluginTestHelper(this);
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+    	WorkspaceSetupHelper.clearWorkspace();
+        WorkspaceSetupHelper.clearDoiModel();
+    }
+	
+	public void testRemoveFile() throws Exception {
+		
+		int dos = 4;
+		
+        CompositeContext t = (CompositeContext)MylarPlugin.getContextManager().getActiveContext();
+		SearchTaskscapeNotifier notifier = new SearchTaskscapeNotifier(t, SOURCE_ID);
+		IMylarContextNode searchNode = notifier.getElement(type1.getHandleIdentifier(), JavaStructureBridge.EXTENSION);
+		
+		//
+		// we should get all results since we are searching the entire workspace
+		searchNode = notifier.getElement(type1.getHandleIdentifier(), JavaStructureBridge.EXTENSION);
+		helper.searchResultsNotNull(notifier, searchNode, dos, 3);
+		//
+		//
+		
+		Collection<MylarContextEdge> edges = searchNode.getEdges();
+		assertEquals(3, edges.size());		
+		
+		ResourceHelper.delete(plugin1);
+		
+		Collection<MylarContextEdge> edgesAfterRemove = searchNode.getEdges();
+		assertEquals(0, edgesAfterRemove.size());	
+	}
+	
+	public void testRemoveProject() throws Exception {
+		int dos = 4;
+		
+        CompositeContext t = (CompositeContext)MylarPlugin.getContextManager().getActiveContext();
+		SearchTaskscapeNotifier notifier = new SearchTaskscapeNotifier(t, SOURCE_ID);
+		IMylarContextNode searchNode = notifier.getElement(type1.getHandleIdentifier(), JavaStructureBridge.EXTENSION);
+		
+		//
+		// we should get all results since we are searching the entire workspace
+		searchNode = notifier.getElement(type1.getHandleIdentifier(), JavaStructureBridge.EXTENSION);
+		helper.searchResultsNotNull(notifier, searchNode, dos, 3);
+		//
+		//
+
+		Collection<MylarContextEdge> edges = searchNode.getEdges();
+		assertEquals(3, edges.size());	
+		
+		ResourceHelper.deleteProject(jp1.getProject().getName());
+		
+		Collection<MylarContextEdge> edgesAfterRemove = searchNode.getEdges();
+		assertEquals(0, edgesAfterRemove.size());	;
+	}
+	
+	public List<?> search(int dos, IMylarContextNode node) throws IOException, CoreException{
+		if(node == null)
+			return null;
+		
+		// test with each of the sepatations	
+		XmlReferencesProvider prov = new XmlReferencesProvider();
+
+		IMylarSearchOperation o = prov.getSearchOperation(node, 0, dos);
+		if(o == null) return null;
+		
+		XMLResultUpdaterSearchListener l = new XMLResultUpdaterSearchListener(prov, node, dos);
+		SearchPluginTestHelper.search(o, l);
+
+		return l.getResults();
+	}
+}
---
+++ org.eclipse.mylyn.resources.tests/src/org/eclipse/mylyn/xml/tests/XMLResultUpdaterSearchListener.java
@@ -0,0 +1,118 @@
+/*******************************************************************************
+ * Copyright (c) 2004 - 2005 University Of British Columbia and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     University Of British Columbia - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.mylar.xml.tests;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.internal.resources.File;
+import org.eclipse.mylar.core.AbstractRelationshipProvider;
+import org.eclipse.mylar.core.IMylarContextNode;
+import org.eclipse.mylar.core.IMylarStructureBridge;
+import org.eclipse.mylar.core.InteractionEvent;
+import org.eclipse.mylar.core.MylarPlugin;
+import org.eclipse.mylar.core.tests.support.search.TestActiveSearchListener;
+import org.eclipse.mylar.xml.XmlNodeHelper;
+import org.eclipse.mylar.xml.XmlReferencesProvider;
+import org.eclipse.mylar.xml.pde.PdeStructureBridge;
+import org.eclipse.search.internal.ui.text.FileSearchResult;
+import org.eclipse.search.ui.text.Match;
+import org.eclipse.ui.part.FileEditorInput;
+
+public class XMLResultUpdaterSearchListener extends TestActiveSearchListener {
+
+	private List<IMylarContextNode> results = null;
+
+	private IMylarContextNode node;
+	
+	private int degreeOfSeparation;
+
+	public XMLResultUpdaterSearchListener(AbstractRelationshipProvider prov,
+			IMylarContextNode searchNode, int degreeOfSeparation) {
+		super(prov);
+		this.node = searchNode;
+		this.degreeOfSeparation = degreeOfSeparation;
+	}
+
+	private boolean gathered = false;
+
+	@Override
+	public void searchCompleted(List<?> l) {
+		results = new ArrayList<IMylarContextNode>();
+		
+		if (l.isEmpty())
+			return;
+
+		Map<String, String> nodes = new HashMap<String, String>();
+
+		if (l.get(0) instanceof FileSearchResult) {
+			FileSearchResult fsr = (FileSearchResult) l.get(0);
+
+			Object[] far = fsr.getElements();
+			for (int i = 0; i < far.length; i++) {
+				Match[] mar = fsr.getMatches(far[i]);
+
+				if (far[i] instanceof File) {
+					File f = (File) far[i];
+
+					// change the file into a document
+					FileEditorInput fei = new FileEditorInput(f);
+
+					for (int j = 0; j < mar.length; j++) {
+						Match m = mar[j];
+						try {
+							XmlNodeHelper xnode = new XmlNodeHelper(fei, m
+									.getOffset());
+							IMylarStructureBridge bridge = MylarPlugin
+									.getDefault().getStructureBridge(
+											f.getName());
+							String handle = xnode.getHandle();
+							Object o = bridge.getObjectForHandle(handle);
+							String name = bridge.getName(o);
+							if (o != null) {
+								nodes.put(handle, name);
+								results.add(node);
+							}
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		}
+
+		for (String handle : nodes.keySet()) {
+			incrementInterest(node, PdeStructureBridge.EXTENSION, handle,
+					degreeOfSeparation);
+		}
+		gathered = true;
+	}
+
+	protected void incrementInterest(IMylarContextNode node, String elementKind, String elementHandle, int degreeOfSeparation) {
+        int predictedInterest = 1;//(7-degreeOfSeparation) * TaskscapeManager.getScalingFactors().getDegreeOfSeparationScale();
+        InteractionEvent event = new InteractionEvent(InteractionEvent.Kind.PREDICTION, elementKind, elementHandle, XmlReferencesProvider.SOURCE_ID, XmlReferencesProvider.SOURCE_ID, null, predictedInterest);
+        MylarPlugin.getContextManager().handleInteractionEvent(event);
+
+    }
+	
+	@Override
+	public boolean resultsGathered() {
+		return gathered;
+	}
+
+	@Override
+	public List<?> getResults() {
+		return results;
+	}
+
+}
