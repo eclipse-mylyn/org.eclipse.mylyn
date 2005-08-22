0ebb5d4a49b553914e24fa998707a517d6cc4228
mkersten <>
2005-08-22 21:09:47 +0000

    Mylar IDE component

---
+++ org.eclipse.mylyn.ide.ui/src/org/eclipse/mylyn/ide/MylarIdePlugin.java
@@ -0,0 +1,54 @@
+package org.eclipse.mylar.ide;
+
+import org.eclipse.ui.plugin.*;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The main plugin class to be used in the desktop.
+ */
+public class MylarIdePlugin extends AbstractUIPlugin {
+
+	//The shared instance.
+	private static MylarIdePlugin plugin;
+	
+	/**
+	 * The constructor.
+	 */
+	public MylarIdePlugin() {
+		plugin = this;
+	}
+
+	/**
+	 * This method is called upon plug-in activation
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+	}
+
+	/**
+	 * This method is called when the plug-in is stopped
+	 */
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+		plugin = null;
+	}
+
+	/**
+	 * Returns the shared instance.
+	 */
+	public static MylarIdePlugin getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Returns an image descriptor for the image file at the given
+	 * plug-in relative path.
+	 *
+	 * @param path the path
+	 * @return the image descriptor
+	 */
+	public static ImageDescriptor getImageDescriptor(String path) {
+		return AbstractUIPlugin.imageDescriptorFromPlugin("org.eclipse.mylar.ide", path);
+	}
+}
