caf3731002141312196b4f767d673ebf474dc1c0
sminto <>
2005-08-23 21:09:08 +0000

    removed sysouts

--- org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/tasklist/TaskActiveTimerListener.java
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/tasklist/TaskActiveTimerListener.java
@@ -1,7 +1,5 @@
 package org.eclipse.mylar.tasklist;
 
-import java.util.Date;
-
 import org.eclipse.mylar.core.InteractionEvent;
 import org.eclipse.mylar.monitor.IInteractionEventListener;
 import org.eclipse.mylar.monitor.MylarMonitorPlugin;
@@ -26,13 +24,10 @@ public class TaskActiveTimerListener implements IActiveTimerListener, IInteracti
 	public void fireTimedOut() {
 		task.setActive(task.isActive(), true);
 		isTaskStalled = true;
-		System.out.println(task.getElapsedTimeForDisplay());
-		System.out.println(new Date());
 		timer.resetTimer();
 	}
 
 	public void interactionObserved(InteractionEvent event) {
-		System.out.println("interaction > " + task.getElapsedTimeForDisplay());
 		timer.resetTimer();		
 		
 		if(isTaskStalled){
@@ -46,7 +41,6 @@ public class TaskActiveTimerListener implements IActiveTimerListener, IInteracti
 	public void stopTimer() {
 		timer.killThread();
 		MylarMonitorPlugin.getDefault().removeListener(this);
-		System.out.println("listener removed");
 	}
 
 	public void stop() {}
