7bbe17ae2e7153f5b695f09535599b48eb87903b
mkersten <>
2005-09-10 01:43:04 +0000

    Test refactoring

--- org.eclipse.mylyn.tests/src/org/eclipse/mylyn/tests/AllTests.java
+++ org.eclipse.mylyn.tests/src/org/eclipse/mylyn/tests/AllTests.java
@@ -16,51 +16,20 @@ package org.eclipse.mylar.tests;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-import org.eclipse.mylar.core.tests.ContextTest;
-import org.eclipse.mylar.core.tests.DegreeOfInterestTest;
-import org.eclipse.mylar.java.tests.ContextManagerTest;
-import org.eclipse.mylar.java.tests.JavaStructureTest;
-import org.eclipse.mylar.java.tests.search.JUnitReferencesSearchPluginTest;
-import org.eclipse.mylar.java.tests.search.JavaImplementorsSearchPluginTest;
-import org.eclipse.mylar.java.tests.search.JavaReadAccessSearchPluginTest;
-import org.eclipse.mylar.java.tests.search.JavaReferencesSearchPluginTest;
-import org.eclipse.mylar.java.tests.search.JavaWriteAccessSearchPluginTest;
-import org.eclipse.mylar.tasklist.bugzilla.tests.BugzillaSearchPluginTest;
-import org.eclipse.mylar.tasklist.bugzilla.tests.BugzillaStackTraceTest;
-import org.eclipse.mylar.tasklist.tests.TaskListManagerTest;
-import org.eclipse.mylar.tasklist.tests.TaskListUiTest;
-import org.eclipse.mylar.xml.tests.XMLSearchPluginTest;
+import org.eclipse.mylar.core.tests.AllCoreTests;
+import org.eclipse.mylar.java.tests.AllJavaTests;
+import org.eclipse.mylar.tasklist.tests.AllTasklistTests;
+import org.eclipse.mylar.xml.tests.AllXmlTests;
 	
 public class AllTests {
 
     public static Test suite() {
         TestSuite suite = new TestSuite("Test for org.eclipse.mylar.tests");
         //$JUnit-BEGIN$
-
-        // Core Tests
-        suite.addTestSuite(ContextTest.class);
-        suite.addTestSuite(TaskListUiTest.class);
-        suite.addTestSuite(DegreeOfInterestTest.class);
-        
-        // Java Tests
-        suite.addTestSuite(ContextManagerTest.class);
-        suite.addTestSuite(JavaStructureTest.class);
-        suite.addTestSuite(JavaImplementorsSearchPluginTest.class);
-        suite.addTestSuite(JavaReadAccessSearchPluginTest.class);
-        suite.addTestSuite(JavaReferencesSearchPluginTest.class);
-        suite.addTestSuite(JavaWriteAccessSearchPluginTest.class);
-        suite.addTestSuite(JUnitReferencesSearchPluginTest.class);
-
-        // Bugzilla Tests 
-        suite.addTestSuite(BugzillaSearchPluginTest.class);
-        suite.addTestSuite(BugzillaStackTraceTest.class);
-        
-        // Xml Tests 
-        suite.addTestSuite(XMLSearchPluginTest.class);
-        
-        // Tasklist Tests
-        suite.addTestSuite(TaskListManagerTest.class);
-
+        suite.addTest(AllCoreTests.suite());
+        suite.addTest(AllJavaTests.suite());
+        suite.addTest(AllTasklistTests.suite());
+        suite.addTest(AllXmlTests.suite());
         //$JUnit-END$
         return suite;
     }
