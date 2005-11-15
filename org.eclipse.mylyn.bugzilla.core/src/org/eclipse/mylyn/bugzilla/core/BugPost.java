99da8545adf14834f8b107cbe059be4ece7c956d
mkersten <>
2005-11-15 22:17:47 +0000

    Progress on: Bug 116518: Submission to Bugzilla works but displays erroneous error Issue URL: https://bugs.eclipse.org/bugs/show_bug.cgi?id=116518

--- org.eclipse.mylyn.bugzilla.core/src/org/eclipse/mylyn/bugzilla/core/BugPost.java
+++ org.eclipse.mylyn.bugzilla.core/src/org/eclipse/mylyn/bugzilla/core/BugPost.java
@@ -10,7 +10,6 @@
  *******************************************************************************/
 package org.eclipse.mylar.bugzilla.core;
 
-
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -36,145 +35,151 @@ import org.eclipse.mylar.bugzilla.core.internal.HtmlStreamTokenizer;
 import org.eclipse.mylar.bugzilla.core.internal.HtmlTag;
 import org.eclipse.mylar.bugzilla.core.internal.HtmlStreamTokenizer.Token;
 
-
 /**
  * 
  * @author Shawn Minto
- *
+ * 
  * Class to handle the positing of a bug
  */
 public class BugPost {
-	
+
 	/** The fields that are to be changed/maintained */
 	Map<String, String> fields = new HashMap<String, String>();
-	
+
 	/** The url to post the bug to */
 	URL anURL;
-	
+
 	/** The prefix for how to find the bug number from the return */
 	String prefix;
-	
+
 	/** The postfix for how to find the bug number from the return */
 	String postfix1;
 
-	/** An alternate postfix for how to find the bug number from the return */	
+	/** An alternate postfix for how to find the bug number from the return */
 	String postfix2;
-	
+
 	String error = null;
-	
+
 	/**
 	 * Add a value to be posted to the bug
-	 * @param key The key for the value to be added
-	 * @param value The value to be added
+	 * 
+	 * @param key
+	 *            The key for the value to be added
+	 * @param value
+	 *            The value to be added
 	 */
 	public void add(String key, String value) {
 		try {
 			fields.put(key, URLEncoder.encode(value == null ? "" : value, "UTF-8"));
-		}
-		catch (UnsupportedEncodingException e) {
+		} catch (UnsupportedEncodingException e) {
 			/*
 			 * Do nothing. Every implementation of the Java platform is required
 			 * to support the standard charset "UTF-8"
 			 */
 		}
 	}
-	
+
 	/**
 	 * Set the url that the bug is supposed to be posted to
-	 * @param urlString The url to post the bug to
+	 * 
+	 * @param urlString
+	 *            The url to post the bug to
 	 */
 	public void setURL(String urlString) throws MalformedURLException {
-			anURL = new URL(urlString);
+		anURL = new URL(urlString);
 	}
-	
+
 	/**
 	 * Post the bug to the bugzilla server
+	 * 
 	 * @return The result of the responses
 	 * @throws BugzillaException
-	 * @throws PossibleBugzillaFailureException 
-	 */	
+	 * @throws PossibleBugzillaFailureException
+	 */
 	public String post() throws BugzillaException, LoginException, PossibleBugzillaFailureException {
 		return post(false);
 	}
-	
+
 	/**
 	 * Post the bug to the bugzilla server
- 	 * @param isDebug Whether we are debugging or not - if it is debug, we get the respose printed to std out
+	 * 
+	 * @param isDebug
+	 *            Whether we are debugging or not - if it is debug, we get the
+	 *            respose printed to std out
 	 * @throws BugzillaException
 	 * @throws LoginException
-	 * @throws PossibleBugzillaFailureException 
-	 */	
+	 * @throws PossibleBugzillaFailureException
+	 */
 	public String post(boolean isDebug) throws BugzillaException, LoginException, PossibleBugzillaFailureException {
 		BufferedOutputStream out = null;
 		BufferedReader in = null;
-		
+
 		try {
 			// connect to the bugzilla server
 			URLConnection cntx = BugzillaPlugin.getDefault().getUrlConnection(anURL);
-			if(cntx == null || !(cntx instanceof HttpURLConnection))
+			if (cntx == null || !(cntx instanceof HttpURLConnection))
 				return null;
 
 			HttpURLConnection postConnection = (HttpURLConnection) cntx;
-			
+
 			// set the connection method
 			postConnection.setRequestMethod("POST");
 			postConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 			// get the url for the update with all of the changed values
-			byte [] body = getPostBody().getBytes();
-			postConnection.setRequestProperty("Content-Length", String.valueOf(body.length));			
-							
+			byte[] body = getPostBody().getBytes();
+			postConnection.setRequestProperty("Content-Length", String.valueOf(body.length));
+
 			// allow outgoing streams and open a stream to post to
 			postConnection.setDoOutput(true);
-			
+
 			out = new BufferedOutputStream(postConnection.getOutputStream());
-			
+
 			// print out debug methods if we are debugging
 			if (isDebug) {
 				System.out.println("SENDING: ");
 				System.out.println("URL: " + anURL);
-				
+
 				System.out.println("Body: \n" + new String(body));
 			}
-			
+
 			// write the data and close the stream
 			out.write(body);
 			out.flush();
-		
+
 			int responseCode = postConnection.getResponseCode();
-			if (responseCode != HttpURLConnection.HTTP_OK &&
-				responseCode != HttpURLConnection.HTTP_CREATED) {
+			if (responseCode != HttpURLConnection.HTTP_OK && responseCode != HttpURLConnection.HTTP_CREATED) {
 				throw new BugzillaException("Server returned HTTP error: " + responseCode + " - " + postConnection.getResponseMessage());
-			}	
+			}
 
 			// open a stream to receive response from bugzilla
 			in = new BufferedReader(new InputStreamReader(postConnection.getInputStream()));
 			String result = null;
-			
+
 			if (isDebug)
 				System.out.println("RECEIVING:");
 			String aString = in.readLine();
-			
+
 			boolean possibleFailure = true;
 			error = "";
-			
+
 			while (aString != null) {
-//				System.err.println(">>>> " + aString);
-				error += aString==null?"":aString + "\n";
-					
-//				// check if we have run into an error
-				if(result == null && (aString.toLowerCase().indexOf("check e-mail") != -1 || aString.toLowerCase().indexOf("error") != -1)) {
-//					 error handling is now passed up
-//					throw new LoginException("Bugzilla login problem");
-				} else if( aString.toLowerCase().matches(".*bug\\s+processed.*")){
+				error += aString == null ? "" : aString + "\n";
+
+				// // check if we have run into an error
+				if (result == null && (aString.toLowerCase().indexOf("check e-mail") != -1 || aString.toLowerCase().indexOf("error") != -1)) {
+					// error handling is now passed up
+					// throw new LoginException("Bugzilla login problem");
+				} else if (aString.toLowerCase().matches(".*bug\\s+processed.*") // TODO: make this configurable
+						|| aString.toLowerCase().matches(".*defect\\s+processed.*")) {
 					possibleFailure = false;
-				}	
-//				// get the bug number if it is required
+				}
+				// // get the bug number if it is required
 				if (prefix != null && postfix1 != null && postfix2 != null && result == null) {
 					int startIndex = aString.toLowerCase().indexOf(prefix.toLowerCase());
 					if (startIndex > -1) {
 						startIndex = startIndex + prefix.length();
 						int stopIndex = aString.toLowerCase().indexOf(postfix1.toLowerCase(), startIndex);
-						if(stopIndex == -1)
+						if (stopIndex == -1)
 							stopIndex = aString.toLowerCase().indexOf(postfix2.toLowerCase(), startIndex);
 						if (stopIndex > -1) {
 							result = (aString.substring(startIndex, stopIndex)).trim();
@@ -182,52 +187,47 @@ public class BugPost {
 						}
 					}
 				}
-				aString = in.readLine();	
+				aString = in.readLine();
 			}
-			
-			if((result == null || result.compareTo("") == 0) && (prefix != null && postfix1 != null && postfix2 != null)){
+
+			if ((result == null || result.compareTo("") == 0) && (prefix != null && postfix1 != null && postfix2 != null)) {
 				throw new PossibleBugzillaFailureException("Could not find bug number for new bug.");
-			} else if(possibleFailure) {
+			} else if (possibleFailure) {
 				throw new PossibleBugzillaFailureException("Could not find \"Bug Processed\".");
 			}
-			
+
 			// set the error to null if we dont think that there was one
 			error = null;
-			
+
 			// return the bug number
 			return result;
 		} catch (IOException e) {
 			throw new BugzillaException("An exception occurred while submitting the bug: " + e.getMessage(), e);
-		} catch (KeyManagementException e)
-		{
+		} catch (KeyManagementException e) {
 			throw new BugzillaException("Could not POST form.  Communications error: " + e.getMessage(), e);
-		} catch (NoSuchAlgorithmException e)
-		{
+		} catch (NoSuchAlgorithmException e) {
 			throw new BugzillaException("Could not POST form.  Communications error: " + e.getMessage(), e);
-		}
-		finally
-		{
-			try{
-				if(in != null)
+		} finally {
+			try {
+				if (in != null)
 					in.close();
-				if(out != null)
-					out.close();
-										
-			}catch(IOException e)
-			{
-				BugzillaPlugin.log(new Status(IStatus.ERROR, IBugzillaConstants.PLUGIN_ID,IStatus.ERROR,"Problem posting the bug", e));
+				if (out != null)
+					out.close(); 
+
+			} catch (IOException e) {
+				BugzillaPlugin.log(new Status(IStatus.ERROR, IBugzillaConstants.PLUGIN_ID, IStatus.ERROR, "Problem posting the bug", e));
 			}
 		}
 	}
-	
-	
+
 	/**
 	 * Get the url that contains the attributes to be posted
+	 * 
 	 * @return The url for posting
 	 */
 	private String getPostBody() {
 		String postBody = "";
-		
+
 		// go through all of the attributes and add them to the body of the post
 		Iterator<Map.Entry<String, String>> anIterator = fields.entrySet().iterator();
 		while (anIterator.hasNext()) {
@@ -238,50 +238,59 @@ public class BugPost {
 		}
 		return postBody;
 	}
-	
+
 	/**
 	 * Gets the prefix
+	 * 
 	 * @return Returns a String
 	 */
 	public String getPrefix() {
 		return prefix;
 	}
-	
+
 	/**
 	 * Sets the prefix
-	 * @param prefix The prefix to set
+	 * 
+	 * @param prefix
+	 *            The prefix to set
 	 */
 	public void setPrefix(String prefix) {
 		this.prefix = prefix;
 	}
-	
+
 	/**
 	 * Gets the postfix
+	 * 
 	 * @return Returns a String
 	 */
 	public String getPostfix1() {
 		return postfix1;
 	}
-	
+
 	/**
 	 * Sets the postfix
-	 * @param postfix The postfix to set
+	 * 
+	 * @param postfix
+	 *            The postfix to set
 	 */
 	public void setPostfix1(String postfix) {
 		this.postfix1 = postfix;
 	}
-	
+
 	/**
 	 * Gets the alternate postfix
+	 * 
 	 * @return Returns a String
 	 */
 	public String getPostfix2() {
 		return postfix2;
 	}
-	
+
 	/**
 	 * Sets the alternate postfix
-	 * @param postfix The postfix to set
+	 * 
+	 * @param postfix
+	 *            The postfix to set
 	 */
 	public void setPostfix2(String postfix) {
 		this.postfix2 = postfix;
@@ -293,34 +302,32 @@ public class BugPost {
 
 	/**
 	 * remove all of the hyperlinks and erroneous info
+	 * 
 	 * @return
 	 */
 	private String parseError() {
 		String newError = "";
-		try{
+		try {
 			HtmlStreamTokenizer tokenizer = new HtmlStreamTokenizer(new StringReader(error), null);
 			for (Token token = tokenizer.nextToken(); token.getType() != Token.EOF; token = tokenizer.nextToken()) {
-				if(token.getType() == Token.TAG && ((HtmlTag)(token.getValue())).getTagType() == HtmlTag.Type.A){
-					
-				} else if(token.getType() == Token.TAG && ((HtmlTag)(token.getValue())).getTagType() == HtmlTag.Type.FORM ){
-					for (Token token2 = tokenizer.nextToken(); token2.getType() != Token.EOF; token2 = tokenizer.nextToken()) 
-					{
-						if(token2.getType() == Token.TAG)
-						{
-							HtmlTag tag = (HtmlTag)token2.getValue();
-							if(tag.getTagType() == HtmlTag.Type.FORM && tag.isEndTag())
+				if (token.getType() == Token.TAG && ((HtmlTag) (token.getValue())).getTagType() == HtmlTag.Type.A) {
+
+				} else if (token.getType() == Token.TAG && ((HtmlTag) (token.getValue())).getTagType() == HtmlTag.Type.FORM) {
+					for (Token token2 = tokenizer.nextToken(); token2.getType() != Token.EOF; token2 = tokenizer.nextToken()) {
+						if (token2.getType() == Token.TAG) {
+							HtmlTag tag = (HtmlTag) token2.getValue();
+							if (tag.getTagType() == HtmlTag.Type.FORM && tag.isEndTag())
 								break;
-					
+
 						}
 					}
 				} else {
 					newError += token.getWhitespace().toString() + token.getValue();
 				}
 			}
-		} catch(Exception e){
+		} catch (Exception e) {
 			newError = error;
 		}
 		return newError;
 	}
 }
-
