e04425b32d95d4c75bf0662d3f9dfd575aba52ea
mkersten <>
2005-12-07 04:50:59 +0000

    Completed: Bug 119556: fix task list drag and drop https://bugs.eclipse.org/bugs/show_bug.cgi?id=119556

--- org.eclipse.mylyn.tests/src/org/eclipse/mylyn/tests/SharedTaskFolderTest.java
+++ org.eclipse.mylyn.tests/src/org/eclipse/mylyn/tests/SharedTaskFolderTest.java
@@ -143,7 +143,7 @@ public class SharedTaskFolderTest extends TestCase{
 		
 		//Create the task and add it to the root of the task list
 		ITask newTask = new Task(MylarTaskListPlugin.getTaskListManager().genUniqueTaskHandle(), taskName, true);
-		manager.addRootTask(newTask);
+		manager.moveToRoot(newTask);
 		MylarContext mockContext = MylarPlugin.getContextManager().loadContext(newTask.getHandleIdentifier(), newTask.getContextPath());
 		InteractionEvent event = new InteractionEvent(InteractionEvent.Kind.EDIT,"structureKind","handle","originId");
 		mockContext.parseEvent(event);
