a680e810db818e3aa88d0bc9ae550b392da5e185
mkersten <>
2005-12-09 03:56:47 +0000

    Progress on: Bug 119557: Tasklist data doesn't change when changing workspace https://bugs.eclipse.org/bugs/show_bug.cgi?id=119557

--- org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/tasklist/internal/TaskListSaveManager.java
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/tasklist/internal/TaskListSaveManager.java
@@ -48,6 +48,20 @@ public class TaskListSaveManager implements ITaskActivityListener, DisposeListen
 		saveTimer.setSaveIntervalMillis(DEFAULT_SAVE_INTERVAL);
 	} 
 	
+	/** 
+	 * Called periodically by the save timer 
+	 */
+	public void saveRequested() {
+		if (MylarTaskListPlugin.getDefault().isShellActive() || forceBackgroundSave) {
+			try {
+				saveTaskListAndContexts();
+				ErrorLogger.log("Automatically saved task list", this);
+			} catch (Exception e) {
+				ErrorLogger.fail(e, "Could not auto save task list", false);
+			}
+		}
+	}
+	
 	public void saveTaskListAndContexts() {
 		if (MylarTaskListPlugin.getDefault() != null) {
 			MylarTaskListPlugin.getTaskListManager().saveTaskList();
@@ -113,20 +127,6 @@ public class TaskListSaveManager implements ITaskActivityListener, DisposeListen
 	/** For testing only **/
 	public BackgroundSaveTimer getSaveTimer() {
 		return saveTimer;
-	}
-	
-	/** 
-	 * Called periodically by the save timer 
-	 */
-	public void saveRequested() {
-		if (MylarTaskListPlugin.getDefault().isShellActive() || forceBackgroundSave) {
-			try {
-				saveTaskListAndContexts();
-				ErrorLogger.log("Automatically saved task list", this);
-			} catch (Exception e) {
-				ErrorLogger.fail(e, "Could not auto save task list", false);
-			}
-		}
 	}
 	
 	public void taskActivated(ITask task) {
