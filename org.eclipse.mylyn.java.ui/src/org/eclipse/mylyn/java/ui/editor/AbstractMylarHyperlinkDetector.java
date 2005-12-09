f739cacfb4e4daf7e079b49b077c8a030ae4b7cb
mkersten <>
2005-12-09 18:13:44 +0000

    Progress on: Bug 119614: support Eclipse 3.2M4 https://bugs.eclipse.org/bugs/show_bug.cgi?id=119614

--- org.eclipse.mylyn.java.ui/src/org/eclipse/mylyn/java/ui/editor/AbstractMylarHyperlinkDetector.java
+++ org.eclipse.mylyn.java.ui/src/org/eclipse/mylyn/java/ui/editor/AbstractMylarHyperlinkDetector.java
@@ -10,7 +10,6 @@
  *******************************************************************************/
 package org.eclipse.mylar.java.ui.editor;
 
-import org.eclipse.core.internal.commands.util.Assert;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.jface.text.hyperlink.IHyperlink;
@@ -23,10 +22,7 @@ import org.eclipse.ui.texteditor.ITextEditor;
 public abstract class AbstractMylarHyperlinkDetector implements IHyperlinkDetector {
 
 	private ITextEditor editor;
-	
-	public AbstractMylarHyperlinkDetector() {
-	}
-	
+		
 	public ITextEditor getEditor() {
 		return editor; 
 	}
@@ -35,7 +31,6 @@ public abstract class AbstractMylarHyperlinkDetector implements IHyperlinkDetect
 			IRegion region, boolean canShowMultipleHyperlinks);
 
 	public void setEditor(ITextEditor editor) {
-		Assert.isNotNull(editor);
 		this.editor = editor;
 	}
 }
