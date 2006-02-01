f7bcc26288a654e2be8774f08640515a92ba1ee1
mkersten <>
2006-02-01 01:11:22 +0000

    Progress on: Bug 125438: make tasklist icons overlay-based https://bugs.eclipse.org/bugs/show_bug.cgi?id=125438

--- org.eclipse.mylyn.tests/src/org/eclipse/mylyn/tests/misc/BugzillaStackTraceTest.java
+++ org.eclipse.mylyn.tests/src/org/eclipse/mylyn/tests/misc/BugzillaStackTraceTest.java
@@ -54,7 +54,7 @@ public class BugzillaStackTraceTest extends TestCase {
 	// * Test parsing the bug for multiple stacks in in
 	// */
 	// public void testMultipleStacksDiffComments(){
-	// // BUG 4862 - 2 stack traces - 1 in description, 1 in comment - text
+	// // REPORT_REPOSITORY 4862 - 2 stack traces - 1 in description, 1 in comment - text
 	// before and after
 	// performParse(4862, "4862.html", 2, false);
 	// }
@@ -64,7 +64,7 @@ public class BugzillaStackTraceTest extends TestCase {
 	// * text before it
 	// */
 	// public void testSingleStackCodeBeforeInDescription(){
-	// // BUG 76388 - 1 stack trace - description - text before and formatted
+	// // REPORT_REPOSITORY 76388 - 1 stack trace - description - text before and formatted
 	// ugly
 	// performParse(76388, "76388.html", 1, false);
 	// }
@@ -75,7 +75,7 @@ public class BugzillaStackTraceTest extends TestCase {
 	 */
 	public void testSingleStackCodeBeforeAndAfterInDescription() {
 
-		// BUG 76146 - 1 stack trace - description - text before and code after
+		// REPORT_REPOSITORY 76146 - 1 stack trace - description - text before and code after
 		performParse(76146, "76146.html", 1, false);
 	}
 
@@ -84,7 +84,7 @@ public class BugzillaStackTraceTest extends TestCase {
 	 * extra text, but has lines in it that span 3 lines
 	 */
 	public void testSingleStackPoorFormatInDescription() {
-		// BUG 67395 - 1 stack trace - description - no extra text, 1 at line
+		// REPORT_REPOSITORY 67395 - 1 stack trace - description - no extra text, 1 at line
 		// spans 3 lines
 		performParse(67395, "67395.html", 1, false);
 	}
@@ -93,7 +93,7 @@ public class BugzillaStackTraceTest extends TestCase {
 	 * Test parsing a bug with no stack traces and no qualified exception names
 	 */
 	public void testNoStackNoQualified() {
-		// BUG 4548 - no stack traces, no qualified reference to an exception
+		// REPORT_REPOSITORY 4548 - no stack traces, no qualified reference to an exception
 		performParse(4548, "4548.html", 0, false);
 	}
 
@@ -102,7 +102,7 @@ public class BugzillaStackTraceTest extends TestCase {
 	 * exception
 	 */
 	public void testNoStackQual() {
-		// BUG 1 - no stack traces, qualified reference to exception - made up
+		// REPORT_REPOSITORY 1 - no stack traces, qualified reference to exception - made up
 		// bug
 		performParse(1, "1.html", 0, false);
 	}
@@ -112,7 +112,7 @@ public class BugzillaStackTraceTest extends TestCase {
 	 * references
 	 */
 	public void testSingleStackQual() {
-		// BUG 2 - 1 stack trace- 2 qual ref, stack trace, 1 qual ref - made up
+		// REPORT_REPOSITORY 2 - 1 stack trace- 2 qual ref, stack trace, 1 qual ref - made up
 		// bug
 		performParse(2, "2.html", 1, false);
 	}
@@ -121,7 +121,7 @@ public class BugzillaStackTraceTest extends TestCase {
 	 * Test parsing of a bug with many stacks traces in a single comment
 	 */
 	public void testMultipleStackSingleComment() {
-		// BUG 40152 - 1 stack trace- 2 qual ref, stack trace, 1 qual ref - made
+		// REPORT_REPOSITORY 40152 - 1 stack trace- 2 qual ref, stack trace, 1 qual ref - made
 		// up bug
 		performParse(40152, "40152.html", 33, false);
 	}
@@ -178,7 +178,7 @@ public class BugzillaStackTraceTest extends TestCase {
 		BugzillaMylarSearchOperation.secondPassBugzillaParser(l);
 
 		// make sure that we received the right number of stack traces back
-		// System.out.println("*** BUG " + hit.getId() + " ***");
+		// System.out.println("*** REPORT_REPOSITORY " + hit.getId() + " ***");
 		// System.out.println("NumStackTraces = " +
 		// doi.getStackTraces().size());
 		assertEquals("Wrong Number stack traces", numTracesExpected, doi.getStackTraces().size());
