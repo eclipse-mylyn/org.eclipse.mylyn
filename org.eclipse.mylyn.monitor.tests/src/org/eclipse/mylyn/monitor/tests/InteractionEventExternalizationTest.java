182f056896ed699be454702152c378045c41cf17
mkersten <>
2006-02-09 05:36:44 +0000

    Fix for failing test and removal of Active Search from Java perspective default.

--- org.eclipse.mylyn.monitor.tests/src/org/eclipse/mylyn/monitor/tests/InteractionEventExternalizationTest.java
+++ org.eclipse.mylyn.monitor.tests/src/org/eclipse/mylyn/monitor/tests/InteractionEventExternalizationTest.java
@@ -33,8 +33,10 @@ public class InteractionEventExternalizationTest extends AbstractContextTest {
 		}
 		InteractionEventLogger logger = new InteractionEventLogger(f);
 		logger.startObserving();
+		String handle = "";
 		for (int i = 0; i < 100; i++) {
-			InteractionEvent event = mockSelection();
+			handle += "1";
+			InteractionEvent event = mockSelection(handle);
 			events.add(event);
 			logger.interactionObserved(event);
 		}
@@ -43,64 +45,7 @@ public class InteractionEventExternalizationTest extends AbstractContextTest {
 		File infile = new File(PATH);
 		List<InteractionEvent> readEvents = logger.getHistoryFromFile(infile);
 		for (int i = 0; i < events.size(); i++) {
-			processEvent(events.get(i), readEvents.get(i));
+			assertEquals(events.get(i), readEvents.get(i));
 		}
-		// f.delete();
-	}
-
-	private void processEvent(InteractionEvent event1, InteractionEvent event2) {
-		assertNotNull(event2);
-		assertTrue(event1.equals(event2));
-	}
-
+	} 
 }
-
-// public void testXStreamExternalization() {
-// try {
-// List<InteractionEvent> events = new ArrayList<InteractionEvent>();
-// File f = new File("./externMylarMem.xml");
-// FileOutputStream fos = new FileOutputStream(f);
-//
-// XStream xstream = new XStream(new DomDriver());
-// xstream.setMode(XStream.XPATH_REFERENCES);
-// xstream.alias("interactionEvent", InteractionEvent.class);
-// xstream.setClassLoader(InteractionEvent.class.getClassLoader());
-// for (int i = 0; i < 100; i++) {
-// InteractionEvent event = mockSelection();
-// events.add(event);
-//
-// // externalize by appending to a file
-// String xml = xstream.toXML(event) + "\r\n";
-// fos.write(xml.getBytes());
-// }
-// fos.close();
-//
-// File infile = new File("./externMylarMem.xml");
-// String tag = "</interactionEvent>";
-// String endl = "\r\n";
-// String buf = "";
-// int index;
-// int i = 0;
-// FileInputStream reader = new FileInputStream(infile);
-// byte[] buffer = new byte[1000];
-// while (reader.read(buffer) != -1) {
-// buf = buf + new String(buffer);
-// while ((index = buf.indexOf(tag)) != -1) {
-// index += tag.length();
-// String xml = buf.substring(0, index);
-// buf = buf.substring(index + endl.length(), buf.length());
-// InteractionEvent event = (InteractionEvent) xstream
-// .fromXML(xml);
-// processEvent(events, event, i);
-// event = readEvent(xml);
-// processEvent(events, event, i);
-// i++;
-// }
-// buffer = new byte[1000];
-// }
-// } catch (FileNotFoundException e) {
-// e.printStackTrace();
-// } catch (IOException e) {
-// e.printStackTrace();
-// }
-// }
