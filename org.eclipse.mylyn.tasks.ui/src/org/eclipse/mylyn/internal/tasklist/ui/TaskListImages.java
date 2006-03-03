3a2fe4955086fc2d0f73134561cfbbf64a882493
mkersten <>
2006-03-03 18:43:42 +0000

    Progress on: 129619: do UI revision and clean-up https://bugs.eclipse.org/bugs/show_bug.cgi?id=129619

--- org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasklist/ui/TaskListImages.java
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasklist/ui/TaskListImages.java
@@ -149,7 +149,7 @@ public class TaskListImages {
 	
 	public static final ImageDescriptor PRIORITY_5 = create(T_EVIEW, "priority-5.gif");
 	
-	public static final ImageDescriptor CALENDAR = create(T_EVIEW, "calendar.gif");
+	public static final ImageDescriptor CALENDAR = create(T_EVIEW, "task-activity.gif");
 		
 	private static ImageDescriptor create(String prefix, String name) {
 		try {
