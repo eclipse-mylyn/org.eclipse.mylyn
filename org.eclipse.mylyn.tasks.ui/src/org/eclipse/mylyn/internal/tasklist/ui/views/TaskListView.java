f31845120c23115e1ae344b7c50129f4f7bb6882
mkersten <>
2006-03-04 03:20:17 +0000

    Progress on: 113470: apply mylar to task list https://bugs.eclipse.org/bugs/show_bug.cgi?id=113470

--- org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasklist/ui/views/TaskListView.java
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasklist/ui/views/TaskListView.java
@@ -191,7 +191,7 @@ public class TaskListView extends ViewPart {
 
 	static final String FILTER_LABEL = "<filter>";
 
-	protected String[] columnNames = new String[] { "", "", "!", " ", "Description" };
+	protected String[] columnNames = new String[] { "", "", " !", " ", "Description" };
 
 	protected int[] columnWidths = new int[] { 52, 20, 12, 12, 160 };
 
