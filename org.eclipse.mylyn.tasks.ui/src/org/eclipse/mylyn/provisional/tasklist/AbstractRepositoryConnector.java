49e26d5fede2e3e89fff677ed6128675ac324764
mkersten <>
2006-04-28 20:57:03 +0000

    Progress on: new task fails to show with Mylar filter on

--- org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/provisional/tasklist/AbstractRepositoryConnector.java
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/provisional/tasklist/AbstractRepositoryConnector.java
@@ -38,7 +38,7 @@ import org.eclipse.ui.PlatformUI;
  */
 public abstract class AbstractRepositoryConnector {
 	
-	public static final String MYLAR_CONTEXT_DESCRIPTION = "mylar/context/xml/zip";
+	public static final String MYLAR_CONTEXT_DESCRIPTION = "Mylar Task Context";
 
 	private static final int MAX_REFRESH_JOBS = 5;
 
