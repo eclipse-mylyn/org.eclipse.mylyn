dc4e058077569e464f41983a25e66cb28ac792b2
mkersten <>
2006-05-16 20:13:34 +0000

    Progress on: 129619: [discussion] UI improvements https://bugs.eclipse.org/bugs/show_bug.cgi?id=129619

--- org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasklist/ui/AbstractTaskListFilter.java
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasklist/ui/AbstractTaskListFilter.java
@@ -11,8 +11,10 @@
 package org.eclipse.mylar.internal.tasklist.ui;
 
 import org.eclipse.mylar.internal.tasklist.ui.actions.NewLocalTaskAction;
+import org.eclipse.mylar.provisional.tasklist.AbstractRepositoryTask;
 import org.eclipse.mylar.provisional.tasklist.ITask;
 import org.eclipse.mylar.provisional.tasklist.MylarTaskListPlugin;
+import org.eclipse.mylar.provisional.tasklist.AbstractRepositoryTask.RepositoryTaskSyncState;
 
 /**
  * Custom filters are used so that the "Find:" filter can 'see through'
@@ -26,9 +28,24 @@ public abstract class AbstractTaskListFilter {
 
 	protected boolean shouldAlwaysShow(ITask task) {
 		return task.isActive() 
+			|| hasChanges(task) 
 			|| (MylarTaskListPlugin.getTaskListManager().isReminderToday(task) && !task.isCompleted())
 			|| (MylarTaskListPlugin.getTaskListManager().isCompletedToday(task))
 			|| (task.isPastReminder() && !task.isCompleted())
 			|| NewLocalTaskAction.DESCRIPTION_DEFAULT.equals(task.getDescription());
+	}
+
+	private boolean hasChanges(ITask task) {
+		if (task instanceof AbstractRepositoryTask) {
+			AbstractRepositoryTask repositoryTask = (AbstractRepositoryTask)task;
+			if (repositoryTask.getSyncState() == RepositoryTaskSyncState.OUTGOING) {
+				return true;
+			} else if (repositoryTask.getSyncState() == RepositoryTaskSyncState.INCOMING) {
+				return true;
+			} else if (repositoryTask.getSyncState() == RepositoryTaskSyncState.CONFLICT) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
