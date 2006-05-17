61b4f4f4a7024cfa4375361760a4aa55907942b1
mkersten <>
2006-05-17 15:40:24 +0000

    Progress on: 129619: [discussion] UI improvements https://bugs.eclipse.org/bugs/show_bug.cgi?id=129619

--- org.eclipse.mylyn.bugzilla.ui/src/org/eclipse/mylyn/internal/bugzilla/ui/tasklist/EditBugzillaQueryWizard.java
+++ org.eclipse.mylyn.bugzilla.ui/src/org/eclipse/mylyn/internal/bugzilla/ui/tasklist/EditBugzillaQueryWizard.java
@@ -13,11 +13,11 @@ package org.eclipse.mylar.internal.bugzilla.ui.tasklist;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.mylar.internal.bugzilla.core.BugzillaPlugin;
 import org.eclipse.mylar.internal.bugzilla.ui.search.BugzillaSearchPage;
 import org.eclipse.mylar.internal.core.util.MylarStatusHandler;
 import org.eclipse.mylar.internal.tasklist.TaskListPreferenceConstants;
+import org.eclipse.mylar.internal.tasklist.ui.wizards.AbstractEditQueryWizard;
 import org.eclipse.mylar.provisional.tasklist.AbstractRepositoryConnector;
 import org.eclipse.mylar.provisional.tasklist.MylarTaskListPlugin;
 import org.eclipse.mylar.provisional.tasklist.TaskRepository;
@@ -26,32 +26,22 @@ import org.eclipse.ui.actions.WorkspaceModifyOperation;
 /**
  * @author Rob Elves
  */
-public class EditBugzillaQueryWizard extends Wizard {
+public class EditBugzillaQueryWizard extends AbstractEditQueryWizard {
 
-	private static final String TITLE = "Edit Bugzilla Query";
-
-	private final TaskRepository repository;
+	public EditBugzillaQueryWizard(TaskRepository repository, BugzillaRepositoryQuery query) {
+		super(repository, query);
+	}
 
 	private AbstractBugzillaQueryPage page;
 
-	private BugzillaRepositoryQuery query;
-
-	public EditBugzillaQueryWizard(TaskRepository repository, BugzillaRepositoryQuery query) {
-		this.repository = repository;
-		this.query = query;
-		setNeedsProgressMonitor(true);
-		setWindowTitle(TITLE);
-	}
-
 	@Override
 	public void addPages() {
-		if (query.isCustomQuery()) {
-			page = new BugzillaCustomQueryWizardPage(repository, query);
+		if (((BugzillaRepositoryQuery)query).isCustomQuery()) {
+			page = new BugzillaCustomQueryWizardPage(repository, (BugzillaRepositoryQuery)query);
 		} else {
-			page = new BugzillaSearchPage(repository, query);
+			page = new BugzillaSearchPage(repository, (BugzillaRepositoryQuery)query);
 		}
 		addPage(page);
-
 	}
 
 	@Override
