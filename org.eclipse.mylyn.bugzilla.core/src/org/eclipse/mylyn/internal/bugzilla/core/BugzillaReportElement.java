ec799c88bafc8408e103111adc7bd940e673073f
relves <>
2006-05-22 20:36:54 +0000

    Progress on: 142888: Fail to add cc to bugzilla report https://bugs.eclipse.org/bugs/show_bug.cgi?id=142888

--- org.eclipse.mylyn.bugzilla.core/src/org/eclipse/mylyn/internal/bugzilla/core/BugzillaReportElement.java
+++ org.eclipse.mylyn.bugzilla.core/src/org/eclipse/mylyn/internal/bugzilla/core/BugzillaReportElement.java
@@ -65,7 +65,7 @@ public enum BugzillaReportElement {
 	VERSION ("Version:", "version", false),
 	VOTES ("Votes:", "votes", false, true),
 	WHO ("who", "who"),
-	QA_CONTACT("QA Contact", "qa_contact", false, true),
+	QA_CONTACT("QA Contact", "qa_contact", false, false),
 	//ADDSELFCC ("Add self to CC", "addselfcc", true, false),
 	// Used by search engine
 	LI ("used by search engine", "li", true),
