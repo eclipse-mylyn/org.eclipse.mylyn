1bc4473b14e75efed38f6b11ae7004b402f4bda2
relves <>
2006-05-24 21:33:23 +0000

    Progress on: 143017: stack trace hyperlink detection fails to open types https://bugs.eclipse.org/bugs/show_bug.cgi?id=143017

--- org.eclipse.mylyn.tasks.core/src/org/eclipse/mylyn/internal/bugs/java/JavaStackTraceHyperlinkDetector.java
+++ org.eclipse.mylyn.tasks.core/src/org/eclipse/mylyn/internal/bugs/java/JavaStackTraceHyperlinkDetector.java
@@ -11,8 +11,9 @@
 
 package org.eclipse.mylar.internal.bugs.java;
 
-import org.eclipse.jdt.internal.ui.util.StringMatcher;
-import org.eclipse.jdt.internal.ui.util.StringMatcher.Position;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
@@ -48,16 +49,29 @@ public class JavaStackTraceHyperlinkDetector implements IHyperlinkDetector {
 			return null;
 		}
 
-		//int offsetInLine = offset - lineInfo.getOffset();
+		// int offsetInLine = offset - lineInfo.getOffset();
 
-		StringMatcher javaElementMatcher = new StringMatcher("*(*.java:*)", true, false);
-		
-		Position position = javaElementMatcher.find(line, 0, line.length());
-		if (position != null) {
-			//String linkText = line.substring(position.getStart() + 1, position.getEnd() - 1);
-			IRegion urlRegion= new Region(lineInfo.getOffset() + position.getStart() + 1, position.getEnd() - position.getStart() - 2);
-			return new IHyperlink[] {new JavaStackTraceFileHyperlink(urlRegion, line)};
+		Pattern p = Pattern.compile("[ {1}|\\n].*({1}.*.{1}java:\\d*){1}", Pattern.CASE_INSENSITIVE);
+		Matcher m = p.matcher(line);
+
+		if (m.find()) {
+			IRegion urlRegion = new Region(lineInfo.getOffset() + m.start() + 1, m.end() - m.start());
+			return new IHyperlink[] { new JavaStackTraceFileHyperlink(urlRegion, m.group()) };
 		}
+
+		// StringMatcher javaElementMatcher = new StringMatcher("*(*.java:*)",
+		// true, false);//[\r|\n|\b|^]
+		//		
+		// Position position = javaElementMatcher.find(line, 0, line.length());
+		// if (position != null) {
+		// //String linkText = line.substring(position.getStart() + 1,
+		// position.getEnd() - 1);
+		// IRegion urlRegion= new Region(lineInfo.getOffset() +
+		// position.getStart() + 1, position.getEnd() - position.getStart() -
+		// 2);
+		// return new IHyperlink[] {new JavaStackTraceFileHyperlink(urlRegion,
+		// line)};
+		// }
 		return null;
 	}
 
