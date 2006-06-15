7c533a3927b37b08152c162cc136a239bd83a493
mkersten <>
2006-06-15 18:41:31 +0000

    Progress on: 143583: Need infrastructure to host summer of code projects https://bugs.eclipse.org/bugs/show_bug.cgi?id=143583

---
+++ org.eclipse.mylyn.trac.ui/src/org/eclipse/mylyn/internal/trac/MylarTracPlugin.java
@@ -0,0 +1,50 @@
+package org.eclipse.mylar.internal.trac;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class MylarTracPlugin extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.eclipse.mylar.trac";
+
+	// The shared instance
+	private static MylarTracPlugin plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public MylarTracPlugin() {
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static MylarTracPlugin getDefault() {
+		return plugin;
+	}
+
+}
