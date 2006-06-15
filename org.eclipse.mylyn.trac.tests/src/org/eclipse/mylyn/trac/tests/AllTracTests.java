2b2105efe109fbed51a7147d59cd7d5270e2a067
mkersten <>
2006-06-15 18:46:45 +0000

    Progress on: 143583: Need infrastructure to host summer of code projects https://bugs.eclipse.org/bugs/show_bug.cgi?id=143583

---
+++ org.eclipse.mylyn.trac.tests/src/org/eclipse/mylyn/trac/tests/AllTracTests.java
@@ -0,0 +1,19 @@
+package org.eclipse.mylar.trac.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * @author Mik Kersten
+ */
+public class AllTracTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite("Test for org.eclipse.mylar.trac.tests");
+		//$JUnit-BEGIN$
+
+		//$JUnit-END$
+		return suite;
+	}
+
+}
--- org.eclipse.mylyn.trac.ui/src/org/eclipse/mylyn/internal/trac/MylarTracPlugin.java
+++ org.eclipse.mylyn.trac.ui/src/org/eclipse/mylyn/internal/trac/MylarTracPlugin.java
@@ -1,48 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2006 - 2006 Mylar eclipse.org project and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Mylar project committers - initial API and implementation
+ *******************************************************************************/
+
 package org.eclipse.mylar.internal.trac;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
- * The activator class controls the plug-in life cycle
+ * @author Mik Kersten
  */
 public class MylarTracPlugin extends AbstractUIPlugin {
 
-	// The plug-in ID
 	public static final String PLUGIN_ID = "org.eclipse.mylar.trac";
 
-	// The shared instance
 	private static MylarTracPlugin plugin;
-	
-	/**
-	 * The constructor
-	 */
+
 	public MylarTracPlugin() {
 		plugin = this;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
 	}
 
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
 	public static MylarTracPlugin getDefault() {
 		return plugin;
 	}
