403e6b4eaab9b8e7c22fec1caeab04741ba0face
mkersten <>
2006-06-16 16:50:47 +0000

    Progress on: 145794: make test case for retrieving Trac report https://bugs.eclipse.org/bugs/show_bug.cgi?id=145794

--- org.eclipse.mylyn.trac.tests/src/org/eclipse/mylyn/trac/tests/AllTracTests.java
+++ org.eclipse.mylyn.trac.tests/src/org/eclipse/mylyn/trac/tests/AllTracTests.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2006 - 2006 Mylar eclipse.org project and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Mylar project committers - initial API and implementation
+ *******************************************************************************/
+
 package org.eclipse.mylar.trac.tests;
 
 import junit.framework.Test;
@@ -11,7 +22,7 @@ public class AllTracTests {
 	public static Test suite() {
 		TestSuite suite = new TestSuite("Test for org.eclipse.mylar.trac.tests");
 		//$JUnit-BEGIN$
-
+		suite.addTestSuite(TracXmlRpcTest.class);
 		//$JUnit-END$
 		return suite;
 	}
