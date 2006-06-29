4934c30a4f28b27ec85f0513ce90b51707bda9a8
mkersten <>
2006-06-29 21:59:55 +0000

    3.1 merge

--- org.eclipse.mylyn.tests/src/org/eclipse/mylyn/tests/AllTests.java
+++ org.eclipse.mylyn.tests/src/org/eclipse/mylyn/tests/AllTests.java
@@ -21,8 +21,7 @@ import org.eclipse.mylar.internal.core.util.MylarStatusHandler;
 import org.eclipse.mylar.internal.ide.MylarIdePlugin;
 import org.eclipse.mylar.java.tests.AllJavaTests;
 import org.eclipse.mylar.monitor.reports.tests.AllMonitorReportTests;
-import org.eclipse.mylar.monitor.tests.AllMonitorTests;
-import org.eclipse.mylar.tasklist.tests.AllTasklistTests;
+import org.eclipse.mylar.tasklist.tests.AllTaskListTests;
 import org.eclipse.mylar.tests.integration.AllIntegrationTests;
 import org.eclipse.mylar.tests.misc.AllMiscTests;
 import org.eclipse.mylar.tests.xml.AllXmlTests;
@@ -41,16 +40,16 @@ public class AllTests {
 		// TODO: the order of these tests might still matter, but shouldn't
 		// $JUnit-BEGIN$
 		suite.addTest(AllMonitorReportTests.suite());
-		suite.addTest(AllMonitorTests.suite());
-		suite.addTest(AllIntegrationTests.suite());
+//		suite.addTest(AllMonitorTests.suite());
 		suite.addTest(AllCoreTests.suite());
 		suite.addTest(AllIdeTests.suite());
 		suite.addTest(AllJavaTests.suite());
-		suite.addTest(AllTasklistTests.suite());
+		suite.addTest(AllTaskListTests.suite());
 		suite.addTest(AllXmlTests.suite());
 		suite.addTest(AllBugzillaTests.suite());
 //		suite.addTest(AllJiraTests.suite());  
 		suite.addTest(AllMiscTests.suite());
+		suite.addTest(AllIntegrationTests.suite());
 		// $JUnit-END$
 		return suite;
 	}
