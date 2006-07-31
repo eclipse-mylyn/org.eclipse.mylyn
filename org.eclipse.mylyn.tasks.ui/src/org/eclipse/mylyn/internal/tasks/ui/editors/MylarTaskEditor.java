8c934f9e5ab9400c2d7c2c588769bb524a5c8b72
relves <>
2006-07-31 17:11:09 +0000

    Progress on: 152255: Failure when saving newly created local task https://bugs.eclipse.org/bugs/show_bug.cgi?id=152255

--- org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/editors/MylarTaskEditor.java
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/editors/MylarTaskEditor.java
@@ -487,10 +487,9 @@ public class MylarTaskEditor extends FormEditor {
 				taskEditorInput = (TaskEditorInput) getEditorInput();
 				task = taskEditorInput.getTask();
 				setPartName(taskEditorInput.getLabel());					
+			} else {
+				this.setTitleImage(TaskListImages.getImage(TaskListImages.TASK_REMOTE));
 			}
-//			else {
-//				this.setTitleImage(TaskListImages.getImage(TaskListImages.O));
-//			}
 			
 			int selectedIndex = index;
 			for (ITaskEditorFactory factory : TasksUiPlugin.getDefault().getTaskEditorFactories()) {
