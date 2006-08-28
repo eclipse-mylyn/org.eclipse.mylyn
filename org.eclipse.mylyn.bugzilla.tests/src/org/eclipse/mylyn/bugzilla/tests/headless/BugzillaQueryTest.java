c13363fe9182603c686189753fb2dc5736eb8be1
relves <>
2006-08-28 19:40:27 +0000

    Progress on: 153018: bugzilla.tests.headless doesn't actually test the headless api. https://bugs.eclipse.org/bugs/show_bug.cgi?id=153018

--- org.eclipse.mylyn.bugzilla.tests/src/org/eclipse/mylyn/bugzilla/tests/headless/BugzillaQueryTest.java
+++ org.eclipse.mylyn.bugzilla.tests/src/org/eclipse/mylyn/bugzilla/tests/headless/BugzillaQueryTest.java
@@ -11,18 +11,30 @@
 
 package org.eclipse.mylar.bugzilla.tests.headless;
 
+import java.io.IOException;
+import java.util.List;
+
+import javax.security.auth.login.LoginException;
+
 import junit.framework.TestCase;
 
-import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.mylar.core.core.tests.support.MylarTestUtils;
+import org.eclipse.mylar.core.core.tests.support.MylarTestUtils.Credentials;
 import org.eclipse.mylar.internal.bugzilla.core.BugzillaCorePlugin;
+import org.eclipse.mylar.internal.bugzilla.core.BugzillaException;
+import org.eclipse.mylar.internal.bugzilla.core.BugzillaReportElement;
+import org.eclipse.mylar.internal.bugzilla.core.BugzillaServerFacade;
 import org.eclipse.mylar.internal.bugzilla.core.IBugzillaConstants;
-import org.eclipse.mylar.internal.bugzilla.ui.search.BugzillaResultCollector;
-import org.eclipse.mylar.internal.bugzilla.ui.search.BugzillaSearchHit;
-import org.eclipse.mylar.internal.bugzilla.ui.search.BugzillaSearchOperation;
+import org.eclipse.mylar.tasks.core.RepositoryTaskData;
 import org.eclipse.mylar.tasks.core.TaskRepository;
 
 /**
+ * 
+ * Runs headless (can be run as regular junit test without platform plugin
+ * support).
+ * 
  * @author Rob Elves
+ * @author Nathan Hapke
  */
 public class BugzillaQueryTest extends TestCase {
 
@@ -34,25 +46,73 @@ public class BugzillaQueryTest extends TestCase {
 		repository = new TaskRepository(BugzillaCorePlugin.REPOSITORY_KIND, IBugzillaConstants.TEST_BUGZILLA_222_URL);
 	}
 
-	public void testQueryBugs() throws Exception {
-		BugzillaResultCollector collector = new BugzillaResultCollector();
-		collector.setProgressMonitor(new NullProgressMonitor());
-		BugzillaSearchOperation operation = new BugzillaSearchOperation(
-				repository,
-				"http://mylar.eclipse.org/bugs222/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=search-match-test&product=TestProduct&long_desc_type=substring&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&deadlinefrom=&deadlineto=&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&emailassigned_to1=1&emailtype1=substring&email1=&emailassigned_to2=1&emailreporter2=1&emailcc2=1&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=",
-				null, collector, "-1");
-		operation.run(new NullProgressMonitor());
+	/**
+	 * This is the first test so that the repository credentials are correctly
+	 * set for the other tests
+	 */
+	public void testAddCredentials() {
+		if (!repository.hasCredentials()) {
+			Credentials credentials = MylarTestUtils.readCredentials();
+			repository.setAuthenticationCredentials(credentials.username, credentials.password);
 
-		assertEquals(2, collector.getResults().size());
-		for (BugzillaSearchHit hit : collector.getResults()) {
-			assertTrue(hit.getDescription().startsWith("search-match-test"));
+			assertTrue(repository.hasCredentials());
 		}
 	}
 
-	// public void testRetrieveBug() throws Exception {
-	// RepositoryTaskData taskData =
-	// BugzillaRepositoryUtil.getBug(IBugzillaConstants.TEST_BUGZILLA_222_URL,
-	// "", "", null, "UTF-8", 1);
-	// assertNotNull(taskData);
-	// }
+	/**
+	 * @throws LoginException
+	 *             Username / Password invalid
+	 * @throws IOException
+	 *             Network or parse failed
+	 * @throws BugzillaException
+	 *             Midair collision, or other bugzilla failure. See
+	 *             {@link BugzillaServerFacade.parseHtmlError}
+	 */
+	public void testValidateCredentials() throws LoginException, IOException, BugzillaException {
+		BugzillaServerFacade.validateCredentials(null, repository.getUrl(), repository.getUserName(), repository
+				.getPassword());
+	}
+
+	public void testGetBug() throws Exception {
+		RepositoryTaskData taskData = BugzillaServerFacade.getBug(repository.getUrl(), repository.getUserName(),
+				repository.getPassword(), null, repository.getCharacterEncoding(), 1);
+		assertNotNull(taskData);
+		assertEquals("user@mylar.eclipse.org", taskData.getAssignedTo());
+
+		// Notice that comment 0 is the Bug Description
+		assertEquals("foo", taskData.getComments().get(0).getText());
+		assertEquals("nhapke@cs.ubc.ca", taskData.getComments().get(0).getAuthor());
+
+		// You can use the getAttributeValue to pull up the information on any
+		// part of the bug
+		assertEquals("P1", taskData.getAttributeValue(BugzillaReportElement.PRIORITY.getKeyString()));
+	}
+
+	public void testGetProductList() throws Exception {
+		List<String> products = BugzillaServerFacade.getProductList(repository.getUrl(), null,
+				repository.getUserName(), repository.getPassword(), repository.getCharacterEncoding());
+
+		assertEquals(3, products.size());
+		assertTrue(products.contains("Read Only Test Cases"));
+		assertTrue(products.contains("Read Write Test Cases"));
+		assertTrue(products.contains("TestProduct"));
+	}
+
+	public void testQueryBugs() throws Exception {
+
+		// Not yet refactored. See bug 155279
+
+		// BugzillaResultCollector collector = new BugzillaResultCollector();
+		// collector.setProgressMonitor(new NullProgressMonitor());
+		// BugzillaSearchOperation operation = new BugzillaSearchOperation(
+		// repository,
+		// "http://mylar.eclipse.org/bugs222/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=search-match-test&product=TestProduct&long_desc_type=substring&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&deadlinefrom=&deadlineto=&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&emailassigned_to1=1&emailtype1=substring&email1=&emailassigned_to2=1&emailreporter2=1&emailcc2=1&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=",
+		// null, collector, "-1");
+		// operation.run(new NullProgressMonitor());
+		//
+		// assertEquals(2, collector.getResults().size());
+		// for (BugzillaSearchHit hit : collector.getResults()) {
+		// assertTrue(hit.getDescription().startsWith("search-match-test"));
+		// }
+	}
 }
