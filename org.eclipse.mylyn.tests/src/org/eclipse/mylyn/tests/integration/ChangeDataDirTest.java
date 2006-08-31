6e062c235450da31d84f9e5a0c4aef89cd7a41fe
relves <>
2006-08-31 18:44:57 +0000

    Progress on: 155279: Split search into headless and ui https://bugs.eclipse.org/bugs/show_bug.cgi?id=155279

--- org.eclipse.mylyn.tests/src/org/eclipse/mylyn/tests/integration/ChangeDataDirTest.java
+++ org.eclipse.mylyn.tests/src/org/eclipse/mylyn/tests/integration/ChangeDataDirTest.java
@@ -19,7 +19,7 @@ import junit.framework.TestCase;
 
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.mylar.context.core.InteractionEvent;
-import org.eclipse.mylar.internal.bugzilla.ui.tasklist.BugzillaTask;
+import org.eclipse.mylar.internal.bugzilla.core.BugzillaTask;
 import org.eclipse.mylar.internal.context.core.MylarContextManager;
 import org.eclipse.mylar.monitor.usage.MylarUsageMonitorPlugin;
 import org.eclipse.mylar.tasks.core.AbstractRepositoryTask;
