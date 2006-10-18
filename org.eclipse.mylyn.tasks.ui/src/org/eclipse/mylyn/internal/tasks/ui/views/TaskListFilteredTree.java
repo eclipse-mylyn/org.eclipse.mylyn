d452700a211bf26ef6457eea936d30aad239a27a
mkersten <>
2006-10-18 01:50:14 +0000

    RESOLVED - bug 160686: activate task dialog allows multiple selection, but should only allow one, as it is only possible to activate one task at a time

--- org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/views/TaskListFilteredTree.java
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/views/TaskListFilteredTree.java
@@ -11,11 +11,21 @@
 
 package org.eclipse.mylar.internal.tasks.ui.views;
 
+import java.util.List;
+import java.util.Set;
+
+import org.eclipse.mylar.internal.tasks.ui.TaskListColorsAndFonts;
+import org.eclipse.mylar.tasks.core.AbstractTaskContainer;
+import org.eclipse.mylar.tasks.core.DateRangeContainer;
 import org.eclipse.mylar.tasks.core.ITask;
+import org.eclipse.mylar.tasks.core.ITaskActivityListener;
+import org.eclipse.mylar.tasks.core.ITaskListChangeListener;
 import org.eclipse.mylar.tasks.ui.TasksUiPlugin;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.dialogs.PatternFilter;
 import org.eclipse.ui.forms.widgets.Hyperlink;
@@ -33,7 +43,99 @@ public class TaskListFilteredTree extends AbstractMylarFilteredTree {
 	
 	private Hyperlink activeTaskLabel;
 	
+	private TaskProgressBar taskProgressBar;
+	
+	@Override
+	protected Composite createProgressComposite(Composite container) {
+//		container.setBackground(new Color(Display.getDefault(), 255, 0, 0));
+//		Composite composite = new Composite(container, SWT.NULL);
+		
+		taskProgressBar = new TaskProgressBar(container);
+		taskProgressBar.setBackground(TaskListColorsAndFonts.COLOR_TASK_COMPLETED);
+		
+		GridLayout gridLayout = new GridLayout();
+		gridLayout.marginWidth = 0;
+		gridLayout.marginHeight = 0;
+		container.setLayout(gridLayout);
+		
+		taskProgressBar.setLayoutData(
+				new GridData(GridData.GRAB_VERTICAL | GridData.VERTICAL_ALIGN_FILL | GridData.GRAB_HORIZONTAL | GridData.HORIZONTAL_ALIGN_FILL));		
+		
+		TasksUiPlugin.getTaskListManager().getTaskList().addChangeListener(new ITaskListChangeListener() {
+
+			public void containerAdded(AbstractTaskContainer container) {
+			}
+
+			public void containerDeleted(AbstractTaskContainer container) {
+			}
+
+			public void containerInfoChanged(AbstractTaskContainer container) {
+			}
+
+			public void localInfoChanged(ITask task) {
+				updateTaskProgressBar(TasksUiPlugin.getTaskListManager().getActivityThisWeek());
+			}
+
+			public void repositoryInfoChanged(ITask task) {
+			}
+
+			public void taskAdded(ITask task) {
+			}
+
+			public void taskDeleted(ITask task) {
+			}
+
+			public void taskMoved(ITask task, AbstractTaskContainer fromContainer, AbstractTaskContainer toContainer) {
+			}
+			
+		});
+		
+		TasksUiPlugin.getTaskListManager().addActivityListener(new ITaskActivityListener() {
+
+			public void activityChanged(DateRangeContainer week) {
+				updateTaskProgressBar(week);
+			}
+
+			public void calendarChanged() {
+			}
+
+			public void taskActivated(ITask task) {
+			}
+
+			public void taskDeactivated(ITask task) {
+			}
+
+			public void taskListRead() {
+			}
+
+			public void tasksActivated(List<ITask> tasks) {
+			}
+			
+		});
+		return container;
+	}
+	
+	private void updateTaskProgressBar(DateRangeContainer week) {
+		if (taskProgressBar.isDisposed()) {
+			return;
+		}
+		Set<ITask> tasksThisWeek = week.getChildren();
+		int totalThisWeek = tasksThisWeek.size();
+		int totalCompleted = 0;
+		for (ITask task : tasksThisWeek) {
+			if (task.isCompleted()) {
+				totalCompleted++;
+			}
+		}
+		
+		taskProgressBar.reset(totalCompleted, totalThisWeek);
+//		taskProgressBar.setMaximum(totalThisWeek);
+//		taskProgressBar.setCount(totalCompleted);
+		taskProgressBar.setToolTipText("Completed " + totalCompleted + " of " + totalThisWeek + " tasks scheduled for this week");
+	}
+	
 	protected Composite createStatusComposite(Composite container) {
+
 		activeTaskLabel = new Hyperlink(container, SWT.LEFT);
 		activeTaskLabel.setText(LABEL_NO_ACTIVE);
 		ITask activeTask = TasksUiPlugin.getTaskListManager().getTaskList().getActiveTask();
