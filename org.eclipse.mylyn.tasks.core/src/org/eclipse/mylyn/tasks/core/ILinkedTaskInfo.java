bf99d8e8724bc2721cd3b6832fbe39d1389099fa
mkersten <>
2006-12-05 01:57:53 +0000

    NEW - bug 165581: Improve Open Correspond Task action https://bugs.eclipse.org/bugs/show_bug.cgi?id=165581

---
+++ org.eclipse.mylyn.tasks.core/src/org/eclipse/mylyn/tasks/core/ILinkedTaskInfo.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2004 - 2006 University Of British Columbia and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Eugene Kuleshov - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.mylar.tasks.core;
+
+/**
+ * Task information linked to artifacts from version control integration 
+ * 
+ * @author Eugene Kuleshov
+ */
+public interface ILinkedTaskInfo {
+
+	public abstract String getTaskId();
+
+	public abstract String getTaskFullUrl();
+
+	public abstract String getRepositoryUrl();
+	
+	public abstract ITask getTask();
+	
+	public abstract String getComment();
+	
+}
