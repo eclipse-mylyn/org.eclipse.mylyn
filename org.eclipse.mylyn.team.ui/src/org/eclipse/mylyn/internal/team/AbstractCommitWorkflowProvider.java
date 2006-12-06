58d3cc35723ec8c4ca79a708b680c88b80b69c55
mkersten <>
2006-12-06 23:25:11 +0000

    NEW - bug 166920: [api] address internal API usage of change set management https://bugs.eclipse.org/bugs/show_bug.cgi?id=166920

--- org.eclipse.mylyn.team.ui/src/org/eclipse/mylyn/internal/team/AbstractCommitWorkflowProvider.java
+++ org.eclipse.mylyn.team.ui/src/org/eclipse/mylyn/internal/team/AbstractCommitWorkflowProvider.java
@@ -6,7 +6,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
 
-package org.eclipse.mylar.team;
+package org.eclipse.mylar.internal.team;
 
 import org.eclipse.core.resources.IResource;
 
