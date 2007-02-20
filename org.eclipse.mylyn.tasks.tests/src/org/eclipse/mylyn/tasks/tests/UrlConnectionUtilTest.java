27caa1121c72edc21c178cb121e4b4e35027a425
relves <>
2007-02-20 21:31:42 +0000

    NEW - bug 172395: JIRA connector should support NTLM proxy authentication https://bugs.eclipse.org/bugs/show_bug.cgi?id=172395

--- org.eclipse.mylyn.tasks.tests/src/org/eclipse/mylyn/tasks/tests/UrlConnectionUtilTest.java
+++ org.eclipse.mylyn.tasks.tests/src/org/eclipse/mylyn/tasks/tests/UrlConnectionUtilTest.java
@@ -8,8 +8,14 @@
 
 package org.eclipse.mylar.tasks.tests;
 
+import java.net.InetSocketAddress;
+import java.net.Proxy.Type;
+
 import junit.framework.TestCase;
 
+import org.apache.commons.httpclient.NTCredentials;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.eclipse.mylar.core.net.AuthenticatedProxy;
 import org.eclipse.mylar.core.net.WebClientUtil;
 
 public class UrlConnectionUtilTest extends TestCase {
@@ -35,5 +41,22 @@ public class UrlConnectionUtilTest extends TestCase {
 		assertEquals("example.com", WebClientUtil.getDomain(url));
 		assertEquals("", WebClientUtil.getRequestPath(url));
 	}
+	
+	public void testCredentials() {
+		AuthenticatedProxy proxy = new AuthenticatedProxy(Type.HTTP, new InetSocketAddress(4567), "user", "password");
+		UsernamePasswordCredentials credentials = (UsernamePasswordCredentials) WebClientUtil.getCredentials(proxy, new InetSocketAddress(1234));
+		assertEquals("user", credentials.getUserName());
+		assertEquals("password", credentials.getPassword());
+		
+		proxy = new AuthenticatedProxy(Type.HTTP, new InetSocketAddress(4567), "domain\\user", "password");
+		NTCredentials ntCredentials = (NTCredentials) WebClientUtil.getCredentials(proxy, new InetSocketAddress("mylar.eclipse.org", 1234));
+		assertEquals("user", ntCredentials.getUserName());
+		assertEquals("password", ntCredentials.getPassword());
+		assertEquals("domain", ntCredentials.getDomain());
+		assertEquals("mylar.eclipse.org", ntCredentials.getHost());
+	}
+	
+	
+	
 
 }
