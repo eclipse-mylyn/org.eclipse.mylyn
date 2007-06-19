e7ce7efddddd3aeba98dc7463b8e3796dbdc1247
mkersten <>
2007-06-19 23:53:00 +0000

    NEW - bug 165359: fix UI nits https://bugs.eclipse.org/bugs/show_bug.cgi?id=165359

--- org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/views/TaskRepositoriesSorter.java
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/views/TaskRepositoriesSorter.java
@@ -10,6 +10,7 @@ package org.eclipse.mylyn.internal.tasks.ui.views;
 
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerSorter;
+import org.eclipse.mylyn.internal.tasks.core.LocalRepositoryConnector;
 import org.eclipse.mylyn.tasks.core.IRepositoryConstants;
 import org.eclipse.mylyn.tasks.core.TaskRepository;
 
@@ -23,8 +24,15 @@ public class TaskRepositoriesSorter extends ViewerSorter {
 		if (e1 instanceof TaskRepository && e2 instanceof TaskRepository) {
 			TaskRepository t1 = (TaskRepository) e1;
 			TaskRepository t2 = (TaskRepository) e2;
+		
 			String label1 = t1.getProperty(IRepositoryConstants.PROPERTY_LABEL);
 			String label2 = t2.getProperty(IRepositoryConstants.PROPERTY_LABEL);
+		
+			if (LocalRepositoryConnector.REPOSITORY_LABEL.equals(label1)) {
+				return -1;
+			} else if (LocalRepositoryConnector.REPOSITORY_LABEL.equals(label2)) {
+				return 1;
+			}
 			
 			if (!t1.getKind().equals(t2.getKind())) {
 				return (t1.getKind()).compareTo(t2.getKind());
