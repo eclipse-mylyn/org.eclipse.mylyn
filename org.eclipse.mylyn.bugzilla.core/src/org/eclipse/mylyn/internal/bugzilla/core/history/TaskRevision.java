1edde4b0c0332981d3eacf81fa71ae70013ebe71
mkersten <>
2007-06-26 01:16:41 +0000

    RESOLVED - bug 194157: update copyright notices https://bugs.eclipse.org/bugs/show_bug.cgi?id=194157

--- org.eclipse.mylyn.bugzilla.core/src/org/eclipse/mylyn/internal/bugzilla/core/history/TaskRevision.java
+++ org.eclipse.mylyn.bugzilla.core/src/org/eclipse/mylyn/internal/bugzilla/core/history/TaskRevision.java
@@ -1,12 +1,9 @@
 /*******************************************************************************
- * Copyright (c) 2003 University Of British Columbia and others.
- * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Common Public License v1.0
+ * Copyright (c) 2003, 2007 Mylyn project committers and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v10.html
- * 
- * Contributors:
- *     University Of British Columbia - initial API and implementation
+ * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
 package org.eclipse.mylyn.internal.bugzilla.core.history;
 
