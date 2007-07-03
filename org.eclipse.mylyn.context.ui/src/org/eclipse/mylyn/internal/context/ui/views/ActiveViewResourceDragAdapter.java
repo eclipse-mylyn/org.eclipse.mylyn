be8a9bf24396a9f92bb7cafec2de533aff8e9a1d
mkersten <>
2007-07-03 23:18:39 +0000

    NEW - bug 195120: Incorrect Copyright dates in About Box https://bugs.eclipse.org/bugs/show_bug.cgi?id=195120

--- org.eclipse.mylyn.context.ui/src/org/eclipse/mylyn/internal/context/ui/views/ActiveViewResourceDragAdapter.java
+++ org.eclipse.mylyn.context.ui/src/org/eclipse/mylyn/internal/context/ui/views/ActiveViewResourceDragAdapter.java
@@ -5,17 +5,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
-///*******************************************************************************
-// * Copyright (c) 2004 - 2006 University Of British Columbia and others.
-// * All rights reserved. This program and the accompanying materials
-// * are made available under the terms of the Eclipse Public License v1.0
-// * which accompanies this distribution, and is available at
-// * http://www.eclipse.org/legal/epl-v10.html
-// *
-// * Contributors:
-// *     University Of British Columbia - initial API and implementation
-// *******************************************************************************/
-//
+
 //package org.eclipse.mylyn.internal.context.ui.views;
 //
 //import java.util.ArrayList;
