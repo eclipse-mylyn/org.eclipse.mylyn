626ef9334d2ab175061c22d0cbe5fb127be6bd1a
mkersten <>
2007-08-26 20:31:03 +0000

    REOPENED - bug 195295: fix layout of download page on IE6 https://bugs.eclipse.org/bugs/show_bug.cgi?id=195295

---
+++ org.eclipse.mylyn.tasks.core/src/org/eclipse/mylyn/tasks/core/TaskGroup.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Mylyn project committers and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.mylyn.tasks.core;
+
+import java.util.Set;
+
+/**
+ * @author Eugene Kuleshov
+ * @since 2.1
+ */
+public class TaskGroup extends AbstractTaskCategory {
+
+	private final String summary;
+
+	private final String groupBy;
+
+	public TaskGroup(String parentHandle, String summary, String groupBy) {
+		super(parentHandle + summary);
+		this.summary = summary;
+		this.groupBy = groupBy;
+	}
+
+	@Override
+	public Set<AbstractTask> getChildren() {
+		return super.getChildren();
+	}
+
+	@Override
+	public String getSummary() {
+		return summary + " / " + super.getChildren().size();
+	}
+
+	public boolean isUserDefined() {
+		return false;
+	}
+
+	public String getGroupBy() {
+		return groupBy;
+	}
+
+}
