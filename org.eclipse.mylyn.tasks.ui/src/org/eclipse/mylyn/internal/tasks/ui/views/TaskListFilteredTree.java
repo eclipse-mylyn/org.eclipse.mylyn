bf566970d0cb3b2f7bbba1ef39fe64b384b1c0aa
mkersten <>
2007-10-10 23:17:32 +0000

    RESOLVED - bug 205538: Clicking active task hyperlink results in go up to root https://bugs.eclipse.org/bugs/show_bug.cgi?id=205538

--- org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/views/TaskListFilteredTree.java
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/views/TaskListFilteredTree.java
@@ -355,13 +355,13 @@ public class TaskListFilteredTree extends AbstractFilteredTree {
 						activateAction.init(PlatformUI.getWorkbench().getActiveWorkbenchWindow());
 						activateAction.run(null);
 					} else {
-						if (TaskListFilteredTree.super.filterText.getText().length() > 0) {
-							TaskListFilteredTree.super.filterText.setText("");
-							TaskListFilteredTree.this.textChanged();
-						}
-						if (TaskListView.getFromActivePerspective().getDrilledIntoCategory() != null) {
-							TaskListView.getFromActivePerspective().goUpToRoot();
-						}
+//						if (TaskListFilteredTree.super.filterText.getText().length() > 0) {
+//							TaskListFilteredTree.super.filterText.setText("");
+//							TaskListFilteredTree.this.textChanged();
+//						}
+//						if (TaskListView.getFromActivePerspective().getDrilledIntoCategory() != null) {
+//							TaskListView.getFromActivePerspective().goUpToRoot();
+//						}
 						TasksUiUtil.refreshAndOpenTaskListElement(activeTask);
 					}
 				}
