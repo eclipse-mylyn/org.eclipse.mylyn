a590b875eec13ac5174362e58aa4b183ef977fd3
relves <>
2007-11-02 01:44:41 +0000

    NEW - bug 108291: automated headless build process on mylyn.eclipse.org server https://bugs.eclipse.org/bugs/show_bug.cgi?id=108291

--- org.eclipse.mylyn.releng/3.4/config/build.sh
+++ org.eclipse.mylyn.releng/3.4/config/build.sh
@@ -3,10 +3,17 @@ rm -R ../build
 mkdir ../build
 cp -R ../../maps ../build/
 
+# BEGIN USER SETTINGS
+
+# Qualifier format: YYYYMMDDHHMM 
+QUALIFIER=200711011830
+
+# root of build tree
 BUILD_HOME=/home/build/mylyn/org.eclipse.mylyn/org.eclipse.mylyn.releng/3.4
 
+# END USER SETTINGS
 export JAVA_HOME=/home/build/mylyn/jdk/jdk1.6.0_03
 
 cd $BUILD_HOME/eclipse3.3/plugins/org.eclipse.pde.build_3.3.2.R331_v20071019/scripts
 
-$JAVA_HOME/bin/java -jar $BUILD_HOME/eclipse3.3/plugins/org.eclipse.equinox.launcher_1.0.1.R33x_v20070828.jar  -application org.eclipse.ant.core.antRunner -buildfile build.xml -Dbase=$BUILD_HOME -DbaseLocation=$BUILD_HOME/eclipse3.4 -Dbuilder=$BUILD_HOME/config
+$JAVA_HOME/bin/java -jar $BUILD_HOME/eclipse3.3/plugins/org.eclipse.equinox.launcher_1.0.1.R33x_v20070828.jar  -application org.eclipse.ant.core.antRunner -buildfile build.xml -Dbase=$BUILD_HOME -DbaseLocation=$BUILD_HOME/eclipse3.4 -Dbuilder=$BUILD_HOME/config -DforceContextQualifier=$QUALIFIER
