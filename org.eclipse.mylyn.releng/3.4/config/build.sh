1c9d82cf23ad93131ed636a5d3ccff83f6794df9
relves <>
2007-11-03 02:27:55 +0000

    NEW - bug 108291: automated headless build process on mylyn.eclipse.org server https://bugs.eclipse.org/bugs/show_bug.cgi?id=108291

--- org.eclipse.mylyn.releng/3.4/config/build.sh
+++ org.eclipse.mylyn.releng/3.4/config/build.sh
@@ -15,6 +15,7 @@ export JAVA_HOME=/home/releng/jdk1.6.0_03
 
 rm -R ../build
 mkdir ../build
-cp -R ../../maps ../build/
+mkdir ../build/maps
+cp ../../maps/mylyn_e3.4.map ../build/maps/
 cd $BUILD_HOME/eclipse3.3/plugins/org.eclipse.pde.build_3.3.2.R331_v20071019/scripts
 $JAVA_HOME/bin/java -jar $BUILD_HOME/eclipse3.3/plugins/org.eclipse.equinox.launcher_1.0.1.R33x_v20070828.jar  -application org.eclipse.ant.core.antRunner -buildfile build.xml -Dbase=$BUILD_HOME -DbaseLocation=$BUILD_HOME/eclipse3.4 -Dbuilder=$BUILD_HOME/config -DforceContextQualifier=$QUALIFIER -DmajorVersion=$MAJOR_VERSION
