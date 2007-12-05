46644367d6597f320e248dd496b208c13d9414e8
mkersten <>
2007-12-05 18:56:12 +0000

    NEW - bug 177974: [Workbench] provide desktop notification popup https://bugs.eclipse.org/bugs/show_bug.cgi?id=177974

---
+++ org.eclipse.mylyn.tests/src/org/eclipse/mylyn/tests/manual/NoticicationPopupMain.java
@@ -0,0 +1,46 @@
+/*
+ * Created on 13.11.2007
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+
+package org.eclipse.mylyn.tests.manual;
+
+import org.eclipse.mylyn.internal.tasks.ui.notifications.AbstractNotificationPopup;
+import org.eclipse.swt.events.ShellAdapter;
+import org.eclipse.swt.events.ShellEvent;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+/**
+ * @author Benjamin Pasero
+ * @author Mik Kersten
+ */
+public class NoticicationPopupMain {
+
+	public static void main(String[] args) {
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		shell.setLayout(new FillLayout());
+
+		shell.open();
+
+		final AbstractNotificationPopup popup = new SampleNotificationPopup(display);
+
+		shell.addShellListener(new ShellAdapter() {
+			public void shellClosed(ShellEvent e) {
+				popup.close();
+			}
+		});
+
+		popup.open();
+
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+}
---
+++ org.eclipse.mylyn.tests/src/org/eclipse/mylyn/tests/manual/SampleNotificationPopup.java
@@ -0,0 +1,60 @@
+/*
+ * Created on 14.11.2007
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+
+package org.eclipse.mylyn.tests.manual;
+
+import org.eclipse.mylyn.internal.tasks.ui.notifications.AbstractNotificationPopup;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+
+/**
+ * @author Benjamin Pasero
+ * @author Mik Kersten
+ */
+public class SampleNotificationPopup extends AbstractNotificationPopup {
+
+	public SampleNotificationPopup(Display display) {
+		super(display);
+	}
+
+	protected void createTitleArea(Composite parent) {
+		((GridData) parent.getLayoutData()).heightHint = 24;
+
+		Label titleCircleLabel = new Label(parent, SWT.NONE);
+		titleCircleLabel.setText("RSSOwl - Incoming News");
+		titleCircleLabel.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, true));
+		titleCircleLabel.setCursor(parent.getDisplay().getSystemCursor(SWT.CURSOR_HAND));
+
+		Label closeButton = new Label(parent, SWT.NONE);
+		closeButton.setText("Close");
+		closeButton.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_END));
+		closeButton.setCursor(parent.getDisplay().getSystemCursor(SWT.CURSOR_HAND));
+		closeButton.addMouseListener(new MouseAdapter() {
+			public void mouseUp(MouseEvent e) {
+				close();
+			}
+		});
+	}
+
+	protected void createContentArea(Composite parent) {
+		for (int i = 0; i < 5; i++) {
+			Label l = new Label(parent, SWT.None);
+			l.setText("News: " + i);
+			l.setBackground(parent.getBackground());
+		}
+	}
+
+	@Override
+	protected String getPopupShellTitle() {
+		return "Sample Notification";
+	}
+}
