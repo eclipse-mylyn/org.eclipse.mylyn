81b222d70c3e859d55a37d070277ac15803ef2cf
relves <>
2007-12-06 03:58:44 +0000

    Incomplete - task 232: manage Mylyn builds

--- org.eclipse.mylyn.releng/3.3/config/build.sh
+++ org.eclipse.mylyn.releng/3.3/config/build.sh
@@ -5,7 +5,7 @@
 MAJOR_VERSION=2.2.0
 
 # Qualifier format: IYYYYMMDD-HHMM
-QUALIFIER=I20071128-2200
+QUALIFIER=I20071205-2030
 
 # root of build tree
 BUILD_HOME=/home/releng/org.eclipse.mylyn/org.eclipse.mylyn.releng/3.3
