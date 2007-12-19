ef07de5a5cd8378de2396e0bb5f325ef5e71e135
mkersten <>
2007-12-19 00:56:04 +0000

    NEW - bug 206568: Improve performance and resource usage of bug reports with ~100 comments https://bugs.eclipse.org/bugs/show_bug.cgi?id=206568

---
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/TaskEditorBloatMonitor.java
@@ -0,0 +1,75 @@
+package org.eclipse.mylyn.internal.tasks.ui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.mylyn.monitor.ui.AbstractEditorTracker;
+import org.eclipse.mylyn.tasks.core.AbstractTask.RepositoryTaskSyncState;
+import org.eclipse.mylyn.tasks.ui.editors.TaskEditor;
+import org.eclipse.mylyn.tasks.ui.editors.TaskEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IEditorReference;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+
+/**
+ * @author Mik Kersten
+ */
+public class TaskEditorBloatMonitor extends AbstractEditorTracker {
+
+	private int MAX_EDITORS = 4;
+
+	@Override
+	protected void editorBroughtToTop(IEditorPart part) {
+		// ignore
+	}
+
+	@Override
+	public void editorOpened(IEditorPart editorPartOpened) {
+		IWorkbenchPage page = editorPartOpened.getSite().getPage();
+		List<IEditorReference> toClose = new ArrayList<IEditorReference>();
+		int totalTaskEditors = 0;
+		for (IEditorReference editorReference : page.getEditorReferences()) {
+			try {
+				if (editorReference.getEditorInput() instanceof TaskEditorInput) {
+					totalTaskEditors++;
+				}
+			} catch (PartInitException e) {
+				// ignore
+			}
+		}
+
+		if (totalTaskEditors > MAX_EDITORS) {
+			for (IEditorReference editorReference : page.getEditorReferences()) {
+				try {
+					if (editorReference.getEditorInput() instanceof TaskEditorInput) {
+						TaskEditorInput taskEditorInput = (TaskEditorInput) editorReference.getEditorInput();
+						TaskEditor taskEditor = (TaskEditor) editorReference.getEditor(false);
+						if (taskEditor == null) {
+							toClose.add(editorReference);
+						} else if (!taskEditor.equals(editorPartOpened)
+								&& !taskEditor.isDirty()
+								&& RepositoryTaskSyncState.SYNCHRONIZED.equals(taskEditorInput.getTask()
+										.getSynchronizationState())) {
+							toClose.add(editorReference);
+						}
+					}
+					if ((totalTaskEditors - toClose.size()) < MAX_EDITORS) {
+						break;
+					}
+				} catch (PartInitException e) {
+					// ignore
+				}
+			}
+		}
+
+		if (toClose.size() > 0) {
+			page.closeEditors(toClose.toArray(new IEditorReference[toClose.size()]), true);
+		}
+	}
+
+	@Override
+	public void editorClosed(IEditorPart editorPart) {
+		// ignore
+	}
+}
