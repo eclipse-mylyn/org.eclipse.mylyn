63afcf175a7e8f9cfed530d1b742f102ef2ab0c5
relves <>
2008-01-17 04:16:22 +0000

    Version Update

--- org.eclipse.mylyn.releng/3.4/config/build.sh
+++ org.eclipse.mylyn.releng/3.4/config/build.sh
@@ -5,7 +5,7 @@
 MAJOR_VERSION=2.3.0
 
 # Qualifier format: IYYYYMMDD-HHMM
-QUALIFIER=I20080111-1700
+QUALIFIER=I20080116-2030
 
 # root of build tree
 BUILD_HOME=/home/releng/org.eclipse.mylyn/org.eclipse.mylyn.releng/3.4
