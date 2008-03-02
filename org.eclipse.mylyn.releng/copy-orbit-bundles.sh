c2508fa5a52092eca0e3f427263edf0467c9ef1e
spingel <>
2008-03-02 05:51:08 +0000

    NEW - bug 214159: consume library dependencies from Orbit https://bugs.eclipse.org/bugs/show_bug.cgi?id=214159

---
+++ org.eclipse.mylyn.releng/copy-orbit-bundles.sh
@@ -0,0 +1,39 @@
+#!/bin/sh -e
+
+SRC=$1/plugins
+DEST=$1
+
+pack() {
+       DIR=$1
+       echo Creating jar for $DIR
+       (cd $DIR; zip -q ../$DIR.jar -r .)
+      }
+
+cd $SRC
+
+pack javax.xml.rpc*
+pack javax.xml.soap*
+pack org.apache.axis*
+pack org.apache.commons.discovery*
+
+mkdir -p $DEST/standardUpdateSite/plugins
+cp -v javax.xml.bind*.jar $DEST/standardUpdateSite/plugins
+cp -v org.apache.commons.codec*.jar $DEST/standardUpdateSite/plugins
+cp -v org.apache.commons.httpclient*.jar $DEST/standardUpdateSite/plugins
+cp -v org.apache.commons.lang*.jar $DEST/standardUpdateSite/plugins
+cp -v org.apache.commons.logging*.jar $DEST/standardUpdateSite/plugins
+cp -v org.apache.xmlrpc*.jar $DEST/standardUpdateSite/plugins
+cp -v org.apache.ws.commons.util*.jar $DEST/standardUpdateSite/plugins
+
+mkdir -p $DEST/extrasUpdateSite/plugins
+cp -v com.sun.syndication*.jar $DEST/extrasUpdateSite/plugins
+cp -v javax.wsdl*.jar $DEST/extrasUpdateSite/plugins
+cp -v javax.xml.rpc*.jar $DEST/extrasUpdateSite/plugins
+cp -v javax.xml.soap*.jar $DEST/extrasUpdateSite/plugins
+cp -v org.apache.axis*.jar $DEST/extrasUpdateSite/plugins
+cp -v org.apache.commons.discovery*.jar $DEST/extrasUpdateSite/plugins
+cp -v org.apache.commons.logging*.jar $DEST/extrasUpdateSite/plugins
+cp -v org.apache.ws.commons.util*.jar $DEST/extrasUpdateSite/plugins
+cp -v org.apache.xerces*.jar $DEST/extrasUpdateSite/plugins
+cp -v org.apache.xml.resolver*.jar $DEST/extrasUpdateSite/plugins
+cp -v org.jdom*.jar $DEST/extrasUpdateSite/plugins
