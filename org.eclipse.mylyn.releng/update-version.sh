c380e520c5277c1b15b73b4fcf76a258039021d5
spingel <>
2008-04-01 22:22:43 +0000

    NEW - bug 221939: manage branches https://bugs.eclipse.org/bugs/show_bug.cgi?id=221939

--- org.eclipse.mylyn.releng/update-version.sh
+++ org.eclipse.mylyn.releng/update-version.sh
@@ -2,14 +2,20 @@
 
 if [ $# -eq 0 ]
 then
- echo "usage: update-version.sh [new version]"
+ echo "usage: update-version.sh [old version] [new version]"
  exit 1
 fi
 
 
 ROOT=$(cd $(dirname $0); pwd)/..
-OLD_VERSION=`grep Bundle-Version $ROOT/org.eclipse.mylyn/META-INF/MANIFEST.MF | sed 's/Bundle-Version: //' | sed 's/.qualifier//'`
-NEW_VERSION=$1
+if [ $# -eq 0 ]
+then
+ OLD_VERSION=`grep Bundle-Version $ROOT/org.eclipse.mylyn/META-INF/MANIFEST.MF | sed 's/Bundle-Version: //' | sed 's/.qualifier//'`
+ NEW_VERSION=$1
+else
+ OLD_VERSION=$1
+ NEW_VERSION=$2
+fi
 
 echo renaming version from $OLD_VERSION to $NEW_VERSION
 
