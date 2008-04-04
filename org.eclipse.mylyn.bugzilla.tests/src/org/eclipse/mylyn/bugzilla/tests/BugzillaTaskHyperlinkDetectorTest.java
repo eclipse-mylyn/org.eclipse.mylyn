24e0c706d02a587d413c3fc8445187cf8fd7b977
spingel <>
2008-04-04 04:52:34 +0000

    NEW - bug 225413: make TaskHyperlinnkDetector work when no repository is defined on a view https://bugs.eclipse.org/bugs/show_bug.cgi?id=225413

--- org.eclipse.mylyn.bugzilla.tests/src/org/eclipse/mylyn/bugzilla/tests/BugzillaTaskHyperlinkDetectorTest.java
+++ org.eclipse.mylyn.bugzilla.tests/src/org/eclipse/mylyn/bugzilla/tests/BugzillaTaskHyperlinkDetectorTest.java
@@ -17,11 +17,15 @@ import org.eclipse.mylyn.internal.bugzilla.core.BugzillaCorePlugin;
 import org.eclipse.mylyn.internal.tasks.ui.editors.RepositoryTextViewer;
 import org.eclipse.mylyn.internal.tasks.ui.editors.TaskHyperlinkDetector;
 import org.eclipse.mylyn.tasks.core.TaskRepository;
+import org.eclipse.mylyn.tasks.core.TaskRepositoryManager;
+import org.eclipse.mylyn.tasks.ui.TaskHyperlink;
+import org.eclipse.mylyn.tasks.ui.TasksUiPlugin;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Shell;
 
 /**
  * @author Rob Elves
+ * @author Terry Hon
  */
 public class BugzillaTaskHyperlinkDetectorTest extends TestCase {
 
@@ -48,23 +52,47 @@ public class BugzillaTaskHyperlinkDetectorTest extends TestCase {
 	private String BUG_FORMAT_1_2 = "bug# 2";
 
 	//private BugzillaTaskHyperlinkDetector detector = new BugzillaTaskHyperlinkDetector();
-	private TaskHyperlinkDetector detector = new TaskHyperlinkDetector();
+	private TaskHyperlinkDetector detector;
 
-	private TaskRepository dummyRepository = new TaskRepository(BugzillaCorePlugin.REPOSITORY_KIND, "repository_url");
+	private TaskRepository repository1;
 
-	private RepositoryTextViewer viewer = new RepositoryTextViewer(dummyRepository, new Shell(), SWT.NONE);
+	private TaskRepository repository2;
 
-	private String[] formats = { TASK_FORMAT_1, TASK_FORMAT_2, TASK_FORMAT_3, TASK_FORMAT_4, BUG_FORMAT_1,
-			BUG_FORMAT_2, BUG_FORMAT_3, BUG_FORMAT_4 };
+	private RepositoryTextViewer viewer;
+
+	private String[] formats;
+
+	private TaskRepositoryManager repositoryManager;
+
+	private Shell shell;
 
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
+
+		detector = new TaskHyperlinkDetector();
+
+		repository1 = new TaskRepository(BugzillaCorePlugin.REPOSITORY_KIND, "repository_url1");
+		repository2 = new TaskRepository(BugzillaCorePlugin.REPOSITORY_KIND, "repository_url2");
+
+		shell = new Shell();
+		viewer = new RepositoryTextViewer(repository1, shell, SWT.NONE);
+
+		repositoryManager = TasksUiPlugin.getRepositoryManager();
+		repositoryManager.clearRepositories(TasksUiPlugin.getDefault().getRepositoriesFilePath());
+
+		formats = new String[] { TASK_FORMAT_1, TASK_FORMAT_2, TASK_FORMAT_3, TASK_FORMAT_4, BUG_FORMAT_1,
+				BUG_FORMAT_2, BUG_FORMAT_3, BUG_FORMAT_4 };
 	}
 
 	@Override
 	protected void tearDown() throws Exception {
 		super.tearDown();
+
+		detector.dispose();
+		repositoryManager.clearRepositories(TasksUiPlugin.getDefault().getRepositoriesFilePath());
+		
+		shell.dispose();
 	}
 
 	public void testBeginning() {
@@ -132,4 +160,88 @@ public class BugzillaTaskHyperlinkDetectorTest extends TestCase {
 		assertEquals(1, links.length);
 		assertEquals(testString.indexOf(DUPLICATE_NUMBER), links[0].getHyperlinkRegion().getOffset());
 	}
+
+	public void testNoRepositoryInViewNoRepositoryInManager() {
+		String testString = "bug 123";
+		viewer.setDocument(new Document(testString));
+		Region region = new Region(0, testString.length());
+		viewer.setRepository(null);
+
+		IHyperlink[] links = detector.detectHyperlinks(viewer, region, true);
+		assertNull(links);
+	}
+
+	public void testRepositoryInViewNoRepositoryInManager() {
+		String testString = "bug 123";
+		viewer.setDocument(new Document(testString));
+		Region region = new Region(0, testString.length());
+		viewer.setRepository(repository1);
+
+		IHyperlink[] links = detector.detectHyperlinks(viewer, region, true);
+		assertNotNull(links);
+		assertEquals(1, links.length);
+		assertTrue(links[0] instanceof TaskHyperlink);
+		assertEquals(((TaskHyperlink) links[0]).getRepository(), repository1);
+	}
+
+	public void testNoRepositoryInViewOneRepositoryInManager() {
+		String testString = "bug 123";
+		viewer.setDocument(new Document(testString));
+		Region region = new Region(0, testString.length());
+		viewer.setRepository(null);
+
+		repositoryManager.addRepository(repository1, TasksUiPlugin.getDefault().getRepositoriesFilePath());
+		IHyperlink[] links = detector.detectHyperlinks(viewer, region, true);
+		assertNotNull(links);
+		assertEquals(1, links.length);
+		assertTrue(links[0] instanceof TaskHyperlink);
+		assertEquals(((TaskHyperlink) links[0]).getRepository(), repository1);
+	}
+
+	public void testRepositoryInViewOneRepositoryInManager() {
+		String testString = "bug 123";
+		viewer.setDocument(new Document(testString));
+		Region region = new Region(0, testString.length());
+		viewer.setRepository(repository1);
+		repositoryManager.addRepository(repository1, TasksUiPlugin.getDefault().getRepositoriesFilePath());
+
+		IHyperlink[] links = detector.detectHyperlinks(viewer, region, true);
+		assertNotNull(links);
+		assertEquals(1, links.length);
+		assertTrue(links[0] instanceof TaskHyperlink);
+		assertEquals(((TaskHyperlink) links[0]).getRepository(), repository1);
+	}
+
+	public void testNoRepositoryInViewTwoRepositoryInManager() {
+		String testString = "bug 123";
+		viewer.setDocument(new Document(testString));
+		Region region = new Region(0, testString.length());
+		viewer.setRepository(null);
+		repositoryManager.addRepository(repository1, TasksUiPlugin.getDefault().getRepositoriesFilePath());
+		repositoryManager.addRepository(repository2, TasksUiPlugin.getDefault().getRepositoriesFilePath());
+
+		IHyperlink[] links = detector.detectHyperlinks(viewer, region, true);
+		assertNotNull(links);
+		assertEquals(2, links.length);
+		assertTrue(links[0] instanceof TaskHyperlink);
+		assertEquals(((TaskHyperlink) links[0]).getRepository(), repository1);
+		assertTrue(links[1] instanceof TaskHyperlink);
+		assertEquals(((TaskHyperlink) links[1]).getRepository(), repository2);
+	}
+
+	public void testRepositoryInViewTwoRepositoryInManager() {
+		String testString = "bug 123";
+		viewer.setDocument(new Document(testString));
+		Region region = new Region(0, testString.length());
+		viewer.setRepository(repository1);
+		repositoryManager.addRepository(repository1, TasksUiPlugin.getDefault().getRepositoriesFilePath());
+		repositoryManager.addRepository(repository2, TasksUiPlugin.getDefault().getRepositoriesFilePath());
+
+		IHyperlink[] links = detector.detectHyperlinks(viewer, region, true);
+		assertNotNull(links);
+		assertEquals(1, links.length);
+		assertTrue(links[0] instanceof TaskHyperlink);
+		assertEquals(((TaskHyperlink) links[0]).getRepository(), repository1);
+	}
+
 }
--- org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/editors/TaskHyperlinkDetector.java
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/editors/TaskHyperlinkDetector.java
@@ -8,6 +8,10 @@
 
 package org.eclipse.mylyn.internal.tasks.ui.editors;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.eclipse.core.resources.IResource;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
@@ -15,21 +19,21 @@ import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.jface.text.hyperlink.AbstractHyperlinkDetector;
 import org.eclipse.jface.text.hyperlink.IHyperlink;
-import org.eclipse.mylyn.tasks.core.AbstractRepositoryConnector;
 import org.eclipse.mylyn.tasks.core.TaskRepository;
-import org.eclipse.mylyn.tasks.core.TaskRepositoryManager;
 import org.eclipse.mylyn.tasks.ui.AbstractRepositoryConnectorUi;
 import org.eclipse.mylyn.tasks.ui.TasksUiPlugin;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 
 /**
  * @author Rob Elves
  * @author Steffen Pingel
  * @author Eugene Kuleshov
+ * @author Terry Hon
  */
 public class TaskHyperlinkDetector extends AbstractHyperlinkDetector {
 
@@ -56,21 +60,33 @@ public class TaskHyperlinkDetector extends AbstractHyperlinkDetector {
 			return null;
 		}
 
-		TaskRepository repository = getRepository(textViewer);
-		if (repository == null) {
-			repository = guessRepository(line);
-		}
-		if (repository == null) {
-			return null;
+		List<TaskRepository> repositories = new ArrayList<TaskRepository>();
+
+		TaskRepository selectedRepository = getRepository(textViewer);
+		if (selectedRepository != null) {
+			repositories.add(selectedRepository);
+		} else {
+			repositories.addAll(TasksUiPlugin.getRepositoryManager().getAllRepositories());
 		}
 
-		AbstractRepositoryConnectorUi connectorUi = TasksUiPlugin.getConnectorUi(repository.getConnectorKind());
-		if (connectorUi == null) {
-			return null;
+		List<IHyperlink> hyperlinks = new ArrayList<IHyperlink>();
+		for (TaskRepository repository : repositories) {
+			AbstractRepositoryConnectorUi connectorUi = TasksUiPlugin.getConnectorUi(repository.getConnectorKind());
+			if (connectorUi == null) {
+				continue;
+			}
+			IHyperlink[] links = (connectorUi.findHyperlinks(repository, line, //
+					region.getOffset() - lineInfo.getOffset(), lineInfo.getOffset()));
+			if (links == null) {
+				continue;
+			}
+			hyperlinks.addAll(Arrays.asList(links));
 		}
 
-		return connectorUi.findHyperlinks(repository, line, //
-				region.getOffset() - lineInfo.getOffset(), lineInfo.getOffset());
+		if (hyperlinks.isEmpty()) {
+			return null;
+		}
+		return hyperlinks.toArray(new IHyperlink[hyperlinks.size()]);
 	}
 
 	private TaskRepository getRepository(ITextViewer textViewer) {
@@ -87,31 +103,22 @@ public class TaskHyperlinkDetector extends AbstractHyperlinkDetector {
 			}
 
 			// use currently active editor (if any)
-			IWorkbenchPage activePage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-			IWorkbenchPart part = activePage.getActivePart();
-			if (part != null && part instanceof IEditorPart) {
-				IEditorInput input = ((IEditorPart) part).getEditorInput();
-				if (input != null) {
-					resource = (IResource) input.getAdapter(IResource.class);
+			IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+			if (window != null) {
+				IWorkbenchPage activePage = window.getActivePage();
+				if (activePage != null) {
+					IWorkbenchPart part = activePage.getActivePart();
+					if (part instanceof IEditorPart) {
+						IEditorInput input = ((IEditorPart) part).getEditorInput();
+						if (input != null) {
+							resource = (IResource) input.getAdapter(IResource.class);
+						}
+					}
 				}
 			}
 		}
 		if (resource != null) {
 			return TasksUiPlugin.getDefault().getRepositoryForResource(resource, true);
-		}
-		return null;
-	}
-
-	// TODO: extract similar code to OpenCorrespondingTaskAction#reconsile(ILinkedTaskInfo info)
-	private TaskRepository guessRepository(String text) {
-		TaskRepositoryManager manager = TasksUiPlugin.getRepositoryManager();
-		for (AbstractRepositoryConnector c : manager.getRepositoryConnectors()) {
-			for (TaskRepository repository : manager.getRepositories(c.getConnectorKind())) {
-				String[] ids = c.getTaskIdsFromComment(repository, text);
-				if (ids != null && ids.length > 0 && ids[0].length() > 0) {
-					return repository;
-				}
-			}
 		}
 		return null;
 	}
