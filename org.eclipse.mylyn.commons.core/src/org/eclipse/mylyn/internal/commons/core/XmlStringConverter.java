efa9cb234f4fdfe736f4828fbf4843dac8c01375
spingel <>
2008-04-29 23:50:55 +0000

    NEW - bug 217725: [api] review plug-in and feature dependencies https://bugs.eclipse.org/bugs/show_bug.cgi?id=217725

--- org.eclipse.mylyn.commons.core/src/org/eclipse/mylyn/internal/commons/core/XmlStringConverter.java
+++ org.eclipse.mylyn.commons.core/src/org/eclipse/mylyn/internal/commons/core/XmlStringConverter.java
@@ -105,4 +105,33 @@ public class XmlStringConverter {
 		return 0;
 	}
 
+	/**
+	 * @param text
+	 *            string to clean
+	 * @return string with all non valid characters removed, if text is null return null
+	 */
+	public static String cleanXmlString(String text) {
+		if (text == null) {
+			return null;
+		}
+		StringBuilder builder = new StringBuilder(text.length());
+		for (int x = 0; x < text.length(); x++) {
+			char ch = text.charAt(x);
+			if (isValid(ch)) {
+				builder.append(ch);
+			}
+		}
+		return builder.toString();
+	}
+
+	/**
+	 * Return true if character is a valid xml character
+	 * 
+	 * @see http://www.w3.org/TR/REC-xml/
+	 */
+	public static boolean isValid(char ch) {
+		return (0x0A == ch || 0x0D == ch || 0x09 == ch) || (ch >= 0x20 && ch <= 0xD7FF)
+				|| (ch >= 0xE000 && ch <= 0xFFFD) || (ch >= 0x10000 && ch <= 0x10FFFF);
+	}
+
 }
