5c3bb56a6aa08d19de4a573ddee25bd8420699f1
spingel <>
2008-06-06 04:09:19 +0000

    NEW - bug 227709: [e3.3] release weekly build for Mylyn 3.0 https://bugs.eclipse.org/bugs/show_bug.cgi?id=227709

--- org.eclipse.mylyn.releng/copy-orbit-bundles.sh
+++ org.eclipse.mylyn.releng/copy-orbit-bundles.sh
@@ -10,10 +10,10 @@ SRC=$1/plugins
 DEST=$1
 
 pack() {
-       DIR=$1
-       echo Creating jar for $DIR
-       (cd $DIR; zip -q ../$DIR.jar -r .)
-      }
+  DIR=$1
+  echo Creating jar for $DIR
+  (cd $DIR; zip -q ../$DIR.jar -r .)
+}
 
 cd $SRC
 
@@ -23,16 +23,14 @@ pack org.apache.axis*
 pack org.apache.commons.discovery*
 
 mkdir -p $DEST/standardUpdateSite/plugins
-cp -v javax.xml.bind*.jar $DEST/standardUpdateSite/plugins
+# httpclient (mylyn)
 cp -v org.apache.commons.codec*.jar $DEST/standardUpdateSite/plugins
 cp -v org.apache.commons.httpclient*.jar $DEST/standardUpdateSite/plugins
 cp -v org.apache.commons.lang*.jar $DEST/standardUpdateSite/plugins
 cp -v org.apache.commons.logging*.jar $DEST/standardUpdateSite/plugins
-cp -v org.apache.xmlrpc*.jar $DEST/standardUpdateSite/plugins
-cp -v org.apache.ws.commons.util*.jar $DEST/standardUpdateSite/plugins
 
 mkdir -p $DEST/extrasUpdateSite/plugins
-cp -v com.sun.syndication*.jar $DEST/extrasUpdateSite/plugins
+# axis (jira)
 cp -v javax.activation*.jar $DEST/extrasUpdateSite/plugins
 cp -v javax.mail*.jar $DEST/extrasUpdateSite/plugins
 cp -v javax.servlet*.jar $DEST/extrasUpdateSite/plugins
@@ -42,6 +40,24 @@ cp -v javax.xml.soap*.jar $DEST/extrasUpdateSite/plugins
 cp -v org.apache.axis*.jar $DEST/extrasUpdateSite/plugins
 cp -v org.apache.commons.discovery*.jar $DEST/extrasUpdateSite/plugins
 cp -v org.apache.commons.logging*.jar $DEST/extrasUpdateSite/plugins
+# xml-rpc (trac)
+cp -v javax.xml.bind*.jar $DEST/standardUpdateSite/plugins
+cp -v org.apache.xmlrpc*.jar $DEST/extrasUpdateSite/plugins
+cp -v org.apache.ws.commons.util*.jar $DEST/extrasUpdateSite/plugins
+
+mkdir -p $DEST/incubatorUpdateSite/plugins
+# axis (xplanner)
+cp -v javax.activation*.jar $DEST/incubatorUpdateSite/plugins
+cp -v javax.mail*.jar $DEST/incubatorUpdateSite/plugins
+cp -v javax.servlet*.jar $DEST/incubatorUpdateSite/plugins
+cp -v javax.wsdl*.jar $DEST/incubatorUpdateSite/plugins
+cp -v javax.xml.rpc*.jar $DEST/incubatorUpdateSite/plugins
+cp -v javax.xml.soap*.jar $DEST/incubatorUpdateSite/plugins
+cp -v org.apache.axis*.jar $DEST/incubatorUpdateSite/plugins
+cp -v org.apache.commons.discovery*.jar $DEST/incubatorUpdateSite/plugins
+cp -v org.apache.commons.logging*.jar $DEST/incubatorUpdateSite/plugins
+# rome (web templates)
+cp -v com.sun.syndication*.jar $DEST/extrasUpdateSite/plugins
 cp -v org.jdom*.jar $DEST/extrasUpdateSite/plugins
 
 mkdir -p $DEST/allUpdateSite/plugins
