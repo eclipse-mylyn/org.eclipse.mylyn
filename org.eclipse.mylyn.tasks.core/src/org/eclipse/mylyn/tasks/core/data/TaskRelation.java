8f6ec51863d6db9d5bcc17e2638e527bec325873
spingel <>
2008-06-30 21:26:21 +0000

    NEW - bug 238046: implement equals() and hashCode() for TaskRelation https://bugs.eclipse.org/bugs/show_bug.cgi?id=238046

--- org.eclipse.mylyn.tasks.core/src/org/eclipse/mylyn/tasks/core/data/TaskRelation.java
+++ org.eclipse.mylyn.tasks.core/src/org/eclipse/mylyn/tasks/core/data/TaskRelation.java
@@ -87,4 +87,28 @@ public class TaskRelation {
 		return new TaskRelation(Kind.DEPENDENCY, direction, taskId);
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + direction.hashCode();
+		result = prime * result + kind.hashCode();
+		result = prime * result + taskId.hashCode();
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		TaskRelation other = (TaskRelation) obj;
+		return direction.equals(other.direction) && kind.equals(other.kind) && taskId.equals(other.taskId);
+	}
 }
