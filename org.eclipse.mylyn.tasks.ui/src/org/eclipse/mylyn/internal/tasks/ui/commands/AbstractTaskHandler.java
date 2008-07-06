2e52ed143aba95e114f8dfa93a203a8c10cdbbeb
spingel <>
2008-07-06 03:03:37 +0000

    NEW - bug 211297: support mark read/unread on categories and queries https://bugs.eclipse.org/bugs/show_bug.cgi?id=211297

--- org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/commands/AbstractTaskHandler.java
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/commands/AbstractTaskHandler.java
@@ -13,7 +13,9 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.mylyn.internal.tasks.core.AbstractTask;
 import org.eclipse.mylyn.tasks.core.ITask;
+import org.eclipse.mylyn.tasks.core.ITaskContainer;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 /**
@@ -21,17 +23,35 @@ import org.eclipse.ui.handlers.HandlerUtil;
  */
 public abstract class AbstractTaskHandler extends AbstractHandler {
 
+	protected boolean recurse;
+
+	public AbstractTaskHandler() {
+	}
+
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		ISelection selection = HandlerUtil.getCurrentSelection(event);
 		if (selection instanceof IStructuredSelection) {
 			Object[] items = ((IStructuredSelection) selection).toArray();
 			for (Object item : items) {
-				if (item instanceof ITask) {
-					execute(event, (ITask) item);
-				}
+				process(event, item, recurse);
 			}
 		}
 		return null;
+	}
+
+	private void process(ExecutionEvent event, Object item, boolean recurse) throws ExecutionException {
+		if (item instanceof ITask) {
+			execute(event, (ITask) item);
+		}
+		if (item instanceof ITaskContainer && (recurse || !(item instanceof AbstractTask))) {
+			execute(event, (ITaskContainer) item);
+		}
+	}
+
+	protected void execute(ExecutionEvent event, ITaskContainer item) throws ExecutionException {
+		for (ITask task : item.getChildren()) {
+			process(event, task, true);
+		}
 	}
 
 	protected void execute(ExecutionEvent event, ITask task) throws ExecutionException {
