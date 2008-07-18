9d539d94046dd6ee1db5e67964d0278af5e8c24f
spingel <>
2008-07-18 00:55:09 +0000

    NEW - bug 221939: [releng] manage branches https://bugs.eclipse.org/bugs/show_bug.cgi?id=221939

---
+++ org.eclipse.mylyn.releng/add-mirrors.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -e
+
+ROOT=~/downloads/tools/mylyn/update
+
+echo "Updating site mirrors"
+sed -i -e 's/<site pack200=\"true\">/<site pack200=\"true\" mirrorsURL="http:\/\/www.eclipse.org\/downloads\/download.php?file=\/tools\/mylyn\/update\/e3.3\/site.xml\&amp;protocol=http\&amp;format=xml">/' $ROOT/e3.3/site.xml
+sed -i -e 's/<site pack200=\"true\">/<site pack200=\"true\" mirrorsURL="http:\/\/www.eclipse.org\/downloads\/download.php?file=\/tools\/mylyn\/update\/e3.4\/site.xml\&amp;protocol=http\&amp;format=xml">/' $ROOT/e3.4/site.xml
+sed -i -e 's/<site pack200=\"true\">/<site pack200=\"true\" mirrorsURL="http:\/\/www.eclipse.org\/downloads\/download.php?file=\/tools\/mylyn\/update\/extras\/site.xml\&amp;protocol=http\&amp;format=xml">/' $ROOT/extras/site.xml
+sed -i -e 's/<site pack200=\"true\">/<site pack200=\"true\" mirrorsURL="http:\/\/www.eclipse.org\/downloads\/download.php?file=\/tools\/mylyn\/update\/extras\/site.xml\&amp;protocol=http\&amp;format=xml">/' $ROOT/incubator/site.xml
+
+echo
+echo "Done"
