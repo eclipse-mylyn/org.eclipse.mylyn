30c6b81d58897947e5f16cfac31479769de9cd54
spingel <>
2008-08-05 23:28:34 +0000

    REOPENED - bug 243221: [build] build using org.eclipse.mylyn.releng/build.sh fails https://bugs.eclipse.org/bugs/show_bug.cgi?id=243221

--- org.eclipse.mylyn.releng/build-3.4.sh
+++ org.eclipse.mylyn.releng/build-3.4.sh
@@ -9,8 +9,8 @@ POSTFIX=e3x
 BUILD_HOME=$BUILD_ROOT/3.4/build
 
 mkdir -p $BUILD_HOME/maps
-cp $BUILD_ROOT/maps/mylyn_e3.4.map $BUILD_HOME/maps/
-cp $BUILD_ROOT/maps/orbit.map $BUILD_HOME/maps/
+cp $BUILD_ROOT/maps/* $BUILD_HOME/maps/
+rm $BUILD_ROOT/maps/mylyn_e3.3.map
 
 $JAVA_HOME/bin/java \
  -jar $ECLIPSE_HOME_3_4/plugins/org.eclipse.equinox.launcher_*.jar \
