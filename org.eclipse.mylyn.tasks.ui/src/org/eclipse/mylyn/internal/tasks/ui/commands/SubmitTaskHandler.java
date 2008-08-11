c4a1aceaea63d6152523b20e721fe4519346b8fb
spingel <>
2008-08-11 23:22:48 +0000

    RESOLVED - bug 238319: provide shortcut for submitting tasks https://bugs.eclipse.org/bugs/show_bug.cgi?id=238319

---
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/commands/SubmitTaskHandler.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Mylyn project committers and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.mylyn.internal.tasks.ui.commands;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.mylyn.tasks.ui.editors.AbstractTaskEditorPage;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchSite;
+import org.eclipse.ui.forms.editor.FormEditor;
+import org.eclipse.ui.forms.editor.IFormPage;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+/**
+ * @author Steffen Pingel
+ */
+public class SubmitTaskHandler extends AbstractHandler {
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchSite site = HandlerUtil.getActiveSite(event);
+		if (site instanceof IEditorSite) {
+			IWorkbenchPart part = ((IEditorSite) site).getPart();
+			if (part instanceof FormEditor) {
+				IFormPage page = ((FormEditor) part).getActivePageInstance();
+				if (page instanceof AbstractTaskEditorPage) {
+					((AbstractTaskEditorPage) page).doSubmit();
+				}
+			}
+		}
+		return null;
+	}
+
+}
