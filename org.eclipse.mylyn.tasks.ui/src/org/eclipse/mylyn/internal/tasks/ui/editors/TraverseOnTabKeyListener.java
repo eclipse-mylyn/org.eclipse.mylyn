3b528e5f4f0e0121de33b9a1c8526e003af61cfe
spingel <>
2008-08-12 02:14:40 +0000

    RESOLVED - bug 242451: tab navigation doesn't work in rich text attributes fields https://bugs.eclipse.org/bugs/show_bug.cgi?id=242451

---
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/editors/TraverseOnTabKeyListener.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Mylyn project committers and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.mylyn.internal.tasks.ui.editors;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.VerifyKeyListener;
+import org.eclipse.swt.events.VerifyEvent;
+
+class TraverseOnTabKeyListener implements VerifyKeyListener {
+
+	public void verifyKey(VerifyEvent event) {
+		// if there is a tab key, do not "execute" it and instead traverse to the next control
+		if (event.keyCode == SWT.TAB) {
+			event.doit = false;
+		}
+	}
+
+}
