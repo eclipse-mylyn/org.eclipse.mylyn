4055b55c028a891567f38d5bb980d18144bc0e52
spingel <>
2008-09-09 21:22:17 +0000

    Version update

--- org.eclipse.mylyn.releng/add-mirrors.sh
+++ org.eclipse.mylyn.releng/add-mirrors.sh
@@ -6,7 +6,7 @@ echo "Updating site mirrors"
 sed -i -e 's/<site pack200=\"true\">/<site pack200=\"true\" mirrorsURL="http:\/\/www.eclipse.org\/downloads\/download.php?file=\/tools\/mylyn\/update\/e3.3\/site.xml\&amp;protocol=http\&amp;format=xml">/' $ROOT/e3.3/site.xml
 sed -i -e 's/<site pack200=\"true\">/<site pack200=\"true\" mirrorsURL="http:\/\/www.eclipse.org\/downloads\/download.php?file=\/tools\/mylyn\/update\/e3.4\/site.xml\&amp;protocol=http\&amp;format=xml">/' $ROOT/e3.4/site.xml
 sed -i -e 's/<site pack200=\"true\">/<site pack200=\"true\" mirrorsURL="http:\/\/www.eclipse.org\/downloads\/download.php?file=\/tools\/mylyn\/update\/extras\/site.xml\&amp;protocol=http\&amp;format=xml">/' $ROOT/extras/site.xml
-sed -i -e 's/<site pack200=\"true\">/<site pack200=\"true\" mirrorsURL="http:\/\/www.eclipse.org\/downloads\/download.php?file=\/tools\/mylyn\/update\/extras\/site.xml\&amp;protocol=http\&amp;format=xml">/' $ROOT/incubator/site.xml
+sed -i -e 's/<site pack200=\"true\">/<site pack200=\"true\" mirrorsURL="http:\/\/www.eclipse.org\/downloads\/download.php?file=\/tools\/mylyn\/update\/incubator\/site.xml\&amp;protocol=http\&amp;format=xml">/' $ROOT/incubator/site.xml
 
 echo
 echo "Done"
