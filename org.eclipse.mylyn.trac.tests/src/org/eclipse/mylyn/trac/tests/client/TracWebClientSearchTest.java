03e1edb3a3931f8f8abee58300e984fe51051e54
spingel <>
2008-09-19 01:59:52 +0000

    NEW - bug 237552: [api] remove deprecated classes https://bugs.eclipse.org/bugs/show_bug.cgi?id=237552

--- org.eclipse.mylyn.trac.tests/src/org/eclipse/mylyn/trac/tests/client/TracWebClientSearchTest.java
+++ org.eclipse.mylyn.trac.tests/src/org/eclipse/mylyn/trac/tests/client/TracWebClientSearchTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (c) 2006, 2008 Steffen Pingel and others.
+ * Copyright (c) 2006, 2008 Steffen Pingel and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -44,7 +44,7 @@ public class TracWebClientSearchTest extends AbstractTracClientSearchTest {
 		search.setOrderBy("id");
 		List<TracTicket> result = new ArrayList<TracTicket>();
 		repository.search(search, result, null);
-		assertEquals(2, result.size());
+		assertEquals(1, result.size());
 		assertTicketEquals(tickets.get(7), result.get(0));
 	}
 
