aa3041074cb58a4f4edae24f3e83e165a2cd577e
spingel <>
2008-10-01 19:59:56 +0000

    NEW - bug 221939: [releng] manage branches https://bugs.eclipse.org/bugs/show_bug.cgi?id=221939

--- org.eclipse.mylyn.releng/run-tests-3.3.sh
+++ org.eclipse.mylyn.releng/run-tests-3.3.sh
@@ -5,17 +5,20 @@ BUILD_ROOT=$(cd $(dirname $0); pwd)
 
 source $BUILD_ROOT/local.sh
 
+echo "Installing Mylyn"
 cp 3.4/build/allUpdateSite/plugins/*jar $ECLIPSE_TEST_HOME_3_3/plugins/
 cp 3.3/build/standardUpdateSite/plugins/*jar $ECLIPSE_TEST_HOME_3_3/plugins/
 
+echo "Unpacking test plug-ins"
 rm -R $ECLIPSE_TEST_HOME_3_3/plugins/org.eclipse.mylyn.*tests
 for i in $ECLIPSE_TEST_HOME_3_3/plugins/org.eclipse.mylyn.*tests_0.0.0.jar; do
 	DIR=`echo $i | sed -e 's/_0.0.0.jar//'`
 	mkdir $DIR
-	unzip -o $i -d $DIR
+	unzip -o $i -d $DIR >> test.log
 	rm $i
 done
 
+echo "Starting Eclipse"
 $JAVA_HOME/bin/java \
  -jar $ECLIPSE_TEST_HOME_3_3/plugins/org.eclipse.equinox.launcher_*.jar \
  -clean \
@@ -23,6 +26,7 @@ $JAVA_HOME/bin/java \
  -file $ECLIPSE_TEST_HOME_3_3/plugins/org.eclipse.mylyn.tests/test.xml \
  -Dos=linux -Dws=gtk -Darch=x86 \
  -Declipse-home=$ECLIPSE_TEST_HOME_3_3 \
+ -Dlibrary-file=$ECLIPSE_TEST_HOME_3_3/plugins/org.eclipse.test_3.2.0/library.xml \
  "-Dvmargs=-Xms256M -Xmx256M" \
  "-DextraVMargs= \
     -ea \
--- org.eclipse.mylyn.releng/run-tests-3.4.sh
+++ org.eclipse.mylyn.releng/run-tests-3.4.sh
@@ -5,22 +5,37 @@ BUILD_ROOT=$(cd $(dirname $0); pwd)
 
 source $BUILD_ROOT/local.sh
 
-cp 3.4/build/allUpdateSite/plugins/*jar $ECLIPSE_TEST_HOME_3_4/plugins/
+rm -rf $ECLIPSE_TEST_HOME_3_4/dropins/mylyn
+mkdir $ECLIPSE_TEST_HOME_3_4/dropins/mylyn
 
-rm -R $ECLIPSE_TEST_HOME_3_4/plugins/org.eclipse.mylyn.*tests
-for i in $ECLIPSE_TEST_HOME_3_4/plugins/org.eclipse.mylyn.*tests_0.0.0.jar; do
-	DIR=`echo $i | sed -e 's/_0.0.0.jar//'`
+echo "Installing Mylyn"
+cp -a 3.4/build/allUpdateSite/features 3.4/build/allUpdateSite/plugins $ECLIPSE_TEST_HOME_3_4/dropins/mylyn
+
+echo "Unpacking features"
+for i in $ECLIPSE_TEST_HOME_3_4/dropins/mylyn/features/*.jar; do
+	DIR=`echo $i | sed -e 's/.jar//'`
 	mkdir $DIR
-	unzip -o $i -d $DIR
+	unzip -o $i -d $DIR >> test.log
 	rm $i
 done
 
+echo "Unpacking test plug-ins"
+# unpack test plug-ins for resource access
+for i in $ECLIPSE_TEST_HOME_3_4/dropins/mylyn/plugins/org.eclipse.mylyn.*tests_0.0.0.jar; do
+	DIR=`echo $i | sed -e 's/_0.0.0.jar//'`
+	mkdir $DIR
+	unzip -o $i -d $DIR >> test.log
+	rm $i
+done
+
+echo "Starting Eclipse to run tests"
 $JAVA_HOME/bin/java \
  -jar $ECLIPSE_TEST_HOME_3_4/plugins/org.eclipse.equinox.launcher_*.jar \
  -clean \
  -application org.eclipse.ant.core.antRunner \
- -file $ECLIPSE_TEST_HOME_3_4/plugins/org.eclipse.mylyn.tests/test.xml \
+ -file $ECLIPSE_TEST_HOME_3_4/dropins/mylyn/plugins/org.eclipse.mylyn.tests/test.xml \
  -Declipse-home=$ECLIPSE_TEST_HOME_3_4 \
+ -Dlibrary-file=$ECLIPSE_TEST_HOME_3_4/dropins/eclipse/plugins/org.eclipse.test_3.2.0/library.xml \
  "-DextraVMargs= \
     -ea \
     -Declipse.perf.dbloc=$BUILD_ROOT/derby \
