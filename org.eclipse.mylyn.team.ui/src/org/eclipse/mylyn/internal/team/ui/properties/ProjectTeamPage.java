3d4e085ae96a6315da94f3bafe91ae6c69e45c83
spingel <>
2008-10-19 20:46:13 +0000

    NEW - bug 251324: repository linking disabled when link provider fails https://bugs.eclipse.org/bugs/show_bug.cgi?id=251324

--- org.eclipse.mylyn.team.ui/src/org/eclipse/mylyn/internal/team/ui/properties/ProjectTeamPage.java
+++ org.eclipse.mylyn.team.ui/src/org/eclipse/mylyn/internal/team/ui/properties/ProjectTeamPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (c) 2004, 2008 Tasktop Technologies and others.
+ * Copyright (c) 2004, 2008 Tasktop Technologies and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -41,6 +41,8 @@ import org.eclipse.ui.forms.events.HyperlinkEvent;
 import org.eclipse.ui.forms.widgets.Hyperlink;
 
 /**
+ * A property page that support per project configuration of commit comment templates.
+ * 
  * @author Rob Elves
  * @author Steffen Pingel
  * @see Adapted from org.eclipse.ui.internal.ide.dialogs.ProjectReferencePage
