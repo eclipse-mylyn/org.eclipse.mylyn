d0e5f4c1eb3557e543a58a740b5ead9a8d55968e
spingel <>
2008-11-05 05:29:03 +0000

    244238: [releng] WikiText packages are only available for download via an eclipse update site https://bugs.eclipse.org/bugs/show_bug.cgi?id=244238

--- org.eclipse.mylyn.releng/upload.sh
+++ org.eclipse.mylyn.releng/upload.sh
@@ -27,7 +27,7 @@ pack() {
   cp -a $BUILD_ROOT/3.4/build/mylyn-$MAJOR_VERSION.$QUALIFIER-e3.4.zip $SITE
   cp -a $BUILD_ROOT/3.4/build/mylyn-$MAJOR_VERSION.$QUALIFIER-extras.zip $SITE
   cp -a $BUILD_ROOT/3.4/build/mylyn-$MAJOR_VERSION.$QUALIFIER-incubator.zip $SITE
-  cp -a $BUILD_ROOT/3.4/build/mylyn-wikitext-standalone-$MAJOR_VERSION.$QUALIFIER_incubation.zip $SITE
+  cp -a $BUILD_ROOT/3.4/build/mylyn-wikitext-standalone-$MAJOR_VERSION.${QUALIFIER}_incubation.zip $SITE
   cp -a $BUILD_ROOT/3.4/build/standardUpdateSite $SITE/e3.4
   cp -a $BUILD_ROOT/3.4/build/extrasUpdateSite $SITE/extras
   cp -a $BUILD_ROOT/3.4/build/incubatorUpdateSite $SITE/incubator
