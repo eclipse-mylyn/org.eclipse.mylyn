6d2f49710f1d5da104776bfb3f05e36dce89bac6
spingel <>
2008-11-11 20:57:56 +0000

    REOPENED - bug 251177: hyperlink detection does not work when in preview mode https://bugs.eclipse.org/bugs/show_bug.cgi?id=251177

--- org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/editors/RepositoryTextViewerConfiguration.java
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/editors/RepositoryTextViewerConfiguration.java
@@ -13,7 +13,6 @@
 package org.eclipse.mylyn.internal.tasks.ui.editors;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 import org.eclipse.core.runtime.Assert;
@@ -21,14 +20,10 @@ import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jface.preference.JFacePreferences;
 import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.TextAttribute;
-import org.eclipse.jface.text.TextPresentation;
 import org.eclipse.jface.text.contentassist.ContentAssistant;
 import org.eclipse.jface.text.contentassist.IContentAssistant;
 import org.eclipse.jface.text.hyperlink.AbstractHyperlinkDetector;
-import org.eclipse.jface.text.hyperlink.DefaultHyperlinkPresenter;
-import org.eclipse.jface.text.hyperlink.IHyperlink;
 import org.eclipse.jface.text.hyperlink.IHyperlinkDetector;
 import org.eclipse.jface.text.hyperlink.IHyperlinkPresenter;
 import org.eclipse.jface.text.presentation.IPresentationReconciler;
@@ -43,14 +38,10 @@ import org.eclipse.jface.text.rules.SingleLineRule;
 import org.eclipse.jface.text.rules.Token;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.mylyn.internal.provisional.commons.ui.CommonColors;
-import org.eclipse.mylyn.internal.tasks.core.TaskList;
-import org.eclipse.mylyn.internal.tasks.ui.TasksUiPlugin;
-import org.eclipse.mylyn.tasks.core.ITask;
 import org.eclipse.mylyn.tasks.core.TaskRepository;
-import org.eclipse.mylyn.tasks.ui.TaskHyperlink;
+import org.eclipse.mylyn.tasks.ui.TaskHyperlinkPresenter;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.StyleRange;
-import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.RGB;
 import org.eclipse.ui.editors.text.EditorsUI;
 import org.eclipse.ui.editors.text.TextSourceViewerConfiguration;
 
@@ -173,8 +164,10 @@ public class RepositoryTextViewerConfiguration extends TextSourceViewerConfigura
 
 	@Override
 	public IHyperlinkPresenter getHyperlinkPresenter(final ISourceViewer sourceViewer) {
-		return new RepositoryTextViewerHyperlinkPresenter(JFaceResources.getColorRegistry().get(
-				JFacePreferences.ACTIVE_HYPERLINK_COLOR), sourceViewer);
+		if (fPreferenceStore == null) {
+			return new TaskHyperlinkPresenter(new RGB(0, 0, 255));
+		}
+		return new TaskHyperlinkPresenter(fPreferenceStore);
 	}
 
 	@Override
@@ -189,81 +182,6 @@ public class RepositoryTextViewerConfiguration extends TextSourceViewerConfigura
 		} else {
 			return null;
 		}
-	}
-
-	private final class RepositoryTextViewerHyperlinkPresenter extends DefaultHyperlinkPresenter {
-
-		private final ISourceViewer sourceViewer;
-
-		private IRegion activeRegion;
-
-		/**
-		 * Stores which task a tooltip is being displayed for. It is used to avoid having the same tooltip being set
-		 * multiple times while you move the mouse over a task hyperlink (bug#209409)
-		 */
-		private ITask currentTaskHyperlink;
-
-		private RepositoryTextViewerHyperlinkPresenter(Color color, ISourceViewer sourceViewer) {
-			super(color);
-			this.sourceViewer = sourceViewer;
-		}
-
-		@SuppressWarnings("unchecked")
-		@Override
-		public void applyTextPresentation(TextPresentation textPresentation) {
-			super.applyTextPresentation(textPresentation);
-			if (activeRegion != null && currentTaskHyperlink != null && currentTaskHyperlink.isCompleted()) {
-				Iterator<StyleRange> styleRangeIterator = textPresentation.getAllStyleRangeIterator();
-				while (styleRangeIterator.hasNext()) {
-					StyleRange styleRange = styleRangeIterator.next();
-					if (activeRegion.getOffset() == styleRange.start && activeRegion.getLength() == styleRange.length) {
-						styleRange.strikeout = true;
-						break;
-					}
-				}
-			}
-		}
-
-		@Override
-		public void showHyperlinks(IHyperlink[] hyperlinks) {
-			activeRegion = null;
-			if (hyperlinks != null && hyperlinks.length > 0 && hyperlinks[0] instanceof TaskHyperlink) {
-				TaskHyperlink hyperlink = (TaskHyperlink) hyperlinks[0];
-
-				TaskList taskList = TasksUiPlugin.getTaskList();
-				String repositoryUrl = hyperlink.getRepository().getRepositoryUrl();
-
-				ITask task = taskList.getTask(repositoryUrl, hyperlink.getTaskId());
-				if (task == null) {
-					task = taskList.getTaskByKey(repositoryUrl, hyperlink.getTaskId());
-				}
-
-				if (task != null && task != currentTaskHyperlink) {
-					currentTaskHyperlink = task;
-					activeRegion = hyperlink.getHyperlinkRegion();
-					if (sourceViewer.getTextWidget() != null && !sourceViewer.getTextWidget().isDisposed()) {
-						if (task.getTaskKey() == null) {
-							sourceViewer.getTextWidget().setToolTipText(task.getSummary());
-						} else {
-							sourceViewer.getTextWidget().setToolTipText(task.getTaskKey() + ": " + task.getSummary());
-						}
-					}
-				}
-			}
-			super.showHyperlinks(hyperlinks);
-		}
-
-		@Override
-		public void hideHyperlinks() {
-			if (currentTaskHyperlink != null) {
-				if (sourceViewer.getTextWidget() != null && !sourceViewer.getTextWidget().isDisposed()) {
-					sourceViewer.getTextWidget().setToolTipText(null);
-				}
-				currentTaskHyperlink = null;
-			}
-			super.hideHyperlinks();
-		}
-
 	}
 
 	private static class RepositoryTextScanner extends RuleBasedScanner {
---
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/tasks/ui/TaskHyperlinkPresenter.java
@@ -0,0 +1,173 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2008 Tasktop Technologies and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tasktop Technologies - initial API and implementation
+ *     Frank Becker - improvements
+ *******************************************************************************/
+
+package org.eclipse.mylyn.tasks.ui;
+
+import java.lang.reflect.Constructor;
+import java.util.Iterator;
+
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.text.TextPresentation;
+import org.eclipse.jface.text.hyperlink.DefaultHyperlinkPresenter;
+import org.eclipse.jface.text.hyperlink.IHyperlink;
+import org.eclipse.jface.text.hyperlink.IHyperlinkPresenter;
+import org.eclipse.mylyn.internal.tasks.core.TaskList;
+import org.eclipse.mylyn.internal.tasks.ui.TasksUiPlugin;
+import org.eclipse.mylyn.tasks.core.ITask;
+import org.eclipse.swt.custom.StyleRange;
+import org.eclipse.swt.graphics.RGB;
+
+/**
+ * A hyperlink presenter that displays a tooltip when hovering over a {@link TaskHyperlink}.
+ * 
+ * @author Steffen Pingel
+ * @author Frank Becker
+ * @since 3.1
+ */
+public final class TaskHyperlinkPresenter extends DefaultHyperlinkPresenter {
+
+	private IRegion activeRegion;
+
+	/**
+	 * Stores which task a tooltip is being displayed for. It is used to avoid having the same tooltip being set
+	 * multiple times while you move the mouse over a task hyperlink (bug#209409)
+	 */
+	private ITask currentTaskHyperlink;
+
+	private ITextViewer textViewer;
+
+	private IHyperlinkPresenter multiplePresenter;
+
+	/**
+	 * @see DefaultHyperlinkPresenter#DefaultHyperlinkPresenter(IPreferenceStore)
+	 */
+	public TaskHyperlinkPresenter(IPreferenceStore store) {
+		super(store);
+		initMultipleHyperlinkSupport(IPreferenceStore.class, store);
+	}
+
+	/**
+	 * @see DefaultHyperlinkPresenter#DefaultHyperlinkPresenter(RGB)
+	 */
+	public TaskHyperlinkPresenter(RGB color) {
+		super(color);
+		initMultipleHyperlinkSupport(RGB.class, color);
+	}
+
+	@Override
+	public boolean canShowMultipleHyperlinks() {
+		return multiplePresenter != null;
+	}
+
+	@Override
+	public boolean canHideHyperlinks() {
+		return true;
+	}
+
+	@SuppressWarnings("unchecked")
+	private <T> void initMultipleHyperlinkSupport(Class<T> argClass, T arg) {
+		try {
+			Class<IHyperlinkPresenter> clazz = (Class<IHyperlinkPresenter>) Class.forName("org.eclipse.jface.text.hyperlink.MultipleHyperlinkPresenter");
+			Constructor<IHyperlinkPresenter> constructor = clazz.getDeclaredConstructor(argClass);
+			multiplePresenter = constructor.newInstance(arg);
+		} catch (Throwable t) {
+			// ignore
+		}
+	}
+
+	@Override
+	public void install(ITextViewer textViewer) {
+		this.textViewer = textViewer;
+		super.install(textViewer);
+		if (multiplePresenter != null) {
+			multiplePresenter.install(textViewer);
+		}
+	}
+
+	@Override
+	public void uninstall() {
+		hideHyperlinks();
+		this.textViewer = null;
+		super.uninstall();
+		if (multiplePresenter != null) {
+			multiplePresenter.uninstall();
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void applyTextPresentation(TextPresentation textPresentation) {
+		super.applyTextPresentation(textPresentation);
+		// decorate hyperlink as strike-through if task is completed, this is now also handled by TaskHyperlinkTextPresentationManager
+		if (activeRegion != null && currentTaskHyperlink != null && currentTaskHyperlink.isCompleted()) {
+			Iterator<StyleRange> styleRangeIterator = textPresentation.getAllStyleRangeIterator();
+			while (styleRangeIterator.hasNext()) {
+				StyleRange styleRange = styleRangeIterator.next();
+				if (activeRegion.getOffset() == styleRange.start && activeRegion.getLength() == styleRange.length) {
+					styleRange.strikeout = true;
+					break;
+				}
+			}
+		}
+	}
+
+	@Override
+	public void showHyperlinks(IHyperlink[] hyperlinks) {
+		if (hyperlinks.length > 1 && multiplePresenter != null) {
+			multiplePresenter.showHyperlinks(hyperlinks);
+		} else {
+			activeRegion = null;
+			if (hyperlinks.length > 0 && hyperlinks[0] instanceof TaskHyperlink) {
+				TaskHyperlink hyperlink = (TaskHyperlink) hyperlinks[0];
+
+				TaskList taskList = TasksUiPlugin.getTaskList();
+				String repositoryUrl = hyperlink.getRepository().getRepositoryUrl();
+
+				ITask task = taskList.getTask(repositoryUrl, hyperlink.getTaskId());
+				if (task == null) {
+					task = taskList.getTaskByKey(repositoryUrl, hyperlink.getTaskId());
+				}
+
+				if (task != null && task != currentTaskHyperlink) {
+					currentTaskHyperlink = task;
+					activeRegion = hyperlink.getHyperlinkRegion();
+					if (textViewer != null && textViewer.getTextWidget() != null
+							&& !textViewer.getTextWidget().isDisposed()) {
+						if (task.getTaskKey() == null) {
+							textViewer.getTextWidget().setToolTipText(task.getSummary());
+						} else {
+							textViewer.getTextWidget().setToolTipText(task.getTaskKey() + ": " + task.getSummary());
+						}
+					}
+				}
+			}
+			super.showHyperlinks(hyperlinks);
+		}
+	}
+
+	@Override
+	public void hideHyperlinks() {
+		if (currentTaskHyperlink != null) {
+			if (textViewer != null && textViewer.getTextWidget() != null && !textViewer.getTextWidget().isDisposed()) {
+				textViewer.getTextWidget().setToolTipText(null);
+			}
+			currentTaskHyperlink = null;
+		}
+		if (multiplePresenter != null) {
+			multiplePresenter.hideHyperlinks();
+		}
+		super.hideHyperlinks();
+	}
+
+}
