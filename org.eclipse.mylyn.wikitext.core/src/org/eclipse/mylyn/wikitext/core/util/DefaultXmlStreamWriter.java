06aec3546bc09671c616552e6ad859076a0a5543
dgreen <dgreen>
2008-11-12 13:31:29 +0000

    Fix license header, should have been EPL

--- org.eclipse.mylyn.wikitext.core/src/org/eclipse/mylyn/wikitext/core/util/DefaultXmlStreamWriter.java
+++ org.eclipse.mylyn.wikitext.core/src/org/eclipse/mylyn/wikitext/core/util/DefaultXmlStreamWriter.java
@@ -1,7 +1,5 @@
 /*******************************************************************************
  * Copyright (c) 2007, 2008 David Green and others.
- * Portions Copyright 2001-2004 The Apache Software Foundation.
- * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,21 +8,6 @@
  * Contributors:
  *     David Green - initial API and implementation
  *******************************************************************************/
-/*
- * Copyright 2001-2004 The Apache Software Foundation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.eclipse.mylyn.wikitext.core.util;
 
 import java.io.IOException;
