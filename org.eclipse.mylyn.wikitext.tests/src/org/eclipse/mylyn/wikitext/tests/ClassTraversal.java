539c04dc8e7ce3c39819582d2d6294f4dee88ff7
dgreen <dgreen>
2008-11-19 14:28:53 +0000

    NEW - bug 255568: [releng] support running tests from build scripts https://bugs.eclipse.org/bugs/show_bug.cgi?id=255568
    add infrastructure for finding/running tests based on the environment in which they are run.

---
+++ org.eclipse.mylyn.wikitext.tests/src/org/eclipse/mylyn/wikitext/tests/ClassTraversal.java
@@ -0,0 +1,122 @@
+/*******************************************************************************
+ * Copyright (c) 2007, 2008 David Green and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     David Green - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.mylyn.wikitext.tests;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.util.Enumeration;
+
+import org.eclipse.mylyn.wikitext.core.WikiTextPlugin;
+import org.osgi.framework.Bundle;
+
+/**
+ * A utility for visiting Mylyn classes available on the classpath.
+ * 
+ * @author dgreen
+ */
+public class ClassTraversal {
+	public void visitClasses(Visitor visitor) {
+		visitClasses(ClassTraversal.class, visitor);
+	}
+
+	private void visitClasses(Class<ClassTraversal> classOnClasspath, Visitor visitor) {
+		ClassLoader loader = classOnClasspath.getClassLoader();
+		String resourceOfClass = classOnClasspath.getCanonicalName().replace('.', '/') + ".class";
+		Enumeration<URL> resources;
+		try {
+			resources = loader.getResources(resourceOfClass);
+		} catch (IOException e) {
+			throw new IllegalStateException(e);
+		}
+		while (resources.hasMoreElements()) {
+			URL url = resources.nextElement();
+			String protocol = url.getProtocol();
+			if (protocol.equals("file")) {
+				String file = url.getFile();
+				try {
+					file = URLDecoder.decode(file, "utf-8").substring(0, file.indexOf(resourceOfClass));
+				} catch (UnsupportedEncodingException e) {
+					throw new IllegalStateException(e);
+				}
+				visitClasses(loader, new File(file), new File(file), visitor);
+
+			} else if (protocol.equals("bundleresource")) {
+				long bundleId = Long.parseLong(url.getHost());
+				Bundle bundle = WikiTextPlugin.getDefault().getBundle().getBundleContext().getBundle(bundleId);
+				if (bundle == null) {
+					throw new IllegalStateException("Cannot get bundle " + bundleId);
+				}
+
+				String path = url.getFile();
+				path = path.substring(0, path.indexOf(resourceOfClass));
+
+				visitClasses(bundle, path, visitor);
+			} else {
+				throw new IllegalStateException("Unimplemented protocol: " + protocol);
+			}
+		}
+	}
+
+	private void visitClasses(ClassLoader loader, File root, File file, Visitor visitor) {
+		File[] files = file.listFiles();
+		if (files != null) {
+			for (File child : files) {
+				if (child.isDirectory()) {
+					visitClasses(loader, root, child, visitor);
+				} else {
+					String path = child.getPath();
+					if (path.endsWith(".class")) {
+						String fqn = path.substring(root.getPath().length() + 1, path.length() - ".class".length());
+						fqn = fqn.replace('/', '.').replace('\\', '.');
+						Class<?> clazz;
+						try {
+							clazz = Class.forName(fqn, true, loader);
+						} catch (Exception e) {
+							// can't load the class, so skip it.
+							continue;
+						}
+						visitor.visit(clazz);
+					}
+				}
+			}
+		}
+	}
+
+	private void visitClasses(Bundle bundle, String path, Visitor visitor) {
+		Enumeration<URL> entries = bundle.findEntries(path, "*.class", true);
+		while (entries.hasMoreElements()) {
+			URL element = entries.nextElement();
+			String filePath = element.getFile();
+			if (filePath.indexOf("org/eclipse/mylyn") != -1) {
+				filePath = filePath.substring(filePath.indexOf("org/eclipse/mylyn"));
+			} else {
+				continue;
+			}
+			String fqn = filePath.substring(0, filePath.length() - ".class".length()).replace('/', '.');
+			Class<?> clazz;
+			try {
+				clazz = bundle.loadClass(fqn);
+			} catch (Exception e) {
+				// can't laod the class, so skip it.
+				continue;
+			}
+			visitor.visit(clazz);
+		}
+	}
+
+	public interface Visitor {
+		public void visit(Class<?> clazz);
+	}
+}
---
+++ org.eclipse.mylyn.wikitext.tests/src/org/eclipse/mylyn/wikitext/tests/HeadlessTests.java
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ * Copyright (c) 2007, 2008 David Green and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     David Green - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.mylyn.wikitext.tests;
+
+import java.lang.reflect.Modifier;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.eclipse.mylyn.wikitext.core.WikiTextPlugin;
+import org.eclipse.mylyn.wikitext.tests.ClassTraversal.Visitor;
+
+/**
+ * A test case that runs all headless tests that are available on the classpath
+ * 
+ * @author dgreen
+ */
+public class HeadlessTests extends TestCase {
+
+	public static Test suite() {
+		final TestSuite testSuite = new TestSuite();
+		testSuite.setName("All Headless Tests");
+
+		new ClassTraversal().visitClasses(new Visitor() {
+			public void visit(Class<?> clazz) {
+				if (isQualifyingTestClass(clazz)) {
+					addTest(testSuite, clazz);
+				}
+			}
+		});
+
+		return testSuite;
+	}
+
+	protected static void addTest(TestSuite testSuite, Class<?> clazz) {
+		testSuite.addTest(new TestSuite(clazz));
+	}
+
+	protected static boolean isQualifyingTestClass(Class<?> clazz) {
+		if (HeadlessTests.class == clazz) {
+			return false;
+		}
+		if (!TestCase.class.isAssignableFrom(clazz)) {
+			return false;
+		}
+		int modifiers = clazz.getModifiers();
+		if (Modifier.isAbstract(modifiers)) {
+			return false;
+		}
+		if (!Modifier.isPublic(modifiers)) {
+			return false;
+		}
+		if (clazz.getAnnotation(HeadRequired.class) != null) {
+			return false;
+		}
+
+		final boolean inEclipse = WikiTextPlugin.getDefault() != null;
+
+		if (!inEclipse && clazz.getAnnotation(EclipseRuntimeRequired.class) != null) {
+			return false;
+		}
+
+		return true;
+	}
+
+}
