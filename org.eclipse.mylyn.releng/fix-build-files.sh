bc0ca6401bf05e5ef9ad0c2703471fd314b6fd43
spingel <>
2008-11-28 23:55:40 +0000

    NEW - bug 255568: [releng] support running tests from build scripts https://bugs.eclipse.org/bugs/show_bug.cgi?id=255568

--- org.eclipse.mylyn.releng/fix-build-files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -e
-
-if [ $# -eq 0 ]
-then
- echo "usage: fix-build-files.sh path"
- exit 1
-fi
-
-sed -i~ -e 's/<antcall target=\"refresh\"\/>//' $1/features/org.eclipse.mylyn*/build.xml
