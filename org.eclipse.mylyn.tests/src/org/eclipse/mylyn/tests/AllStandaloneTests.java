c5a89a774c1efeef31277e7ac3a32fcb31b29ab4
dgreen <>
2008-12-05 01:47:45 +0000

    NEW - bug 255568: [releng] support running tests from build scripts https://bugs.eclipse.org/bugs/show_bug.cgi?id=255568
    Add stand-alone tests

--- org.eclipse.mylyn.tests/src/org/eclipse/mylyn/tests/AllStandaloneTests.java
+++ org.eclipse.mylyn.tests/src/org/eclipse/mylyn/tests/AllStandaloneTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (c) 2004, 2008 Tasktop Technologies and others.
+ * Copyright (c) 2004, 2008 Tasktop Technologies and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -32,6 +32,9 @@ public class AllStandaloneTests {
 		// XXX: refactor
 		// suite.addTestSuite(TaskListStandaloneTest.class);
 		// $JUnit-END$
+
+		suite.addTest(org.eclipse.mylyn.wikitext.tests.HeadlessStandaloneTests.suite());
+
 		return suite;
 	}
 }
