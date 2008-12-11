a726dcc3c7790d2e072802e36f62dd9b0684be2a
spingel <>
2008-12-11 22:46:38 +0000

    NEW - bug 258571: [api] provide utility method for accessing platform proxy https://bugs.eclipse.org/bugs/show_bug.cgi?id=258571

--- org.eclipse.mylyn.tasks.core/src/org/eclipse/mylyn/internal/tasks/core/TaskRepositoryLocation.java
+++ org.eclipse.mylyn.tasks.core/src/org/eclipse/mylyn/internal/tasks/core/TaskRepositoryLocation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (c) 2004, 2008 Tasktop Technologies and others.
+ * Copyright (c) 2004, 2008 Tasktop Technologies and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,17 +11,15 @@
 
 package org.eclipse.mylyn.internal.tasks.core;
 
-import java.net.InetSocketAddress;
 import java.net.Proxy;
-import java.net.Proxy.Type;
 
-import org.eclipse.core.net.proxy.IProxyData;
-import org.eclipse.core.net.proxy.IProxyService;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.mylyn.commons.core.StatusHandler;
 import org.eclipse.mylyn.commons.net.AbstractWebLocation;
 import org.eclipse.mylyn.commons.net.AuthenticationCredentials;
 import org.eclipse.mylyn.commons.net.AuthenticationType;
-import org.eclipse.mylyn.internal.commons.net.AuthenticatedProxy;
-import org.eclipse.mylyn.internal.commons.net.CommonsNetPlugin;
+import org.eclipse.mylyn.commons.net.WebUtil;
+import org.eclipse.mylyn.tasks.core.RepositoryStatus;
 import org.eclipse.mylyn.tasks.core.TaskRepository;
 
 /**
@@ -33,7 +31,6 @@ public class TaskRepositoryLocation extends AbstractWebLocation {
 
 	public TaskRepositoryLocation(TaskRepository taskRepository) {
 		super(taskRepository.getRepositoryUrl());
-
 		this.taskRepository = taskRepository;
 	}
 
@@ -42,55 +39,19 @@ public class TaskRepositoryLocation extends AbstractWebLocation {
 		if (!taskRepository.isDefaultProxyEnabled()) {
 			String proxyHost = taskRepository.getProperty(TaskRepository.PROXY_HOSTNAME);
 			String proxyPort = taskRepository.getProperty(TaskRepository.PROXY_PORT);
-			String proxyUsername = ""; //$NON-NLS-1$
-			String proxyPassword = ""; //$NON-NLS-1$
-			AuthenticationCredentials credentials = taskRepository.getCredentials(AuthenticationType.PROXY);
-			if (proxyHost != null && proxyHost.length() > 0 && credentials != null) {
-				proxyUsername = credentials.getUserName();
-				proxyPassword = credentials.getPassword();
-			}
-			return getProxy(proxyHost, proxyPort, proxyUsername, proxyPassword);
-		}
-
-		IProxyService service = CommonsNetPlugin.getProxyService();
-		if (service != null && service.isProxiesEnabled()) {
-			IProxyData data = service.getProxyDataForHost(host, proxyType);
-			if (data != null && data.getHost() != null) {
-				String proxyHost = data.getHost();
-				int proxyPort = data.getPort();
-				// change the IProxyData default port to the Java default port
-				if (proxyPort == -1) {
-					proxyPort = 0;
-				}
-
-				InetSocketAddress sockAddr = new InetSocketAddress(proxyHost, proxyPort);
-				if (data.isRequiresAuthentication()) {
-					return new AuthenticatedProxy(getJavaProxyType(proxyType), sockAddr, data.getUserId(),
-							data.getPassword());
-				} else {
-					return new Proxy(getJavaProxyType(proxyType), sockAddr);
+			if (proxyPort != null) {
+				try {
+					int proxyPortNum = Integer.parseInt(proxyPort);
+					AuthenticationCredentials credentials = taskRepository.getCredentials(AuthenticationType.PROXY);
+					return WebUtil.createProxy(proxyHost, proxyPortNum, credentials);
+				} catch (NumberFormatException e) {
+					StatusHandler.log(new RepositoryStatus(taskRepository, IStatus.ERROR,
+							ITasksCoreConstants.ID_PLUGIN, 0, "Error occured while configuring proxy. Invalid port \"" //$NON-NLS-1$
+									+ proxyPort + "\" specified.", e)); //$NON-NLS-1$
 				}
 			}
 		}
-		return null;
-	}
-
-	private static Proxy getProxy(String proxyHost, String proxyPort, String proxyUsername, String proxyPassword) {
-		boolean authenticated = (proxyUsername != null && proxyPassword != null && proxyUsername.length() > 0 && proxyPassword.length() > 0);
-		if (proxyHost != null && proxyHost.length() > 0 && proxyPort != null && proxyPort.length() > 0) {
-			int proxyPortNum = Integer.parseInt(proxyPort);
-			InetSocketAddress sockAddr = new InetSocketAddress(proxyHost, proxyPortNum);
-			if (authenticated) {
-				return new AuthenticatedProxy(Type.HTTP, sockAddr, proxyUsername, proxyPassword);
-			} else {
-				return new Proxy(Type.HTTP, sockAddr);
-			}
-		}
-		return Proxy.NO_PROXY;
-	}
-
-	private Type getJavaProxyType(String type) {
-		return (IProxyData.SOCKS_PROXY_TYPE.equals(type)) ? Proxy.Type.SOCKS : Proxy.Type.HTTP;
+		return WebUtil.getProxy(host, proxyType);
 	}
 
 	@Override
