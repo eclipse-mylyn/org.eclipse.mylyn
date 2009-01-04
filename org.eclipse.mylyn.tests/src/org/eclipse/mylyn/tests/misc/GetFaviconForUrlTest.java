6822c68c0ca832388cdd65d175dc7d3eeab91a34
spingel <>
2009-01-04 22:32:27 +0000

    NEW - bug 259919: fix failing tests https://bugs.eclipse.org/bugs/show_bug.cgi?id=259919

--- org.eclipse.mylyn.tests/src/org/eclipse/mylyn/tests/misc/GetFaviconForUrlTest.java
+++ org.eclipse.mylyn.tests/src/org/eclipse/mylyn/tests/misc/GetFaviconForUrlTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (c) 2004, 2008 Tasktop Technologies and others.
+ * Copyright (c) 2004, 2008 Tasktop Technologies and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -51,6 +51,6 @@ public class GetFaviconForUrlTest extends TestCase {
 	}
 
 	public void testNoFavicon() throws MalformedURLException {
-		assertNull(WebUiUtil.getFaviconForUrl("http://help.eclipse.org/help32/index.jsp"));
+		assertNull(WebUiUtil.getFaviconForUrl("http://mylyn.eclipse.org/invalid"));
 	}
 }
