656a80c98c488ff13f46c754688d18022472a356
spingel <>
2009-02-19 18:30:34 +0000

    REOPENED - bug 226598: [patch] move "Validate Settings" button to the lower-left of the dialog https://bugs.eclipse.org/bugs/show_bug.cgi?id=226598

--- org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/tasks/ui/wizards/TaskRepositoryWizardDialog.java
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/tasks/ui/wizards/TaskRepositoryWizardDialog.java
@@ -11,7 +11,11 @@
 
 package org.eclipse.mylyn.tasks.ui.wizards;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+
 import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.wizard.IWizard;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.mylyn.internal.provisional.commons.ui.CommonImages;
@@ -33,6 +37,8 @@ import org.eclipse.swt.widgets.Shell;
  * @since 3.1
  */
 public class TaskRepositoryWizardDialog extends WizardDialog {
+
+	private static final String VALIDATE_BUTTON_KEY = "validate"; //$NON-NLS-1$
 
 	private Button validateServerButton;
 
@@ -118,4 +124,50 @@ public class TaskRepositoryWizardDialog extends WizardDialog {
 		}
 	}
 
+	/**
+	 * Overridden to be able to set proper state for our validate button
+	 */
+	@Override
+	public void run(boolean fork, boolean cancelable, IRunnableWithProgress runnable) throws InvocationTargetException,
+			InterruptedException {
+
+		HashMap<String, Boolean> savedEnabledState = null;
+		try {
+			savedEnabledState = saveAndSetEnabledStateMylyn();
+			super.run(fork, cancelable, runnable);
+		} finally {
+			if (savedEnabledState != null) {
+				restoreEnabledStateMylyn(savedEnabledState);
+			}
+		}
+	}
+
+	/**
+	 * Modeled after super.saveAndSetEnabledState(), but that one is private, so create our own
+	 */
+	private HashMap<String, Boolean> saveAndSetEnabledStateMylyn() {
+		HashMap<String, Boolean> savedEnabledState = null;
+		if (getShell() != null) {
+			savedEnabledState = new HashMap<String, Boolean>();
+			if (validateServerButton != null && getShell().getDisplay().getFocusControl() == validateServerButton
+					&& validateServerButton.getShell() == getShell()) {
+				savedEnabledState.put(VALIDATE_BUTTON_KEY, validateServerButton.getEnabled());
+				validateServerButton.setEnabled(false);
+			}
+		}
+		return savedEnabledState;
+	}
+
+	/**
+	 * Modeled after super.restoreEnabledState() and super.restoreUIState() -- couldn't override those since they are
+	 * private, so create our own. Currently only single button to work with, so don't create two separate methods
+	 */
+	private void restoreEnabledStateMylyn(HashMap<String, Boolean> savedEnabledState) {
+		if (savedEnabledState != null) {
+			Boolean savedValidateEnabledState = savedEnabledState.get(VALIDATE_BUTTON_KEY);
+			if (validateServerButton != null && savedValidateEnabledState != null) {
+				validateServerButton.setEnabled(savedValidateEnabledState);
+			}
+		}
+	}
 }
