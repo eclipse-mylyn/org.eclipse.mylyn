470b580d6eb4d00d70a30e2458d4e7afc9baec81
spingel <>
2009-03-03 18:17:11 +0000

    NEW - bug 257972: fix standalone tests https://bugs.eclipse.org/bugs/show_bug.cgi?id=257972

--- org.eclipse.mylyn.commons.tests/src/org/eclipse/mylyn/commons/tests/net/SslProtocolSocketFactoryTest.java
+++ org.eclipse.mylyn.commons.tests/src/org/eclipse/mylyn/commons/tests/net/SslProtocolSocketFactoryTest.java
@@ -12,39 +12,61 @@
 package org.eclipse.mylyn.commons.tests.net;
 
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 
 import junit.framework.TestCase;
 
 import org.apache.commons.httpclient.params.HttpConnectionParams;
+import org.eclipse.mylyn.commons.tests.support.TestProxy;
 import org.eclipse.mylyn.internal.commons.net.SslProtocolSocketFactory;
 
+/**
+ * @author Steffen Pingel
+ */
 public class SslProtocolSocketFactoryTest extends TestCase {
+
+	private TestProxy testProxy;
+
+	private InetSocketAddress proxyAddress;
+
+	@Override
+	protected void setUp() throws Exception {
+		testProxy = new TestProxy();
+		int proxyPort = testProxy.startAndWait();
+		assert proxyPort > 0;
+		proxyAddress = new InetSocketAddress("localhost", proxyPort);
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		testProxy.stop();
+	}
 
 	public void testTrustAllSslProtocolSocketFactory() throws Exception {
 		SslProtocolSocketFactory factory = SslProtocolSocketFactory.getInstance();
 		Socket s;
 
-		s = factory.createSocket("mylyn.eclipse.org", 80);
+		s = factory.createSocket(proxyAddress.getHostName(), proxyAddress.getPort());
 		assertNotNull(s);
 		assertTrue(s.isConnected());
 		s.close();
 
 		InetAddress anyHost = new Socket().getLocalAddress();
 
-		s = factory.createSocket("mylyn.eclipse.org", 80, anyHost, 0);
+		s = factory.createSocket(proxyAddress.getHostName(), proxyAddress.getPort(), anyHost, 0);
 		assertNotNull(s);
 		assertTrue(s.isConnected());
 		s.close();
 
 		HttpConnectionParams params = new HttpConnectionParams();
-		s = factory.createSocket("mylyn.eclipse.org", 80, anyHost, 0, params);
+		s = factory.createSocket(proxyAddress.getHostName(), proxyAddress.getPort(), anyHost, 0, params);
 		assertNotNull(s);
 		assertTrue(s.isConnected());
 		s.close();
 
 		params.setConnectionTimeout(1000);
-		s = factory.createSocket("mylyn.eclipse.org", 80, anyHost, 0, params);
+		s = factory.createSocket(proxyAddress.getHostName(), proxyAddress.getPort(), anyHost, 0, params);
 		assertNotNull(s);
 		assertTrue(s.isConnected());
 		s.close();
