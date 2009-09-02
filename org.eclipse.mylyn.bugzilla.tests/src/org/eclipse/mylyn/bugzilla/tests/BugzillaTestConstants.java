2fc474589d61d10150a9f3b5b0a425f868bddada
spingel <>
2009-09-02 19:44:01 +0000

    RESOLVED - bug 286589: [releng] setup mylyn.eclipse.org https://bugs.eclipse.org/bugs/show_bug.cgi?id=286589

--- org.eclipse.mylyn.bugzilla.tests/src/org/eclipse/mylyn/bugzilla/tests/BugzillaTestConstants.java
+++ org.eclipse.mylyn.bugzilla.tests/src/org/eclipse/mylyn/bugzilla/tests/BugzillaTestConstants.java
@@ -1,12 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2009 Tasktop Technologies and others.
+ * Copyright (c) 2004, 2009 Frank Becker and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     Tasktop Technologies - initial API and implementation
+ *     Frank Becker - initial API and implementation
+ *     Tasktop Technologies - improvements
  *******************************************************************************/
 
 package org.eclipse.mylyn.bugzilla.tests;
@@ -16,8 +17,9 @@ package org.eclipse.mylyn.bugzilla.tests;
  */
 public class BugzillaTestConstants {
 
-	/* For now test on 3.0 as earliest bugzilla. Also bugs30 should always be the latest 3.0.x release */
-	public static final String TEST_BUGZILLA_30_URL = getServerUrl("bugs30"); //$NON-NLS-1$
+	public static final String SERVER = System.getProperty("mylyn.bugzilla.server", "mylyn.eclipse.org");
+
+	public static final String TEST_BUGZILLA_30_URL = getServerUrl("bugs30");
 
 	public static final String TEST_BUGZILLA_218_URL = getServerUrl("bugs218");
 
@@ -35,12 +37,12 @@ public class BugzillaTestConstants {
 
 	public static final String TEST_BUGZILLA_323_URL = getServerUrl("bugs323");
 
-	public static final String TEST_BUGZILLA_34_URL = getServerUrl("bugs34"); //$NON-NLS-1$
+	public static final String TEST_BUGZILLA_34_URL = getServerUrl("bugs34");
 
 	public static final String TEST_BUGZILLA_LATEST_URL = TEST_BUGZILLA_34_URL;
 
 	private static final String getServerUrl(String version) {
-		String url = System.getProperty("bugzilla.server.url" + version, null); //$NON-NLS-1$
-		return (url != null) ? url : System.getProperty("bugzilla.server.url", "http://mylyn.eclipse.org/" + version); //$NON-NLS-1$
+		return "http://" + SERVER + "/" + version;
 	}
+
 }
