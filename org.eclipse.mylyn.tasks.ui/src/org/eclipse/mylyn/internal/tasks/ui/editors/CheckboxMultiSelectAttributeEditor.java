c4fab6b3bbcd6b07f7222501676d39d663cce95a
spingel <>
2009-09-11 20:58:21 +0000

    NEW - bug 287832: [patch] show comma separated list of versions in Fix For and Affects https://bugs.eclipse.org/bugs/show_bug.cgi?id=287832

--- org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/editors/CheckboxMultiSelectAttributeEditor.java
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/editors/CheckboxMultiSelectAttributeEditor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2009 Tasktop Technologies and others.
+ * Copyright (c) 2004, 2009 Tasktop Technologies and others. 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,12 +7,16 @@
  *
  * Contributors:
  *     Tasktop Technologies - initial API and implementation
+ *     Pawel Niewiadomski - fix for bug 287832
  *******************************************************************************/
 package org.eclipse.mylyn.internal.tasks.ui.editors;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.eclipse.jface.window.Window;
 import org.eclipse.mylyn.internal.provisional.commons.ui.CheckBoxTreeDialog;
@@ -25,9 +29,10 @@ import org.eclipse.mylyn.tasks.ui.editors.LayoutHint.ColumnSpan;
 import org.eclipse.mylyn.tasks.ui.editors.LayoutHint.RowSpan;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -35,6 +40,8 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.ui.forms.widgets.Section;
+import org.eclipse.ui.forms.widgets.SharedScrolledComposite;
 
 /**
  * @author Shawn Minto
@@ -43,6 +50,8 @@ public class CheckboxMultiSelectAttributeEditor extends AbstractAttributeEditor 
 
 	private Text valueText;
 
+	private Composite parent;
+
 	public CheckboxMultiSelectAttributeEditor(TaskDataModel manager, TaskAttribute taskAttribute) {
 		super(manager, taskAttribute);
 		setLayoutHint(new LayoutHint(RowSpan.SINGLE, ColumnSpan.MULTIPLE));
@@ -50,12 +59,14 @@ public class CheckboxMultiSelectAttributeEditor extends AbstractAttributeEditor 
 
 	@Override
 	public void createControl(Composite parent, FormToolkit toolkit) {
+		this.parent = parent;
+
 		Composite composite = toolkit.createComposite(parent);
 		GridLayout layout = new GridLayout(2, false);
 		layout.marginWidth = 1;
 		composite.setLayout(layout);
 
-		valueText = toolkit.createText(composite, "", SWT.FLAT); //$NON-NLS-1$
+		valueText = toolkit.createText(composite, "", SWT.FLAT | SWT.WRAP); //$NON-NLS-1$
 		valueText.setFont(EditorUtil.TEXT_FONT);
 
 		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
@@ -65,34 +76,25 @@ public class CheckboxMultiSelectAttributeEditor extends AbstractAttributeEditor 
 				SWT.FLAT);
 		gd = new GridData();
 		changeValueButton.setLayoutData(gd);
-		changeValueButton.addSelectionListener(new SelectionListener() {
-
-			public void widgetDefaultSelected(SelectionEvent e) {
-			}
-
+		changeValueButton.addSelectionListener(new SelectionAdapter() {
+			@Override
 			public void widgetSelected(SelectionEvent e) {
-
 				List<String> values = getValues();
-
 				Map<String, String> validValues = getAttributeMapper().getOptions(getTaskAttribute());
 
-				Shell shell = WorkbenchUtil.getShell();
-
-				CheckBoxTreeDialog selectionDialog = new CheckBoxTreeDialog(shell, values, validValues, NLS.bind(
-						Messages.CheckboxMultiSelectAttributeEditor_Select_X, getLabel()));
+				CheckBoxTreeDialog selectionDialog = new CheckBoxTreeDialog(WorkbenchUtil.getShell(), values,
+						validValues, NLS.bind(Messages.CheckboxMultiSelectAttributeEditor_Select_X, getLabel()));
 				int responseCode = selectionDialog.open();
 
-				List<String> newValues = selectionDialog.getSelectedValues();
-				if (responseCode == Window.OK && values != null) {
-					setValues(newValues);
-					attributeChanged();
-					updateText();
-				} else {
-					return;
+				if (responseCode == Window.OK) {
+					Set<String> newValues = selectionDialog.getSelectedValues();
+					if (!new HashSet<String>(values).equals(newValues)) {
+						setValues(new ArrayList<String>(newValues));
+						attributeChanged();
+						updateText();
+					}
 				}
-
 			}
-
 		});
 		toolkit.adapt(valueText, false, false);
 		updateText();
@@ -111,6 +113,48 @@ public class CheckboxMultiSelectAttributeEditor extends AbstractAttributeEditor 
 				}
 			}
 			valueText.setText(valueString.toString());
+			if (valueText != null && parent != null && parent.getParent() != null
+					&& parent.getParent().getParent() != null) {
+				Point size = valueText.getSize();
+				// subtract 1 from size for border
+				Point newSize = valueText.computeSize(size.x - 1, SWT.DEFAULT);
+				if (!newSize.equals(valueText.getSize())) {
+					reflow();
+				}
+			}
+		}
+	}
+
+	/**
+	 * Update scroll bars of the enclosing form.
+	 * 
+	 * @see Section#reflow()
+	 */
+	private void reflow() {
+		Composite c = parent;
+		while (c != null) {
+			c.setRedraw(false);
+			c = c.getParent();
+			if (c instanceof SharedScrolledComposite || c instanceof Shell) {
+				break;
+			}
+		}
+		c = parent;
+		while (c != null) {
+			c.layout(true);
+			c = c.getParent();
+			if (c instanceof SharedScrolledComposite) {
+				((SharedScrolledComposite) c).reflow(true);
+				break;
+			}
+		}
+		c = parent;
+		while (c != null) {
+			c.setRedraw(true);
+			c = c.getParent();
+			if (c instanceof SharedScrolledComposite || c instanceof Shell) {
+				break;
+			}
 		}
 	}
 
