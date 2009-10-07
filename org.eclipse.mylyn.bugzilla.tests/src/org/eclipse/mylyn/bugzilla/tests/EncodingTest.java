db224c85af109e0da18f7f2d54768b507c61bb87
relves <>
2009-10-07 18:13:06 +0000

    NEW - bug 290465: fix Bugzilla test suite https://bugs.eclipse.org/bugs/show_bug.cgi?id=290465

--- org.eclipse.mylyn.bugzilla.tests/src/org/eclipse/mylyn/bugzilla/tests/EncodingTest.java
+++ org.eclipse.mylyn.bugzilla.tests/src/org/eclipse/mylyn/bugzilla/tests/EncodingTest.java
@@ -11,13 +11,25 @@
 
 package org.eclipse.mylyn.bugzilla.tests;
 
-import org.eclipse.core.runtime.CoreException;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.mylyn.bugzilla.tests.support.BugzillaFixture;
+import org.eclipse.mylyn.commons.net.AuthenticationType;
+import org.eclipse.mylyn.commons.net.WebLocation;
 import org.eclipse.mylyn.internal.bugzilla.core.BugzillaAttribute;
 import org.eclipse.mylyn.internal.bugzilla.core.BugzillaClient;
+import org.eclipse.mylyn.internal.tasks.ui.TasksUiPlugin;
+import org.eclipse.mylyn.internal.tasks.ui.util.TasksUiInternal;
 import org.eclipse.mylyn.tasks.core.ITask;
 import org.eclipse.mylyn.tasks.core.data.TaskAttribute;
+import org.eclipse.mylyn.tasks.core.data.TaskData;
 import org.eclipse.mylyn.tasks.core.data.TaskDataModel;
+import org.eclipse.mylyn.tasks.ui.TasksUi;
+import org.eclipse.mylyn.tests.util.TestUtil;
+import org.eclipse.mylyn.tests.util.TestUtil.Credentials;
+import org.eclipse.mylyn.tests.util.TestUtil.PrivilegeLevel;
 
 /**
  * @author Mik Kersten
@@ -40,30 +52,32 @@ public class EncodingTest extends AbstractBugzillaTest {
 	}
 
 	/**
-	 * This test just shows that when the encoding is changed on the repository synchronization does in fact return in a
-	 * different encoding (though it may not be legible)
+	 * This test just shows that when the encoding is changed on the repository, synchronization does in fact return in
+	 * a different encoding (though it may not be legible)
 	 */
-	public void testDifferentReportEncoding() throws CoreException {
-		init222();
-		repository.setCharacterEncoding("UTF-8");
-		ITask task = generateLocalTaskAndDownload("57");
-		assertNotNull(task);
-		assertTrue(task.getSummary().equals("\u00E6"));//"\u05D0"));
-		taskList.deleteTask(task);
-		connector.getClientManager().repositoryRemoved(repository);
-		repository.setCharacterEncoding("ISO-8859-1");
-		task = generateLocalTaskAndDownload("57");
-		assertNotNull(task);
+	public void testDifferentReportEncoding() throws Exception {
+		TaskData data = BugzillaFixture.current().createTask(PrivilegeLevel.USER, "\u00E6", null);
+		assertNotNull(data);
+		assertTrue(data.getRoot().getMappedAttribute(TaskAttribute.SUMMARY).getValue().equals("\u00E6"));//"\u05D0"));
+
+		WebLocation location = new WebLocation(repository.getRepositoryUrl());
+		Credentials credentials = TestUtil.readCredentials(PrivilegeLevel.USER);
+		location.setCredentials(AuthenticationType.REPOSITORY, credentials.username, credentials.password);
+		BugzillaClient client2 = BugzillaFixture.current().client(location, "ISO-8859-1");
+		data = BugzillaFixture.current().getTask(data.getTaskId(), client2);
+		assertNotNull(data);
 		// iso-8859-1 'incorrect' interpretation
-		assertFalse(task.getSummary().equals("\u00E6"));//"\u05D0"));
+		assertFalse(data.getRoot().getMappedAttribute(TaskAttribute.SUMMARY).getValue().equals("\u00E6"));//"\u05D0"));
 	}
 
-	public void testProperEncodingUponPost() throws CoreException {
-		init222();
-		repository.setCharacterEncoding("UTF-8");
-		ITask task = generateLocalTaskAndDownload("57");
-		assertNotNull(task);
-		assertTrue(task.getSummary().equals("\u00E6"));//"\u05D0"));
+	public void testProperEncodingUponPost() throws Exception {
+		TaskData data = BugzillaFixture.current().createTask(PrivilegeLevel.USER, "\u00E6", null);
+		assertNotNull(data);
+		ITask task = TasksUi.getRepositoryModel().createTask(repository, data.getTaskId());
+		TasksUiPlugin.getTaskList().addTask(task);
+		Set<ITask> tasks = new HashSet<ITask>();
+		tasks.add(task);
+		TasksUiInternal.synchronizeTasks(connector, tasks, true, null);
 		String priority = null;
 		TaskDataModel model = createModel(task);
 		if (task.getPriority().equals("P1")) {
@@ -92,9 +106,8 @@ public class EncodingTest extends AbstractBugzillaTest {
 		model.save(new NullProgressMonitor());
 
 		submit(model);
-		taskList.deleteTask(task);
-		task = generateLocalTaskAndDownload("57");
-		assertNotNull(task);
-		assertTrue(task.getSummary().equals("\u00E6"));//"\u05D0"));
+		data = BugzillaFixture.current().getTask(data.getTaskId(), client);
+		assertNotNull(data);
+		assertTrue(data.getRoot().getMappedAttribute(TaskAttribute.SUMMARY).getValue().equals("\u00E6"));//"\u05D0"));
 	}
 }
