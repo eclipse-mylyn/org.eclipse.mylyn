9021e876054b25f367cb7302f149a6d9a113f5ad
spingel <>
2009-10-27 15:24:27 +0000

    RESOLVED - bug 291146: create New & Noteworthy for Mylyn 3.3 https://bugs.eclipse.org/bugs/show_bug.cgi?id=291146

--- org.eclipse.mylyn.releng/bin/promote.sh
+++ org.eclipse.mylyn.releng/bin/promote.sh
@@ -23,7 +23,7 @@ ROOT=~/downloads/tools/mylyn/update
 
 # backup old release 
 
-OLD_VERSION=`grep mylyn_feature.*version $ROOT/e3.3/site.xml | sed 's/.*\"\([^\"]*\)\"./\1/' | sed 's/-e33//' | head`
+OLD_VERSION=`grep mylyn_feature.*version $ROOT/e3.4/site.xml | sed 's/.*\"\([^\"]*\)\"./\1/' | sed 's/-e33//' | head`
 OLD_VERSION_SHORT=`echo $OLD_VERSION | sed 's/\(.*\)\..*/\1/'`
 
 if [ "$OLD_VERSION" == "" ] || [ "$OLD_VERSION_SHORT" == "" ]
@@ -36,7 +36,7 @@ BACKUP=$ROOT/$OLD_VERSION-DELETE
 echo "Archiving old version to $BACKUP"
 
 mkdir -p $BACKUP
-mv $ROOT/e3.3 $BACKUP
+#mv $ROOT/e3.3 $BACKUP
 mv $ROOT/e3.4 $BACKUP
 mv $ROOT/extras $BACKUP
 mv $ROOT/incubator $BACKUP
@@ -50,7 +50,7 @@ echo
 echo "Promoting $MAJOR.$BUILD"
 
 WEEKLY=$ROOT-archive/$MAJOR/$BUILD
-cp -a $WEEKLY/e3.3 $ROOT
+#cp -a $WEEKLY/e3.3 $ROOT
 cp -a $WEEKLY/e3.4 $ROOT
 cp -a $WEEKLY/extras $ROOT
 cp -a $WEEKLY/incubator $ROOT
@@ -60,22 +60,22 @@ NEW_VERSION_SHORT=$MAJOR
 
 echo "Updating site archives"
 
-cp $WEEKLY/mylyn-$NEW_VERSION*-e3.3.zip $ROOT/mylyn-$NEW_VERSION_SHORT-e3.3.zip 
+#cp $WEEKLY/mylyn-$NEW_VERSION*-e3.3.zip $ROOT/mylyn-$NEW_VERSION_SHORT-e3.3.zip 
 cp $WEEKLY/mylyn-$NEW_VERSION*-e3.4.zip $ROOT/mylyn-$NEW_VERSION_SHORT-e3.4.zip 
 cp $WEEKLY/mylyn-$NEW_VERSION*-extras.zip $ROOT/mylyn-$NEW_VERSION_SHORT-extras.zip
 cp $WEEKLY/mylyn-$NEW_VERSION*-incubator.zip $ROOT/mylyn-$NEW_VERSION_SHORT-incubator.zip  
 
 echo "Updating mirror ulrs"
-sed -i -e 's/http:\/\/download.eclipse.org\/tools\/mylyn\/update\/weekly\//http:\/\/download.eclipse.org\/tools\/mylyn\/update\//' $ROOT/e3.3/site.xml
+#sed -i -e 's/http:\/\/download.eclipse.org\/tools\/mylyn\/update\/weekly\//http:\/\/download.eclipse.org\/tools\/mylyn\/update\//' $ROOT/e3.3/site.xml
 sed -i -e 's/http:\/\/download.eclipse.org\/tools\/mylyn\/update\/weekly\//http:\/\/download.eclipse.org\/tools\/mylyn\/update\//' $ROOT/e3.4/site.xml
 sed -i -e 's/http:\/\/download.eclipse.org\/tools\/mylyn\/update\/weekly\//http:\/\/download.eclipse.org\/tools\/mylyn\/update\//' $ROOT/extras/site.xml
 sed -i -e 's/http:\/\/download.eclipse.org\/tools\/mylyn\/update\/weekly\//http:\/\/download.eclipse.org\/tools\/mylyn\/update\//' $ROOT/incubator/site.xml
 
 echo "Done"
 
-echo
-echo $ROOT/e3.3
-ls $ROOT/e3.3
+#echo
+#echo $ROOT/e3.3
+#ls $ROOT/e3.3
 echo
 echo $ROOT/e3.4
 ls $ROOT/e3.4
