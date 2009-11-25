f5ab8c6369fbd9348145fbac67b852b122c7672a
spingel <>
2009-11-25 22:51:36 +0000

    NEW - bug 296197: java compatibility checker fails on J9 https://bugs.eclipse.org/bugs/show_bug.cgi?id=296197

--- org.eclipse.mylyn.tests/src/org/eclipse/mylyn/tests/integration/JavaRuntimeVersionCheckerTest.java
+++ org.eclipse.mylyn.tests/src/org/eclipse/mylyn/tests/integration/JavaRuntimeVersionCheckerTest.java
@@ -20,11 +20,12 @@ import org.eclipse.mylyn.internal.compatibility.JavaRuntimeVersionChecker;
  */
 public class JavaRuntimeVersionCheckerTest extends TestCase {
 
-	public void testGetJavaVersion() {
+	public void testParseVersion() {
 		assertEquals(1.0f, JavaRuntimeVersionChecker.parseVersion("1.0"));
 		assertEquals(1.0f, JavaRuntimeVersionChecker.parseVersion("1.0.3_abc"));
 		assertEquals(1.6f, JavaRuntimeVersionChecker.parseVersion("1.6"));
 		assertEquals(1.6f, JavaRuntimeVersionChecker.parseVersion("1.6."));
+		assertEquals(1.6f, JavaRuntimeVersionChecker.parseVersion("1.6.0"));
 		assertEquals(1.6f, JavaRuntimeVersionChecker.parseVersion("1.6.0_14-b08"));
 		assertEquals(1.5f, JavaRuntimeVersionChecker.parseVersion("1.5.0_16"));
 		assertEquals(1.5f, JavaRuntimeVersionChecker.parseVersion("1.5.0.07-_20_mar_2007_05_31"));
@@ -33,5 +34,30 @@ public class JavaRuntimeVersionCheckerTest extends TestCase {
 		assertEquals(0.0f, JavaRuntimeVersionChecker.parseVersion("1.-5"));
 		assertEquals(0.0f, JavaRuntimeVersionChecker.parseVersion("1.5_14"));
 		assertEquals(0.0f, JavaRuntimeVersionChecker.parseVersion("abc"));
+		assertEquals(0.0f, JavaRuntimeVersionChecker.parseVersion("jvmxp3260-20080816_22093"));
 	}
+
+	public void testIsJavaVersionSmallerThanDefault() {
+		assertFalse(JavaRuntimeVersionChecker.isJavaVersionSmallerThan(1.0f));
+		assertFalse(JavaRuntimeVersionChecker.isJavaVersionSmallerThan(1.5f));
+		assertTrue(JavaRuntimeVersionChecker.isJavaVersionSmallerThan(10.0f));
+	}
+
+	public void testIsJavaVersionSmallerThan() {
+		String runtimeVersion = System.getProperty("java.runtime.version");
+		String version = System.getProperty("java.version");
+		try {
+			System.setProperty("java.runtime.version", "jvmxp3260-20080816_22093");
+			System.setProperty("java.version", "1.5.0");
+			assertFalse(JavaRuntimeVersionChecker.isJavaVersionSmallerThan(1.5f));
+			assertTrue(JavaRuntimeVersionChecker.isJavaVersionSmallerThan(1.6f));
+			System.setProperty("java.version", "invalid");
+			assertFalse(JavaRuntimeVersionChecker.isJavaVersionSmallerThan(0.0f));
+			assertFalse(JavaRuntimeVersionChecker.isJavaVersionSmallerThan(10.0f));
+		} finally {
+			System.setProperty("java.runtime.version", runtimeVersion);
+			System.setProperty("java.version", version);
+		}
+	}
+
 }
