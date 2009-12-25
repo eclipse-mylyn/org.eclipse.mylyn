ebfae70f96ceaff6a59f3fd08a35f138f2b1cefa
fbecker <>
2009-12-25 20:42:17 +0000

    ASSIGNED - bug 298493: Exception following hyperlink to bug 000001 https://bugs.eclipse.org/bugs/show_bug.cgi?id=298493

--- org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/tasks/ui/TaskHyperlink.java
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/tasks/ui/TaskHyperlink.java
@@ -37,6 +37,10 @@ public final class TaskHyperlink implements IHyperlink {
 	public TaskHyperlink(IRegion region, TaskRepository repository, String taskId) {
 		this.region = region;
 		this.repository = repository;
+		// remove leading blanks
+		while (taskId.startsWith("0")) {
+			taskId = taskId.substring(1);
+		}
 		this.taskId = taskId;
 	}
 
