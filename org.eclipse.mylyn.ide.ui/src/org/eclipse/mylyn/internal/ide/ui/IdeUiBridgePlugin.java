57fb619b527ee7472c57dc212977c2987beae1cd
spingel <>
2010-01-29 21:58:04 +0000

    NEW - bug 292090: cleanup fixme and todo comments for 3.4 https://bugs.eclipse.org/bugs/show_bug.cgi?id=292090

--- org.eclipse.mylyn.ide.ui/src/org/eclipse/mylyn/internal/ide/ui/IdeUiBridgePlugin.java
+++ org.eclipse.mylyn.ide.ui/src/org/eclipse/mylyn/internal/ide/ui/IdeUiBridgePlugin.java
@@ -25,6 +25,8 @@ public class IdeUiBridgePlugin extends AbstractUIPlugin {
 
 	private static IdeUiBridgePlugin INSTANCE;
 
+	// FIXME 3.4 remove unused field
+	@Deprecated
 	public static final ImageDescriptor EDGE_REF_XML = getImageDescriptor("icons/elcl16/edge-ref-xml.gif"); //$NON-NLS-1$
 
 	public static class IdeUiBridgeStartup implements IContextUiStartup {
