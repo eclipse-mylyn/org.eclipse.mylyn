863c324634d0349c50854bc31d228b3ad3424745
sminto <>
2010-02-05 01:12:35 +0000

    ASSIGNED - bug 241790: [backport] context not saved on shutdown https://bugs.eclipse.org/bugs/show_bug.cgi?id=241790

--- org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/TasksUiPlugin.java
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/TasksUiPlugin.java
@@ -579,9 +579,6 @@ public class TasksUiPlugin extends AbstractUIPlugin {
 				public void saving(ISaveContext context) throws CoreException {
 					if (context.getKind() == ISaveContext.FULL_SAVE) {
 						externalizationManager.stop();
-						if (taskActivityManager != null) {
-							taskActivityManager.deactivateActiveTask();
-						}
 					}
 				}
 			};
