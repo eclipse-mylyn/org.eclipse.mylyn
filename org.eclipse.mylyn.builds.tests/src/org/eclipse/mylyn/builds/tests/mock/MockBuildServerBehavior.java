6f946c1ab26a176d57e5d1f43d10f69e44760b89
spingel <spingel>
2010-05-30 20:28:46 +0000

    NEW - bug 314773: [build] create common build view https://bugs.eclipse.org/bugs/show_bug.cgi?id=314773

--- org.eclipse.mylyn.builds.tests/src/org/eclipse/mylyn/builds/tests/mock/MockBuildServerBehavior.java
+++ org.eclipse.mylyn.builds.tests/src/org/eclipse/mylyn/builds/tests/mock/MockBuildServerBehavior.java
@@ -17,14 +17,12 @@ import java.util.List;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.mylyn.builds.core.BuildState;
 import org.eclipse.mylyn.builds.core.IBuildPlan;
+import org.eclipse.mylyn.builds.core.IBuildPlanWorkingCopy;
 import org.eclipse.mylyn.builds.core.IBuildServer;
 import org.eclipse.mylyn.builds.core.IOperationMonitor;
 import org.eclipse.mylyn.builds.core.spi.BuildServerBehaviour;
-import org.eclipse.mylyn.internal.builds.core.BuildFactory;
-import org.eclipse.mylyn.internal.builds.core.BuildPackage;
-import org.eclipse.mylyn.internal.builds.core.BuildPlan;
-import org.eclipse.mylyn.internal.builds.core.BuildState;
 
 /**
  * @author Steffen Pingel
@@ -37,33 +35,35 @@ public class MockBuildServerBehavior extends BuildServerBehaviour {
 
 	@Override
 	public List<IBuildPlan> getPlans(IOperationMonitor monitor) {
-		BuildFactory factory = BuildPackage.eINSTANCE.getBuildFactory();
-
-		BuildPlan failingPlan = factory.createBuildPlan();
+		IBuildPlanWorkingCopy failingPlan = getServer().createBuildPlan();
 		failingPlan.setId("1");
 		failingPlan.setName("Failing Build Plan");
 		failingPlan.setState(BuildState.RUNNING);
 		failingPlan.setStatus("Failed");
+		failingPlan.setHealth(15);
 
-		BuildPlan childPlan1 = factory.createBuildPlan();
+		IBuildPlanWorkingCopy childPlan1 = getServer().createBuildPlan();
 		childPlan1.setId("1.1");
 		childPlan1.setName("Stopped Child Build Plan");
 		childPlan1.setState(BuildState.STOPPED);
 		childPlan1.setStatus("Failed");
 		failingPlan.getChildren().add(childPlan1);
 
-		BuildPlan childPlan2 = factory.createBuildPlan();
+		IBuildPlanWorkingCopy childPlan2 = getServer().createBuildPlan();
 		childPlan2.setId("1.2");
 		childPlan2.setName("Running Child Build Plan");
 		childPlan2.setState(BuildState.STOPPED);
 		childPlan2.setStatus("Unknown");
-		childPlan2.setParent(failingPlan);
+		childPlan2.setHealth(55);
+		failingPlan.getChildren().add(childPlan2);
 
-		BuildPlan succeedingPlan = factory.createBuildPlan();
+		IBuildPlanWorkingCopy succeedingPlan = getServer().createBuildPlan();
 		succeedingPlan.setId("2");
 		succeedingPlan.setName("Succeeding Build Plan");
 		succeedingPlan.setState(BuildState.STOPPED);
 		succeedingPlan.setStatus("Success");
+		succeedingPlan.setInfo("12 tests passing");
+		succeedingPlan.setHealth(89);
 
 		return Arrays.asList((IBuildPlan) failingPlan, childPlan1, childPlan2, succeedingPlan);
 	}
