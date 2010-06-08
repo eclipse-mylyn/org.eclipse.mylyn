9762d4ecdbca719016fe6c64d3b33e2ff58cb762
spingel <spingel>
2010-06-08 02:15:57 +0000

    NEW - bug 315716: [build] implement repository verification for Hudson https://bugs.eclipse.org/bugs/show_bug.cgi?id=315716

---
+++ org.eclipse.mylyn.hudson.ui/src/org/eclipse/mylyn/internal/hudson/ui/HudsonConnectorUi.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Markus Knittig and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Markus Knittig - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.mylyn.internal.hudson.ui;
+
+import org.eclipse.mylyn.builds.ui.spi.BuildConnectorUi;
+
+/**
+ * @author Markus Knittig
+ */
+public class HudsonConnectorUi extends BuildConnectorUi {
+
+}
