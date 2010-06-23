c7f607bb0acde159dedb5c46fe5581e8c6a51b0d
fbecker <>
2010-06-23 18:57:20 +0000

    ASSIGNED - bug 317651: clearing of iplog flag fails on bugs.eclipse.org https://bugs.eclipse.org/bugs/show_bug.cgi?id=317651

--- org.eclipse.mylyn.bugzilla.core/src/org/eclipse/mylyn/internal/bugzilla/core/BugzillaClient.java
+++ org.eclipse.mylyn.bugzilla.core/src/org/eclipse/mylyn/internal/bugzilla/core/BugzillaClient.java
@@ -850,7 +850,7 @@ public class BugzillaClient {
 						a = a.getAttribute("state"); //$NON-NLS-1$
 						String id = "flag-" + flagnumber.getValue(); //$NON-NLS-1$
 						String value = a.getValue();
-						if (value.equals(" ")) { //$NON-NLS-1$
+						if (value.equals(" ") || value.equals("")) { //$NON-NLS-1$ //$NON-NLS-2$
 							value = "X"; //$NON-NLS-1$
 						}
 						if (value.equals("?") && requestee != null) { //$NON-NLS-1$
@@ -966,7 +966,7 @@ public class BugzillaClient {
 				String requesteeName = "requestee_type-" + id.substring(26); //$NON-NLS-1$
 				String requesteeValue = requestee.getValue();
 				value = state.getValue();
-				if (value.equals(" ")) { //$NON-NLS-1$
+				if (value.equals(" ") || value.equals("")) { //$NON-NLS-1$ //$NON-NLS-2$
 					value = "X"; //$NON-NLS-1$
 				}
 				if (value.equals("?")) { //$NON-NLS-1$
@@ -979,7 +979,7 @@ public class BugzillaClient {
 				String requesteeName = "requestee-" + id.substring(21); //$NON-NLS-1$
 				String requesteeValue = requestee.getValue();
 				value = state.getValue();
-				if (value.equals(" ") | value.equals("")) { //$NON-NLS-1$//$NON-NLS-2$
+				if (value.equals(" ") || value.equals("")) { //$NON-NLS-1$//$NON-NLS-2$
 					value = "X"; //$NON-NLS-1$
 				}
 				if (value.equals("?")) { //$NON-NLS-1$
@@ -1280,7 +1280,7 @@ public class BugzillaClient {
 						TaskAttribute requestee = a.getAttribute("requestee"); //$NON-NLS-1$
 						a = a.getAttribute("state"); //$NON-NLS-1$
 						value = a.getValue();
-						if (value.equals(" ")) { //$NON-NLS-1$
+						if (value.equals(" ") || value.equals("")) { //$NON-NLS-1$ //$NON-NLS-2$
 							continue;
 						}
 						String flagname = a.getMetaData().getLabel();
@@ -1306,7 +1306,7 @@ public class BugzillaClient {
 						a = a.getAttribute("state"); //$NON-NLS-1$
 						id = "flag-" + flagnumber.getValue(); //$NON-NLS-1$
 						value = a.getValue();
-						if (value.equals(" ")) { //$NON-NLS-1$
+						if (value.equals(" ") || value.equals("")) { //$NON-NLS-1$ //$NON-NLS-2$
 							value = "X"; //$NON-NLS-1$
 						}
 						if (value.equals("?") && requestee != null) { //$NON-NLS-1$
