dbb81060bd6d7d7590b1ce505f6bc927134c4760
spingel <>
2010-06-24 06:07:54 +0000

    NEW - bug 317767: [build] automatically refresh Builds view https://bugs.eclipse.org/bugs/show_bug.cgi?id=317767

---
+++ org.eclipse.mylyn.commons.ui/src/org/eclipse/mylyn/internal/provisional/commons/ui/actions/CollapseAllAction.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Tasktop Technologies and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tasktop Technologies - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.mylyn.internal.provisional.commons.ui.actions;
+
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.viewers.AbstractTreeViewer;
+import org.eclipse.mylyn.internal.provisional.commons.ui.CommonImages;
+
+/**
+ * @author Mik Kersten
+ * @author Steffen Pingel
+ */
+public class CollapseAllAction extends Action {
+
+	private final AbstractTreeViewer viewer;
+
+	public CollapseAllAction(AbstractTreeViewer viewer) {
+		Assert.isNotNull(viewer);
+		this.viewer = viewer;
+		setText(Messages.CollapseAllAction_Label);
+		setToolTipText(Messages.CollapseAllAction_ToolTip);
+		setImageDescriptor(CommonImages.COLLAPSE_ALL);
+	}
+
+	@Override
+	public void run() {
+		viewer.collapseAll();
+	}
+
+}
---
+++ org.eclipse.mylyn.commons.ui/src/org/eclipse/mylyn/internal/provisional/commons/ui/actions/Messages.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Tasktop Technologies and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tasktop Technologies - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.mylyn.internal.provisional.commons.ui.actions;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+
+	private static final String BUNDLE_NAME = "org.eclipse.mylyn.internal.provisional.commons.ui.actions.messages"; //$NON-NLS-1$
+
+	public static String CollapseAllAction_Label;
+
+	public static String CollapseAllAction_ToolTip;
+
+	public static String ExpandAllAction_Label;
+
+	public static String ExpandAllAction_ToolTip;
+
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+
+}
