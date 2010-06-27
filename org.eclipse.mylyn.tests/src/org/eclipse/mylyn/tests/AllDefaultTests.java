2f4b0688366420dec01ee0b6be060886729c2bf2
spingel <>
2010-06-27 03:53:57 +0000

    NEW - bug 221939: [releng] manage branches https://bugs.eclipse.org/bugs/show_bug.cgi?id=221939

---
+++ org.eclipse.mylyn.tests/src/org/eclipse/mylyn/tests/AllDefaultTests.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2010 Tasktop Technologies and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tasktop Technologies - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.mylyn.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.eclipse.mylyn.commons.tests.support.ManagedTestSuite;
+
+/**
+ * @author Mik Kersten
+ */
+public class AllDefaultTests {
+
+	public static Test suite() {
+		TestSuite suite = new ManagedTestSuite("Tests for org.eclipse.mylyn.tests");
+		suite.addTest(AllNonConnectorTests.suite());
+		suite.addTest(AllConnectorTests.suite());
+		return suite;
+	}
+
+}
