381fdc6c8ca49d338dbe06db2dbccda06c7f5d96
fbecker <>
2010-06-28 04:51:22 +0000

    ASSIGNED - bug 318112: [releng] update the test repositories to the latest version of Bugzilla https://bugs.eclipse.org/bugs/show_bug.cgi?id=318112

--- org.eclipse.mylyn.bugzilla.tests/src/org/eclipse/mylyn/bugzilla/tests/support/BugzillaFixture.java
+++ org.eclipse.mylyn.bugzilla.tests/src/org/eclipse/mylyn/bugzilla/tests/support/BugzillaFixture.java
@@ -92,25 +92,25 @@ public class BugzillaFixture extends TestFixture {
 			"3.0.11", "");
 
 	public static BugzillaFixture BUGS_3_2 = new BugzillaFixture(BugzillaFixture.TEST_BUGZILLA_32_URL, //
-			"3.2.6", "");
+			"3.2.7", "");
 
 	public static BugzillaFixture BUGS_3_4 = new BugzillaFixture(BugzillaFixture.TEST_BUGZILLA_34_URL, //
-			"3.4.6", "");
+			"3.4.7", "");
 
 	public static BugzillaFixture BUGS_3_6 = new BugzillaFixture(BugzillaFixture.TEST_BUGZILLA_36_URL, //
-			"3.6", "");
+			"3.6.1", "");
 
 	public static BugzillaFixture BUGS_3_6_CUSTOM_WF = new BugzillaFixture(BugzillaFixture.TEST_BUGZILLA_36_URL
-			+ "-custom-wf", "3.6", "Custom Workflow");
+			+ "-custom-wf", "3.6.1", "Custom Workflow");
 
 	public static BugzillaFixture BUGS_3_6_CUSTOM_WF_AND_STATUS = new BugzillaFixture(
-			BugzillaFixture.TEST_BUGZILLA_36_URL + "-custom-wf-and-status", "3.6", "Custom Workflow and Status");
+			BugzillaFixture.TEST_BUGZILLA_36_URL + "-custom-wf-and-status", "3.6.1", "Custom Workflow and Status");
 
 	public static BugzillaFixture BUGS_3_6_XML_RPC_DISABLED = new BugzillaFixture(BugzillaFixture.TEST_BUGZILLA_36_URL
-			+ "-xml-rpc-disabled", "3.6", "XML-RPC disabled");
+			+ "-xml-rpc-disabled", "3.6.1", "XML-RPC disabled");
 
 	public static BugzillaFixture BUGS_HEAD = new BugzillaFixture(BugzillaFixture.TEST_BUGZILLA_HEAD_URL, //
-			"3.7", "");
+			"3.7.1+", "");
 
 	public static BugzillaFixture DEFAULT = BUGS_3_6;
 
