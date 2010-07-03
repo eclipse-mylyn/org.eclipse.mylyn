f0bdbd55024d4b0aca910a8aaf1fa63df0c482be
spingel <>
2010-07-03 08:07:39 +0000

    NEW - bug 272189: [api] provide notification provider API in commons https://bugs.eclipse.org/bugs/show_bug.cgi?id=272189

---
+++ org.eclipse.mylyn.commons.notifications/src/org/eclipse/mylyn/commons/notifications/INotificationService.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Tasktop Technologies and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tasktop Technologies - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.mylyn.commons.notifications;
+
+
+/**
+ * @author Steffen Pingel
+ * @noextend This interface is not intended to be extended by clients.
+ * @noimplement This interface is not intended to be implemented by clients.
+ */
+public interface INotificationService {
+
+}
