e00d79c2d606b13e24d244746dec7c7c22429847
spingel <>
2010-07-29 16:26:37 +0000

    RESOLVED - bug 303509: TaskComparator.sortByRank(...) does not sort when one of the tasks has null in RANK https://bugs.eclipse.org/bugs/show_bug.cgi?id=303509

--- org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/util/TaskComparator.java
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/util/TaskComparator.java
@@ -187,7 +187,11 @@ public class TaskComparator implements Comparator<ITask> {
 				String rankString1 = element1.getAttribute(TaskAttribute.RANK);
 				String rankString2 = element2.getAttribute(TaskAttribute.RANK);
 				try {
-					return compare(Double.parseDouble(rankString1), Double.parseDouble(rankString2), sortDirection);
+					Double rank1 = rankString1 == null || rankString1.length() == 0 ? Double.valueOf(0)
+							: Double.valueOf(rankString1);
+					Double rank2 = rankString2 == null || rankString2.length() == 0 ? Double.valueOf(0)
+							: Double.valueOf(rankString2);
+					return compare(rank1, rank2, sortDirection);
 				} catch (NumberFormatException e) {
 					// ignore, means that there is no rank on one of the elements
 				}
