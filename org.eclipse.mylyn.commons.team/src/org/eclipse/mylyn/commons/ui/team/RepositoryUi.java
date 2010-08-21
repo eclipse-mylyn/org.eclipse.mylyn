90a219c73095b5dd937fb34851860c549bc8eff8
spingel <>
2010-08-21 21:39:01 +0000

    NEW - bug 321813: [build] fix Hudson connector UI nits https://bugs.eclipse.org/bugs/show_bug.cgi?id=321813

---
+++ org.eclipse.mylyn.commons.team/src/org/eclipse/mylyn/commons/ui/team/RepositoryUi.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Tasktop Technologies and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tasktop Technologies - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.mylyn.commons.ui.team;
+
+/**
+ * @author Steffen Pingel
+ */
+public final class RepositoryUi {
+
+	private RepositoryUi() {
+	}
+
+	public static final String ID_VIEW_REPOSITORIES = "org.eclipse.mylyn.commons.team.navigator.Repositories"; //$NON-NLS-1$
+
+}
