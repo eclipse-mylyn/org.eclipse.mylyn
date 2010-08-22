670289bdd8881fa40628048a4d7765d2d54b31ac
spingel <>
2010-08-22 07:36:59 +0000

    NEW - bug 322192: [build] strip html tags from hudson job descriptions https://bugs.eclipse.org/bugs/show_bug.cgi?id=322192

---
+++ org.eclipse.mylyn.commons.ui/src/org/eclipse/mylyn/internal/provisional/commons/ui/PlatformUiUtil.java
@@ -0,0 +1,121 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2010 Tasktop Technologies and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tasktop Technologies - initial API and implementation
+ *     David Green - fix for bug 247182
+ *     Frank Becker - fixes for bug 259877
+ *******************************************************************************/
+
+package org.eclipse.mylyn.internal.provisional.commons.ui;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyledText;
+
+/**
+ * @author Steffen Pingel
+ */
+public class PlatformUiUtil {
+
+	private static class Eclipse36Checker {
+		public static final boolean result;
+		static {
+			boolean methodAvailable = false;
+			try {
+				StyledText.class.getMethod("setTabStops", int[].class); //$NON-NLS-1$
+				methodAvailable = true;
+			} catch (NoSuchMethodException e) {
+			}
+			result = methodAvailable;
+		}
+	}
+
+	/**
+	 * bug 247182: file import dialog doesn't work on Mac OS X if the file extension has more than one dot.
+	 */
+	public static String[] getFilterExtensions(String... extensions) {
+		for (int i = 0; i < extensions.length; i++) {
+			String extension = extensions[i];
+			if (Platform.OS_MACOSX.equals(Platform.getOS())) {
+				int j = extension.lastIndexOf('.');
+				if (j != -1) {
+					extension = extension.substring(j);
+				}
+			}
+			extensions[i] = "*" + extension; //$NON-NLS-1$
+		}
+		return extensions;
+	}
+
+	public static int getToolTipXShift() {
+		if ("gtk".equals(SWT.getPlatform()) || "carbon".equals(SWT.getPlatform()) || "cocoa".equals(SWT.getPlatform())) { //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+			return -26;
+		} else {
+			return -23;
+		}
+	}
+
+	public static int getTreeImageOffset() {
+		if ("carbon".equals(SWT.getPlatform())) { //$NON-NLS-1$
+			return 16;
+		} else if ("cocoa".equals(SWT.getPlatform())) { //$NON-NLS-1$
+			return 13;
+		} else {
+			return 20;
+		}
+	}
+
+	public static int getIncomingImageOffset() {
+		if ("carbon".equals(SWT.getPlatform())) { //$NON-NLS-1$
+			return 5;
+		} else if ("cocoa".equals(SWT.getPlatform())) { //$NON-NLS-1$
+			return 2;
+		} else {
+			return 6;
+		}
+	}
+
+	public static int getTreeItemSquish() {
+		if ("gtk".equals(SWT.getPlatform())) { //$NON-NLS-1$
+			return 8;
+		} else if (isMac()) {
+			return 3;
+		} else {
+			return 0;
+		}
+	}
+
+	private static boolean isMac() {
+		return "carbon".equals(SWT.getPlatform()) || "cocoa".equals(SWT.getPlatform()); //$NON-NLS-1$ //$NON-NLS-2$
+	}
+
+	// TODO e3.5: remove, platform has been fixed, see bug 272046
+	public static boolean isPaintItemClippingRequired() {
+		return "gtk".equals(SWT.getPlatform()); //$NON-NLS-1$
+	}
+
+	public static boolean spinnerHasNativeBorder() {
+		return isMac() && !isEclipse36orLater();
+	}
+
+	private static boolean isEclipse36orLater() {
+		return Eclipse36Checker.result;
+	}
+
+	public static boolean hasNarrowToolBar() {
+		return Platform.WS_WIN32.equals(SWT.getPlatform());
+	}
+
+	/**
+	 * Returns the width of the view menu drop-down button.
+	 */
+	public static int getViewMenuWidth() {
+		return 32;
+	}
+
+}
---
+++ org.eclipse.mylyn.commons.ui/src/org/eclipse/mylyn/internal/provisional/commons/ui/RichToolTip.java
@@ -0,0 +1,187 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2010 Tasktop Technologies and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tasktop Technologies - initial API and implementation
+ *     Kevin Barnes, IBM Corporation - fix for bug 277974
+ *******************************************************************************/
+
+package org.eclipse.mylyn.internal.provisional.commons.ui;
+
+import org.eclipse.mylyn.internal.commons.ui.CommonsUiPlugin;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Scrollable;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.swt.widgets.ToolBar;
+import org.eclipse.swt.widgets.ToolItem;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
+import org.eclipse.swt.widgets.Widget;
+import org.eclipse.ui.forms.IFormColors;
+
+/**
+ * @author Mik Kersten
+ * @author Eric Booth
+ * @author Leo Dos Santos
+ * @author Steffen Pingel
+ */
+public abstract class RichToolTip extends GradientToolTip {
+
+	private final static int X_SHIFT = PlatformUiUtil.getToolTipXShift();
+
+	private final static int Y_SHIFT = 1;
+
+	private final Control control;
+
+	private Object data;
+
+	private boolean enabled;
+
+	private boolean triggeredByMouse = true;
+
+	private boolean visible;
+
+	public RichToolTip(Control control) {
+		super(control);
+		this.control = control;
+		setEnabled(true);
+		setShift(new Point(1, 1));
+	}
+
+	@Override
+	protected void afterHideToolTip(Event event) {
+		triggeredByMouse = true;
+		visible = false;
+	}
+
+	protected Object computeData(Widget hoverWidget) {
+		return (hoverWidget).getData();
+	}
+
+	private boolean contains(int x, int y) {
+		if (control instanceof Scrollable) {
+			return ((Scrollable) control).getClientArea().contains(x, y);
+		} else {
+			return control.getBounds().contains(x, y);
+		}
+	}
+
+	public void dispose() {
+		hide();
+	}
+
+	private Rectangle getBounds(Widget widget) {
+		if (widget instanceof ToolItem) {
+			ToolItem w = (ToolItem) widget;
+			return w.getBounds();
+		}
+		if (widget instanceof TableItem) {
+			TableItem w = (TableItem) widget;
+			return w.getBounds();
+		}
+		if (widget instanceof TreeItem) {
+			TreeItem w = (TreeItem) widget;
+			return w.getBounds();
+		}
+		return null;
+	}
+
+	public Object getData() {
+		return data;
+	}
+
+	@Override
+	public Point getLocation(Point tipSize, Event event) {
+		Widget widget = getTipWidget(event);
+		if (widget != null) {
+			Rectangle bounds = getBounds(widget);
+			if (bounds != null) {
+				return control.toDisplay(bounds.x + X_SHIFT, bounds.y + bounds.height + Y_SHIFT);
+			}
+		}
+		return super.getLocation(tipSize, event);//control.toDisplay(event.x + xShift, event.y + yShift);
+	}
+
+	protected Widget getTipWidget(Event event) {
+		Point widgetPosition = new Point(event.x, event.y);
+		Widget widget = event.widget;
+		if (widget instanceof ToolBar) {
+			ToolBar w = (ToolBar) widget;
+			return w.getItem(widgetPosition);
+		}
+		if (widget instanceof Table) {
+			Table w = (Table) widget;
+			return w.getItem(widgetPosition);
+		}
+		if (widget instanceof Tree) {
+			Tree w = (Tree) widget;
+			return w.getItem(widgetPosition);
+		}
+
+		return widget;
+	}
+
+	protected Color getTitleColor() {
+		return CommonsUiPlugin.getDefault().getFormColors(control.getDisplay()).getColor(IFormColors.TITLE);
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public boolean isTriggeredByMouse() {
+		return triggeredByMouse;
+	}
+
+	public boolean isVisible() {
+		return visible;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	@Override
+	protected boolean shouldCreateToolTip(Event event) {
+		data = null;
+
+		if (isTriggeredByMouse() && !enabled) {
+			return false;
+		}
+
+		if (super.shouldCreateToolTip(event)) {
+			Widget tipWidget = getTipWidget(event);
+			if (tipWidget != null) {
+				Rectangle bounds = getBounds(tipWidget);
+				if (tipWidget instanceof ScalingHyperlink) {
+					data = computeData(tipWidget);
+				} else if (bounds != null && contains(bounds.x, bounds.y)) {
+					data = computeData(tipWidget);
+				}
+			}
+		}
+
+		if (data == null) {
+			hide();
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	@Override
+	public void show(Point location) {
+		triggeredByMouse = false;
+		super.show(location);
+	}
+
+}
