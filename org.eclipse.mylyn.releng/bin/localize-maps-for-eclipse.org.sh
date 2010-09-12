0bd2a11597f7fa60c5fde028d514261ef5c098b9
spingel <>
2010-09-12 23:02:21 +0000

    removed stale mirror

--- org.eclipse.mylyn.releng/bin/localize-maps-for-eclipse.org.sh
+++ org.eclipse.mylyn.releng/bin/localize-maps-for-eclipse.org.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 sed -i -e s/:pserver:anonymous@dev.eclipse.org:// maps/*.map
-sed -i -e s/http:\\/\\/eclipse.unixheads.org/file:\\/home\\/data\\/httpd\\/download.eclipse.org/ maps/*.map
 sed -i -e s/http:\\/\\/download.eclipse.org/file:\\/home\\/data\\/httpd\\/download.eclipse.org/ maps/*.map
