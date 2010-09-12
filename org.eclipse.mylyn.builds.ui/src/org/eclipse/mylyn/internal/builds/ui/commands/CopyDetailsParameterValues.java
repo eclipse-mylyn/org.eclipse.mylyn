3bdc01f8c47999b85d36e91cc8cf57341fa80bce
spingel <spingel>
2010-09-12 03:01:46 +0000

    NEW - bug 324372: [build] add copy details menu https://bugs.eclipse.org/bugs/show_bug.cgi?id=324372

---
+++ org.eclipse.mylyn.builds.ui/src/org/eclipse/mylyn/internal/builds/ui/commands/CopyDetailsParameterValues.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Tasktop Technologies and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tasktop Technologies - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.mylyn.internal.builds.ui.commands;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.core.commands.IParameterValues;
+import org.eclipse.mylyn.internal.builds.ui.commands.CopyDetailsHandler.Mode;
+
+/**
+ * @author Steffen Pingel
+ */
+public class CopyDetailsParameterValues implements IParameterValues {
+
+	public Map getParameterValues() {
+		Map<String, String> result = new HashMap<String, String>();
+		for (Mode mode : CopyDetailsHandler.Mode.values()) {
+			result.put(mode.toString(), mode.name());
+		}
+		return result;
+	}
+
+}
