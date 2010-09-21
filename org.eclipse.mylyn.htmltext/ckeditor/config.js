0173b9bd3148390b7de94f814c5ae41e548f8126
tseidel <tseidel>
2010-09-21 01:50:46 +0000

    Initial version, see CQ 4372 for details

---
+++ org.eclipse.mylyn.htmltext/ckeditor/config.js
@@ -0,0 +1,19 @@
+﻿/*
+Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+	config.skin = 'office2003';
+	config.resize_enabled = false;
+	config.toolbar =[[]];
+	config.toolbarLocation = 'bottom';
+	config.toolbarCanCollapse = false;
+	config.toolbarStartupExpanded = false;
+	config.scayt_autoStartup = false;
+	config.startupFocus = true;
+};
