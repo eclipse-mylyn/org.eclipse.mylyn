d7d83820b776b4217642936eb040f728997797f0
spingel <spingel>
2010-10-20 05:40:40 +0000

    NEW - bug 328183: [patch][build][hudson] group test results in junit view https://bugs.eclipse.org/bugs/show_bug.cgi?id=328183

--- org.eclipse.mylyn.builds.tests/src/org/eclipse/mylyn/builds/tests/util/JUnitResultGeneratorTest.java
+++ org.eclipse.mylyn.builds.tests/src/org/eclipse/mylyn/builds/tests/util/JUnitResultGeneratorTest.java
@@ -32,14 +32,25 @@ import org.eclipse.mylyn.builds.internal.core.util.JUnitResultGenerator;
  */
 public class JUnitResultGeneratorTest extends TestCase {
 
-	public void testWrite() throws Exception {
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
+	private ByteArrayOutputStream out;
+
+	private ITestResult testResult;
+
+	private ITestSuite suite;
+
+	private ITestCase testCase;
+
+	private TransformerHandler handler;
+
+	@Override
+	protected void setUp() throws Exception {
+		out = new ByteArrayOutputStream();
 		StreamResult streamResult = new StreamResult(out);
 		SAXTransformerFactory factory = (SAXTransformerFactory) TransformerFactory.newInstance();
-		TransformerHandler handler = factory.newTransformerHandler();
+		handler = factory.newTransformerHandler();
 		handler.setResult(streamResult);
 
-		ITestResult testResult = BuildFactory.eINSTANCE.createTestResult();
+		testResult = BuildFactory.eINSTANCE.createTestResult();
 		testResult.setDuration(111L);
 		testResult.setErrorCount(1);
 		testResult.setFailCount(2);
@@ -49,22 +60,35 @@ public class JUnitResultGeneratorTest extends TestCase {
 		build.setLabel("Build1");
 		testResult.setBuild(build);
 
-		ITestSuite suite = BuildFactory.eINSTANCE.createTestSuite();
-		suite.setLabel("Suite1");
+		suite = BuildFactory.eINSTANCE.createTestSuite();
+		suite.setLabel("TestClass1");
 		suite.setDuration(222L);
 		testResult.getSuites().add(suite);
 
-		ITestCase testCase = BuildFactory.eINSTANCE.createTestCase();
+		testCase = BuildFactory.eINSTANCE.createTestCase();
 		testCase.setClassName("TestClass1");
 		testCase.setLabel("TestCase1");
 		testCase.setDuration(333L);
 		suite.getCases().add(testCase);
+	}
+
+	public void testWrite() throws Exception {
+		JUnitResultGenerator generator = new JUnitResultGenerator(testResult);
+		generator.write(handler);
+
+		assertEquals(
+				"<?xml version=\"1.0\" encoding=\"UTF-8\"?><testrun name=\"Build1\" project=\"Build1\" tests=\"6\" started=\"6\" failures=\"2\" errors=\"1\" ignored=\"3\"><testsuite name=\"TestClass1\" time=\"0.222\"><testcase name=\"TestCase1\" classname=\"TestClass1\" time=\"0.333\"/></testsuite></testrun>",
+				out.toString());
+	}
+
+	public void testWriteWithChildSuite() throws Exception {
+		suite.setLabel("Suite1");
 
 		JUnitResultGenerator generator = new JUnitResultGenerator(testResult);
 		generator.write(handler);
 
 		assertEquals(
-				"<?xml version=\"1.0\" encoding=\"UTF-8\"?><testrun name=\"Build1\" project=\"Build1\" tests=\"6\" started=\"6\" failures=\"2\" errors=\"1\" ignored=\"3\"><testsuite name=\"Suite1\" time=\"0.222\"><testcase name=\"TestCase1\" classname=\"TestClass1\" time=\"0.333\"/></testsuite></testrun>",
+				"<?xml version=\"1.0\" encoding=\"UTF-8\"?><testrun name=\"Build1\" project=\"Build1\" tests=\"6\" started=\"6\" failures=\"2\" errors=\"1\" ignored=\"3\"><testsuite name=\"Suite1\" time=\"0.222\"><testsuite name=\"TestClass1\"><testcase name=\"TestCase1\" classname=\"TestClass1\" time=\"0.333\"/></testsuite></testsuite></testrun>",
 				out.toString());
 	}
 
