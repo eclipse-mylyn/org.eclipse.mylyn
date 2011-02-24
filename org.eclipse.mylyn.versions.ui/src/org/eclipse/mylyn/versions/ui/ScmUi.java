93d01bd72e21fed5e43d2ae57ae39f44caa09d7d
spingel <>
2011-02-24 03:31:51 +0000

    NEW - bug 336293: [git] Provide a versions connector for EGit https://bugs.eclipse.org/bugs/show_bug.cgi?id=336293

--- org.eclipse.mylyn.versions.ui/src/org/eclipse/mylyn/versions/ui/ScmUi.java
+++ org.eclipse.mylyn.versions.ui/src/org/eclipse/mylyn/versions/ui/ScmUi.java
@@ -15,7 +15,9 @@ import org.eclipse.compare.CompareUI;
 import org.eclipse.compare.ITypedElement;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.Assert;
 import org.eclipse.jface.util.OpenStrategy;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.team.core.history.IFileRevision;
 import org.eclipse.team.internal.core.history.LocalFileRevision;
 import org.eclipse.team.internal.ui.history.CompareFileRevisionEditorInput;
@@ -27,6 +29,28 @@ import org.eclipse.ui.IWorkbenchPage;
  * @author Steffen Pingel
  */
 public class ScmUi {
+
+	private static class EmptyTypedElement implements ITypedElement {
+
+		private final String name;
+
+		public EmptyTypedElement(String name) {
+			this.name = name;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public Image getImage() {
+			return null;
+		}
+
+		public String getType() {
+			return ITypedElement.UNKNOWN_TYPE;
+		}
+
+	}
 
 	protected static ITypedElement getElementFor(IResource resource) {
 		return SaveableCompareEditorInput.createFileElement((IFile) resource);
@@ -41,6 +65,8 @@ public class ScmUi {
 	}
 
 	public static void openCompareEditor(IWorkbenchPage page, IFileRevision file1, IFileRevision file2) {
+		Assert.isNotNull(file2);
+
 		IResource resource = getResource(file2);
 		if (resource != null) {
 			IFileRevision temp = file1;
@@ -48,11 +74,15 @@ public class ScmUi {
 			file2 = temp;
 		}
 		ITypedElement left;
-		resource = getResource(file1);
-		if (resource != null) {
-			left = getElementFor(resource);
+		if (file1 != null) {
+			resource = getResource(file1);
+			if (resource != null) {
+				left = getElementFor(resource);
+			} else {
+				left = new FileRevisionTypedElement(file1, null);
+			}
 		} else {
-			left = new FileRevisionTypedElement(file1, null);
+			left = new EmptyTypedElement(file2.getName());
 		}
 		ITypedElement right = new FileRevisionTypedElement(file2, null);
 
