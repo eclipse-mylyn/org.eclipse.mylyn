4ba11dbe28fc2377fa6c8349b1cc09236071b829
spingel <>
2011-02-27 20:01:40 +0000

    NEW - bug 337513: rename "Tasks" UI entries to "Mylyn" https://bugs.eclipse.org/bugs/show_bug.cgi?id=337513

---
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/preferences/MylynPreferencePage.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Tasktop Technologies and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tasktop Technologies - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.mylyn.internal.tasks.ui.preferences;
+
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+/**
+ * @author Steffen Pingel
+ */
+public class MylynPreferencePage extends PreferencePage implements IWorkbenchPreferencePage {
+
+	public MylynPreferencePage() {
+		super("Mylyn");
+	}
+
+	public void init(IWorkbench workbench) {
+	}
+
+	@Override
+	protected Control createContents(Composite parent) {
+		Composite composite = new Composite(parent, SWT.NONE);
+		return composite;
+	}
+
+}
