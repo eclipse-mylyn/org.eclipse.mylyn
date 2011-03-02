59e6cee95f12f6151a33a75a276f4bb192171a79
spingel <>
2011-03-02 06:04:04 +0000

    bug 335647: [releng] automate publishing of releases https://bugs.eclipse.org/bugs/show_bug.cgi?id=335647

--- org.eclipse.mylyn.releng/pack-and-sign/promote.sh
+++ org.eclipse.mylyn.releng/pack-and-sign/promote.sh
@@ -1,17 +1,10 @@
 #!/bin/sh -e
 
-SRC=https://hudson.eclipse.org/hudson/job/mylyn-release/lastSuccessfulBuild/artifact/org.eclipse.mylyn/org.eclipse.mylyn.releng/main-site/target
-
-wget -O version.properties $SRC/version.properties
-
-VERSION=$(head -n 1 version.properties)
-QUALIFIER=$(tail -n 1 version.properties)
-SRC=$2
+VERSION=$1
+QUALIFIER=$2
+SRC=$3
 DST=/home/data/httpd/download.eclipse.org/mylyn/archive/$VERSION/$QUALIFIER
-
 SITE=/home/data/httpd/download.eclipse.org/mylyn/snapshots
-
-rm version.properties
 
 if [ -e $DST ]; then
  echo $DST already exists
@@ -25,7 +18,7 @@ unzip -d $DST/ $SRC/site-packed.zip
 cp $SRC/site-archive.zip $DST/mylyn-$VERSION.$QUALIFIER.zip
 
 #chgrp -R mylynadmin $DST
-#chmod g+w -R $DST
+chmod g+w -R $DST
 
 echo Updating $SITE
 cd $(dirname $0)
