d81a57743fc47f14a5d916e7de45720d1c313a8e
sminto <>
2011-03-15 17:29:11 +0000

    RESOLVED - bug 315338: [keybinding] Keys for Remove from Context and Mark as Landmark doesn't work https://bugs.eclipse.org/bugs/show_bug.cgi?id=315338

--- org.eclipse.mylyn.context.ui/src/org/eclipse/mylyn/internal/context/ui/actions/MarkAsLandmarkCommandHandler.java
+++ org.eclipse.mylyn.context.ui/src/org/eclipse/mylyn/internal/context/ui/actions/MarkAsLandmarkCommandHandler.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Tasktop Technologies and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tasktop Technologies - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.mylyn.internal.context.ui.actions;
 
 import org.eclipse.core.commands.AbstractHandler;
@@ -5,12 +15,14 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.ui.handlers.HandlerUtil;
 
+/**
+ * @author Shawn Minto
+ */
 public class MarkAsLandmarkCommandHandler extends AbstractHandler {
 
 	private final InterestIncrementAction action = new InterestIncrementAction();
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		System.out.println(HandlerUtil.getCurrentSelection(event));
 		action.selectionChanged(null, HandlerUtil.getCurrentSelection(event));
 		action.run(null);
 		return null;
