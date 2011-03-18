40fb19141c86b2e031ffd040b079feb0cd95185e
spingel <>
2011-03-18 09:54:21 +0000

    update meta-data

--- org.eclipse.mylyn.releng/bin/add-mirrors.sh
+++ org.eclipse.mylyn.releng/bin/add-mirrors.sh
@@ -10,6 +10,11 @@
 #*******************************************************************************
 #!/bin/bash -e
 
+if [ -z "$1" ]; then
+ echo "usage statsId"
+ exit 1
+fi
+
 if [ ! -e site.xml ]; then
  echo "missing site.xml"
  exit 1
@@ -32,8 +37,23 @@ fi
 # escape slashes
 MIRROR_PATH=$(echo $RELATIVE | sed s/\\//\\\\\\//g)
 
-echo "Updated mirrorsURL in site.xml to $RELATIVE"
+echo "Updating mirrorsURL in site.xml to $RELATIVE"
 sed -i -e 's/<site pack200=\"true\">/<site pack200=\"true\" mirrorsURL="http:\/\/www.eclipse.org\/downloads\/download.php?file='$MIRROR_PATH'\/site.xml\&amp;protocol=http\&amp;format=xml">/' site.xml
 
-echo "Updated mirrorsURL in category.xml to $RELATIVE"
+if [ -e category.xml ]; then
+echo "Updating mirrorsURL in category.xml to $RELATIVE"
 sed -i -e 's/<site pack200=\"true\">/<site pack200=\"true\" mirrorsURL="http:\/\/www.eclipse.org\/downloads\/download.php?file='$MIRROR_PATH'\/\&amp;protocol=http\&amp;format=xml">/' category.xml
+fi
+
+MIRRORS_URL="http://www.eclipse.org/downloads/download.php?file=$MIRROR_PATH/&amp;protocol=http&amp;format=xml"
+STATS_URI="http://mylyn.eclipse.org/stats/$1
+
+echo "Updating p2.mirrorsURL in content.jar to $MIRRORS_URL and p2.statsURI to $STATS_URI"
+unzip -p content.jar | xsltproc -stringparam mirrorsURL http://foo/bar -stringparam statsURI http://stats/ /home/spingel/mylyn/src/org.eclipse.mylyn/org.eclipse.mylyn.releng/scripts/p2.xsl - > content.xml
+zip content.jar content.xml
+rm content.xml
+
+echo "Updating p2.mirrorsURL in artifacts.jar to $MIRRORS_URL and p2.statsURI to $STATS_URI"
+unzip -p artifacts.jar | xsltproc -stringparam mirrorsURL http://foo/bar -stringparam statsURI http://stats/ /home/spingel/mylyn/src/org.eclipse.mylyn/org.eclipse.mylyn.releng/scripts/p2.xsl - > artifacts.xml
+zip artifacts.jar artifacts.xml
+rm artifacts.xml
