d1db183f3d741b55fa3d66b05ecd9b719a837af3
Kevin Sawicki <kevin@github.com>
2011-04-10 11:15:54 -0700

    Add interface for new API constants
    Change-Id: I6d6a7c4265921a1cccd5dc5d979824923272c38a
    Signed-off-by: Kevin Sawicki <kevin@github.com>
    Signed-off-by: Chris Aniszczyk <caniszczyk@gmail.com>

---
+++ org.eclipse.mylyn.github.core/src/org/eclipse/mylyn/github/internal/IGitHubConstants.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ *  Copyright (c) 2011 GitHub Inc.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *    Kevin Sawicki (GitHub Inc.) - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.mylyn.github.internal;
+
+/**
+ * GitHub constants.
+ *
+ * @author Kevin Sawicki (kevin@github.com)
+ */
+public interface IGitHubConstants {
+
+	/**
+	 * REPOS_SEGMENT
+	 */
+	String SEGMENT_REPOS = "/repos"; //$NON-NLS-1$
+
+	/**
+	 * SEGMENT_ISSUES
+	 */
+	String SEGMENT_ISSUES = "/issues"; //$NON-NLS-1$
+
+	/**
+	 * SUFFIX_JSON
+	 */
+	String SUFFIX_JSON = ".json"; //$NON-NLS-1$
+
+	/**
+	 * HOST_API
+	 */
+	String HOST_API = "api.github.com"; //$NON-NLS-1$
+
+	/**
+	 * PROTOCOL_HTTPS
+	 */
+	String PROTOCOL_HTTPS = "https"; //$NON-NLS-1$
+
+}
