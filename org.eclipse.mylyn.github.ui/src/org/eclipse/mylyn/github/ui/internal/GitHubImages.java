99013bc4271ff4643a1b8a0046929c02c4cf7bdd
Kevin Sawicki <kevin@github.com>
2011-04-13 08:25:56 -0700

    Add icon for to be used for adding labels controls
    Change-Id: I9c60f286a3c15665cfb0b3d10a4c9d5acf62da8b
    Signed-off-by: Kevin Sawicki <kevin@github.com>
    Signed-off-by: Chris Aniszczyk <caniszczyk@gmail.com>

--- org.eclipse.mylyn.github.ui/src/org/eclipse/mylyn/github/ui/internal/GitHubImages.java
+++ org.eclipse.mylyn.github.ui/src/org/eclipse/mylyn/github/ui/internal/GitHubImages.java
@@ -30,12 +30,12 @@ public class GitHubImages {
 
 	public static final String GITHUB_LOGO_OBJ = NAME_PREFIX + "OBJ_GITHUB_LOGO"; //$NON-NLS-1$
 	public static final String GITHUB_ISSUE_LABEL_OBJ = NAME_PREFIX + "OBJ_GITHUB_ISSUE_LABEL"; //$NON-NLS-1$
+	public static final String GITHUB_ADD_OBJ = NAME_PREFIX + "GITHUB_ADD_OBJ"; //$NON-NLS-1$
 
-	
 	public static final ImageDescriptor DESC_GITHUB_LOGO = create(PATH_OBJ, "github.png"); //$NON-NLS-1$
 	public static final ImageDescriptor DESC_GITHUB_ISSUE_LABEL = create(PATH_OBJ, "issue_label.png"); //$NON-NLS-1$
+	public static final ImageDescriptor DESC_GITHUB_ADD = create(PATH_OBJ, "add.png"); //$NON-NLS-1$
 
-	
 	private static ImageDescriptor create(String prefix, String name) {
 		return ImageDescriptor.createFromURL(makeImageURL(prefix, name));
 	}
@@ -50,6 +50,7 @@ public class GitHubImages {
 		manager = new ImageRegistry();
 		manage(GITHUB_LOGO_OBJ, DESC_GITHUB_LOGO);
 		manage(GITHUB_ISSUE_LABEL_OBJ, DESC_GITHUB_ISSUE_LABEL);
+		manage(GITHUB_ADD_OBJ, DESC_GITHUB_ADD);
 	}
 
 	private static URL makeImageURL(String prefix, String name) {
