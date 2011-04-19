606777402ad2a5f0cf41c05145dd1ae6a930ebdc
Kevin Sawicki <kevin@github.com>
2011-04-19 15:17:16 -0700

    Drop 'as' from label for LEAVE operation
    Change-Id: Id4a90aff3c3a82b4d8d54db3e0dfa6c6d481aa00
    Signed-off-by: Kevin Sawicki <kevin@github.com>
    Signed-off-by: Chris Aniszczyk <caniszczyk@gmail.com>

--- org.eclipse.mylyn.github.core/src/org/eclipse/mylyn/github/internal/GitHubTaskOperation.java
+++ org.eclipse.mylyn.github.core/src/org/eclipse/mylyn/github/internal/GitHubTaskOperation.java
@@ -12,37 +12,65 @@
  *******************************************************************************/
 package org.eclipse.mylyn.github.internal;
 
+/**
+ * Enumeration of task operations
+ */
 public enum GitHubTaskOperation {
-	LEAVE("Leave as "),
+
+	/**
+	 * LEAD
+	 */
+	LEAVE("Leave "),
+
+	/**
+	 * REOPEN
+	 */
 	REOPEN("Reopen"),
+
+	/**
+	 * CLOSE
+	 */
 	CLOSE("Close");
-	
+
 	private final String label;
 
 	private GitHubTaskOperation(String label) {
 		this.label = label;
 	}
-	
+
+	/**
+	 * Get label
+	 * 
+	 * @return label
+	 */
 	public String getLabel() {
 		return label;
 	}
 
+	/**
+	 * Get id
+	 * 
+	 * @return id
+	 */
 	public String getId() {
 		return name();
 	}
 
 	/**
 	 * get the operation by its id
-	 * @param opId the id, or null
-	 * @return the operation, or null if the id was null or did not match any operation
+	 * 
+	 * @param opId
+	 *            the id, or null
+	 * @return the operation, or null if the id was null or did not match any
+	 *         operation
 	 */
 	public static GitHubTaskOperation fromId(String opId) {
-		for (GitHubTaskOperation op: values()) {
+		for (GitHubTaskOperation op : values()) {
 			if (op.getId().equals(opId)) {
 				return op;
 			}
 		}
 		return null;
 	}
-	
+
 }
