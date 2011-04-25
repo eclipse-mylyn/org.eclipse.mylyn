abdac698cb76d5a21951f65fb1be891e3b0ec999
Kevin Sawicki <kevin@github.com>
2011-04-25 15:22:23 -0700

    Add credentials page for import repositories wizard.
    The import repositories wizard will be used to bulk add
    all GitHub repositories to the Mylyn Task Repositories
    view.  Currently repositories are added one a time.
    
    Change-Id: I7c346216d4de8b4996fdcee14e4a41beb0a8a6ca
    Signed-off-by: Kevin Sawicki <kevin@github.com>

---
+++ org.eclipse.mylyn.github.ui/src/org/eclipse/mylyn/github/ui/internal/CredentialsWizardPage.java
@@ -0,0 +1,107 @@
+/*******************************************************************************
+ *  Copyright (c) 2011 GitHub Inc.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *    Kevin Sawicki (GitHub Inc.) - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.mylyn.github.ui.internal;
+
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+
+/**
+ * Credentials wizard page class.
+ * 
+ * @author Kevin Sawicki (kevin@github.com)
+ */
+public class CredentialsWizardPage extends WizardPage {
+
+	private Text userText;
+
+	private Text passwordText;
+
+	/**
+	 * Create credentials wizard page
+	 */
+	public CredentialsWizardPage() {
+		super("credentialsPage", Messages.CredentialsWizardPage_Title, null); //$NON-NLS-1$
+		setDescription(Messages.CredentialsWizardPage_Description);
+	}
+
+	/**
+	 * @see org.eclipse.jface.dialogs.IDialogPage#createControl(org.eclipse.swt.widgets.Composite)
+	 */
+	public void createControl(Composite parent) {
+		Composite displayArea = new Composite(parent, SWT.NONE);
+		GridLayoutFactory.swtDefaults().numColumns(2).equalWidth(false)
+				.applyTo(displayArea);
+
+		new Label(displayArea, SWT.NONE).setText(Messages.CredentialsWizardPage_LabelUser);
+
+		userText = new Text(displayArea, SWT.BORDER | SWT.SINGLE);
+		userText.addModifyListener(new ModifyListener() {
+
+			public void modifyText(ModifyEvent e) {
+				validatePage();
+			}
+		});
+		GridDataFactory.fillDefaults().grab(true, false).applyTo(userText);
+
+		new Label(displayArea, SWT.NONE).setText(Messages.CredentialsWizardPage_LabelPassword);
+		passwordText = new Text(displayArea, SWT.BORDER | SWT.SINGLE
+				| SWT.PASSWORD);
+		passwordText.addModifyListener(new ModifyListener() {
+
+			public void modifyText(ModifyEvent e) {
+				validatePage();
+			}
+		});
+		GridDataFactory.fillDefaults().grab(true, false).applyTo(passwordText);
+
+		setControl(displayArea);
+		setPageComplete(false);
+	}
+
+	private void validatePage() {
+		String message = null;
+		if (message == null && userText.getText().trim().length() == 0)
+			message = Messages.CredentialsWizardPage_ErrorUser;
+
+		if (message == null && passwordText.getText().trim().length() == 0)
+			message = Messages.CredentialsWizardPage_ErrorPassword;
+
+		setErrorMessage(message);
+		setPageComplete(message == null);
+
+	}
+
+	/**
+	 * Get user name
+	 * 
+	 * @return user name
+	 */
+	public String getUserName() {
+		return this.userText.getText();
+	}
+
+	/**
+	 * Get password
+	 * 
+	 * @return password
+	 */
+	public String getPassword() {
+		return this.passwordText.getText();
+	}
+
+}
--- org.eclipse.mylyn.github.ui/src/org/eclipse/mylyn/github/ui/internal/GitHubImages.java
+++ org.eclipse.mylyn.github.ui/src/org/eclipse/mylyn/github/ui/internal/GitHubImages.java
@@ -31,10 +31,14 @@ public class GitHubImages {
 	public static final String GITHUB_LOGO_OBJ = NAME_PREFIX + "OBJ_GITHUB_LOGO"; //$NON-NLS-1$
 	public static final String GITHUB_ISSUE_LABEL_OBJ = NAME_PREFIX + "OBJ_GITHUB_ISSUE_LABEL"; //$NON-NLS-1$
 	public static final String GITHUB_ADD_OBJ = NAME_PREFIX + "GITHUB_ADD_OBJ"; //$NON-NLS-1$
+	public static final String GITHUB_CHECKALL_OBJ = NAME_PREFIX + "GITHUB_CHECKALL_OBJ"; //$NON-NLS-1$
+	public static final String GITHUB_UNCHECKALL_OBJ = NAME_PREFIX + "GITHUB_UNCHECKALL_OBJ"; //$NON-NLS-1$
 
 	public static final ImageDescriptor DESC_GITHUB_LOGO = create(PATH_OBJ, "github.png"); //$NON-NLS-1$
 	public static final ImageDescriptor DESC_GITHUB_ISSUE_LABEL = create(PATH_OBJ, "issue_label.png"); //$NON-NLS-1$
 	public static final ImageDescriptor DESC_GITHUB_ADD = create(PATH_OBJ, "add.png"); //$NON-NLS-1$
+	public static final ImageDescriptor DESC_GITHUB_CHECKALL = create(PATH_OBJ, "checkall.gif"); //$NON-NLS-1$
+	public static final ImageDescriptor DESC_GITHUB_UNCHECKALL = create(PATH_OBJ, "uncheckall.gif"); //$NON-NLS-1$
 
 	private static ImageDescriptor create(String prefix, String name) {
 		return ImageDescriptor.createFromURL(makeImageURL(prefix, name));
@@ -51,6 +55,8 @@ public class GitHubImages {
 		manage(GITHUB_LOGO_OBJ, DESC_GITHUB_LOGO);
 		manage(GITHUB_ISSUE_LABEL_OBJ, DESC_GITHUB_ISSUE_LABEL);
 		manage(GITHUB_ADD_OBJ, DESC_GITHUB_ADD);
+		manage(GITHUB_CHECKALL_OBJ, DESC_GITHUB_CHECKALL);
+		manage(GITHUB_UNCHECKALL_OBJ, DESC_GITHUB_UNCHECKALL);
 	}
 
 	private static URL makeImageURL(String prefix, String name) {
