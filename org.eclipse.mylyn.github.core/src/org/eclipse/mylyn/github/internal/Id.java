89e6e655530b83dfeae60bc2f479979f12bd3a80
Kevin Sawicki <kevin@github.com>
2011-04-27 12:48:01 -0700

    Add pull request id and discussion model classes.
    Change-Id: Ia8d5894e271a7c2856329521a75f1e3132493cdc
    Signed-off-by: Kevin Sawicki <kevin@github.com>

---
+++ org.eclipse.mylyn.github.core/src/org/eclipse/mylyn/github/internal/Id.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ *  Copyright (c) 2011 GitHub Inc.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ * 
+ *  Contributors:
+ *    Kevin Sawicki (GitHub Inc.) - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.mylyn.github.internal;
+
+/**
+ * Id class.
+ */
+public class Id {
+
+	private String id;
+
+	/**
+	 * Get id
+	 * 
+	 * @return id
+	 */
+	public String getId() {
+		return this.id;
+	}
+
+}
