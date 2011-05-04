8a737de93fa82eaea3081942c30d6d34e9aa3e23
fbecker <>
2011-05-04 18:02:04 +0000

    ASSIGNED - bug 344489: NPE in PlatformUiUtil.supportsMultipleHyperlinkPresenter causing bugzilla to fail to load and error dialog https://bugs.eclipse.org/bugs/show_bug.cgi?id=344489

--- org.eclipse.mylyn.commons.ui/src/org/eclipse/mylyn/internal/provisional/commons/ui/PlatformUiUtil.java
+++ org.eclipse.mylyn.commons.ui/src/org/eclipse/mylyn/internal/provisional/commons/ui/PlatformUiUtil.java
@@ -142,9 +142,14 @@ public class PlatformUiUtil {
 	public static boolean supportsMultipleHyperlinkPresenter() {
 		if (isMac()) {
 			Bundle bundle = Platform.getBundle("org.eclipse.platform"); //$NON-NLS-1$
-			String versionString = (String) bundle.getHeaders().get("Bundle-Version"); //$NON-NLS-1$
-			Version version = new Version(versionString);
-			return version.compareTo(new Version("3.7.0.v201101192000")) >= 0; //$NON-NLS-1$
+			if (bundle != null) {
+				String versionString = (String) bundle.getHeaders().get("Bundle-Version"); //$NON-NLS-1$
+				Version version = new Version(versionString);
+				return version.compareTo(new Version("3.7.0.v201101192000")) >= 0; //$NON-NLS-1$
+			} else {
+				//TODO: change this to true when eclipse 3.6 reach end of live!
+				return false;
+			}
 		}
 		return true;
 	}
