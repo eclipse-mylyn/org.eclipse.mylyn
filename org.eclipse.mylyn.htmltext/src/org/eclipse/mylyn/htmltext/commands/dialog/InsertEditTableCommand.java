37562e01b75f51de194250be8aa5da0ace32c218
tseidel <tom.seidel@remus-software.org>
2011-05-08 02:20:23 +0200

    Bug 345066 - [Command] Provide a command to create tables

--- org.eclipse.mylyn.htmltext/src/org/eclipse/mylyn/htmltext/commands/dialog/InsertEditTableCommand.java
+++ org.eclipse.mylyn.htmltext/src/org/eclipse/mylyn/htmltext/commands/dialog/InsertEditTableCommand.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 Tom Seidel, Remus Software
+ * Copyright (c) 2011 Tom Seidel, Remus Software
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,8 +14,7 @@ package org.eclipse.mylyn.htmltext.commands.dialog;
 import org.eclipse.mylyn.htmltext.commands.Command;
 
 /**
- * @author tom
- *
+ * @author Tom Seidel <tom.seidel@remus-software.org>
  */
 public class InsertEditTableCommand extends Command {
 
