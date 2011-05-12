388b7b6aab46b49d8293c66eb75a54dddb694d8e
Christian Trutz <christian.trutz@gmail.com>
2011-05-12 22:52:18 +0200

    Unit tests for Repository.
    Change-Id: I770a575c60f9a51d94210d02eb6e3816fb04c051
    Signed-off-by: Christian Trutz <christian.trutz@gmail.com>

---
+++ mylyn.github/org.eclipse.egit.github.core.tests/src/org/eclipse/egit/github/core/tests/RepositoryTest.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ *  Copyright (c) 2011 Christian Trutz
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *    Christian Trutz - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.egit.github.core.tests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.eclipse.egit.github.core.Repository;
+import org.junit.Test;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+/**
+ * Tests for {@link Repository}.
+ */
+public class RepositoryTest {
+
+	private static final Gson gson = new GsonBuilder().setDateFormat(
+			"yyyy-MM-dd").create();
+
+	@Test
+	public void getCreatedAt_ReferenceMutableObject() {
+		Repository repository = gson.fromJson("{createdAt : '2003-10-10'}",
+				Repository.class);
+		repository.getCreatedAt().setTime(0);
+		assertTrue(repository.getCreatedAt().getTime() != 0);
+	}
+
+	@Test
+	public void getPushedAt_ReferenceMutableObject() {
+		Repository repository = gson.fromJson("{pushedAt : '2003-10-10'}",
+				Repository.class);
+		repository.getPushedAt().setTime(0);
+		assertTrue(repository.getPushedAt().getTime() != 0);
+	}
+
+}
