bd6f5e5268672afc17adfb98339a45347ccef4e8
spingel <>
2011-05-14 20:30:32 +0000

    NEW - bug 343602: [api][identity] add support for retrieving Gravatar information https://bugs.eclipse.org/bugs/show_bug.cgi?id=343602

---
+++ org.eclipse.mylyn.commons.identity/src/org/eclipse/mylyn/internal/commons/identity/gravatar/Messages.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ *  Copyright (c) 2011 GitHub Inc.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *    Kevin Sawicki (GitHub Inc.) - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.mylyn.internal.commons.identity.gravatar;
+
+import org.eclipse.osgi.util.NLS;
+
+/**
+ * NLS
+ * 
+ * @author Kevin Sawicki (kevin@github.com)
+ */
+public class Messages extends NLS {
+
+	private static final String BUNDLE_NAME = "org.eclipse.mylyn.commons.identity.gravatar.messages"; //$NON-NLS-1$
+
+	/**
+	 * GravatarStore_LoadingAvatar
+	 */
+	public static String GravatarStore_LoadingAvatar;
+
+	/**
+	 * GravatarStore_RefreshJobName
+	 */
+	public static String GravatarStore_RefreshJobName;
+
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
