8fd340ebc5d8aaf206df347ae2be872339040bb3
Kevin Sawicki <kevin@github.com>
2011-08-02 17:01:23 -0700

    Support error formatting in request exception class.
    Change-Id: I5a89615ea4cd585eff7b6464e7c83c6d24299aed
    Signed-off-by: Kevin Sawicki <kevin@github.com>

--- org.eclipse.egit.github.core/src/org/eclipse/egit/github/core/client/RequestException.java
+++ org.eclipse.egit.github.core/src/org/eclipse/egit/github/core/client/RequestException.java
@@ -11,7 +11,10 @@
 package org.eclipse.egit.github.core.client;
 
 import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.List;
 
+import org.eclipse.egit.github.core.FieldError;
 import org.eclipse.egit.github.core.RequestError;
 
 /**
@@ -20,6 +23,14 @@ import org.eclipse.egit.github.core.RequestError;
  * @author Kevin Sawicki (kevin@github.com)
  */
 public class RequestException extends IOException {
+
+	private static final String FIELD_INVALID_WITH_VALUE = "Invalid value of {0} for field {1}"; //$NON-NLS-1$
+
+	private static final String FIELD_INVALID = "Invalid value for field {1}"; //$NON-NLS-1$
+
+	private static final String FIELD_MISSING = "Missing required field {0}"; //$NON-NLS-1$
+
+	private static final String FIELD_ERROR = "Error with field {0} in {1} resource"; //$NON-NLS-1$
 
 	/**
 	 * serialVersionUID
@@ -47,7 +58,7 @@ public class RequestException extends IOException {
 	 * @return error
 	 */
 	public RequestError getError() {
-		return this.error;
+		return error;
 	}
 
 	/**
@@ -56,7 +67,49 @@ public class RequestException extends IOException {
 	 * @return status
 	 */
 	public int getStatus() {
-		return this.status;
+		return status;
 	}
 
+	/**
+	 * Format field error into human-readable message
+	 * 
+	 * @param error
+	 * @return formatted field error
+	 */
+	protected String format(FieldError error) {
+		String code = error.getCode();
+		String value = error.getValue();
+		String field = error.getField();
+		if (FieldError.CODE_INVALID.equals(code))
+			if (value != null)
+				return MessageFormat.format(FIELD_INVALID_WITH_VALUE, value,
+						field);
+			else
+				return MessageFormat.format(FIELD_INVALID, field, value);
+		else if (FieldError.CODE_MISSING_FIELD.equals(code))
+			return MessageFormat.format(FIELD_MISSING, field);
+		else
+			return MessageFormat
+					.format(FIELD_ERROR, field, error.getResource());
+	}
+
+	/**
+	 * Format all fields error into single human-readable message.
+	 * 
+	 * @return formatted message
+	 */
+	public String formatErrors() {
+		String errorMessage = error.getMessage();
+		if (errorMessage == null)
+			errorMessage = "";
+		StringBuilder message = new StringBuilder(errorMessage);
+		List<FieldError> errors = error.getErrors();
+		if (errors != null && errors.size() > 0) {
+			message.append(':');
+			for (FieldError fieldError : errors)
+				message.append(' ').append(format(fieldError)).append(',');
+			message.deleteCharAt(message.length() - 1);
+		}
+		return message.toString();
+	}
 }
