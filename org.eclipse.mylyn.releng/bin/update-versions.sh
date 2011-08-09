4f4aa88588d867b2eb4e3d1edf948f2038e73308
Steffen Pingel <steffen.pingel@tasktop.com>
2011-08-09 12:28:08 +0200

    updated versions for 3.6.2
    352398: [release] plan Mylyn 3.6.1 release
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=352398

--- org.eclipse.mylyn.releng/bin/update-versions.sh
+++ org.eclipse.mylyn.releng/bin/update-versions.sh
@@ -13,25 +13,34 @@ find -mindepth 4 -not -path "*feature*" -path "*org.eclipse.mylyn.reviews*" -nam
 update_features() {
 OLD=$1
 NEW=$2
+
+# feature versions
 find -path "*feature*" -name feature.xml | xargs sed -i -e "s/$OLD.qualifier/$NEW.qualifier/"
 find -path "*feature*" -name pom.xml | xargs sed -i -e "s/<version>$OLD-SNAPSHOT<\/version>/<version>$NEW-SNAPSHOT<\/version>/"
 
+# feature dependencies
 find -path "*feature*" -name feature.xml | xargs sed -i -e "s/\(mylyn.* version=\"\)$OLD\(\" match=\"equivalent\".*\)/\1$NEW\2/"
 
+# parent poms
 find -name pom.xml | xargs sed -i -e "s/^    <version>$OLD-SNAPSHOT<\/version>/    <version>$NEW-SNAPSHOT<\/version>/"
 find -maxdepth 2 -not -path "*feature*" -not -path "*org.eclipse.mylyn.reviews*" -name pom.xml | xargs sed -i -e "s/^  <version>$OLD-SNAPSHOT<\/version>/  <version>$NEW-SNAPSHOT<\/version>/"
 find -maxdepth 3 -not -path "*feature*" -path "*org.eclipse.mylyn.reviews*" -name pom.xml | xargs sed -i -e "s/^  <version>$OLD-SNAPSHOT<\/version>/  <version>$NEW-SNAPSHOT<\/version>/"
+
+# org.eclipse.mylyn
+find org.eclipse.mylyn/org.eclipse.mylyn.releng -name pom.xml | xargs sed -i -e "s/<version>$OLD-SNAPSHOT<\/version>/<version>$NEW-SNAPSHOT<\/version>/"
+sed -i -e "s/<mylyn-version>$OLD-SNAPSHOT<\/mylyn-version>/<mylyn-version>$NEW-SNAPSHOT<\/mylyn-version>/" org.eclipse.mylyn/org.eclipse.mylyn.releng/pom.xml
+
 }
 
-update_features 0.8.0 0.8.1
-update_features 1.5.0 1.5.1
-update_features 3.6.0 3.6.1
-update_features 5.2.300 5.2.301
+update_features 0.8.1 0.8.2
+update_features 1.5.1 1.5.2
+update_features 3.6.1 3.6.2
+update_features 5.2.301 5.2.302
 
-update_qualifier 0.8.0 v20110608-1400
-update_qualifier 1.5.0 v20110608-1400
-update_qualifier 3.6.0 v20110608-1400
+#update_qualifier 0.8.0 v20110608-1400
+#update_qualifier 1.5.0 v20110608-1400
+#update_qualifier 3.6.0 v20110608-1400
 
-echo "Fix version in org.eclipse.mylyn/org.eclipse.mylyn.releng"
+#echo "Fix version in org.eclipse.mylyn/org.eclipse.mylyn.releng"
 
 #grep Bundle-Version */META-INF/MANIFEST.MF | grep -v 0.9.0 | grep -v 3.7.0 | grep -v 1.5.0
