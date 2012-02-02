19a93c87dceaa01f336f6f2537ab3257adde5243
Steffen Pingel <steffen.pingel@tasktop.com>
2012-02-02 15:13:00 +0100

    REOPENED - bug 369697: fix test failures on Java 1.7 https://bugs.eclipse.org/bugs/show_bug.cgi?id=369697

--- org.eclipse.mylyn.trac.tests/src/org/eclipse/mylyn/trac/tests/core/TracRepositoryConnectorTest.java
+++ org.eclipse.mylyn.trac.tests/src/org/eclipse/mylyn/trac/tests/core/TracRepositoryConnectorTest.java
@@ -268,19 +268,20 @@ public class TracRepositoryConnectorTest extends TestCase {
 		TaskData taskData = taskDataHandler.createTaskDataFromTicket(client, repository, ticket, null);
 
 		ITask task = TasksUi.getRepositoryModel().createTask(repository, taskData.getTaskId());
-		task.setPriority("P1");
+		task.setPriority("P2");
 
 		// create task from task data
 		connector.updateTaskFromTaskData(repository, task, taskData);
 		assertEquals(repository.getRepositoryUrl() + ITracClient.TICKET_URL + "456", task.getUrl());
 		assertEquals("456", task.getTaskKey());
 		assertEquals("mysummary", task.getSummary());
-		assertEquals("P3", task.getPriority());
 		// depending on the access mode createTaskDataFromTicket() creates different default attributes  
 		if (client.getAccessMode() == Version.TRAC_0_9) {
 			assertEquals(AbstractTask.DEFAULT_TASK_KIND, task.getTaskKind());
+			assertEquals("P2", task.getPriority());
 		} else {
 			assertEquals("Defect", task.getTaskKind());
+			assertEquals("P3", task.getPriority());
 		}
 	}
 
