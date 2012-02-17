388cbef05d6c24a777ba7df2ab599ac49cd37af9
Steffen Pingel <steffen.pingel@tasktop.com>
2012-02-17 11:54:46 +0100

    NEW - bug 356914: NPE when activating a task having another task open with same invisible context elements https://bugs.eclipse.org/bugs/show_bug.cgi?id=356914
    Change-Id: Id3d7bd32c7cab6799d858a492ed7a65560ede676

--- org.eclipse.mylyn.context.tasks.ui/src/org/eclipse/mylyn/internal/context/tasks/ui/editors/ContextWrapper.java
+++ org.eclipse.mylyn.context.tasks.ui/src/org/eclipse/mylyn/internal/context/tasks/ui/editors/ContextWrapper.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2008 Tasktop Technologies and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tasktop Technologies - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.mylyn.internal.context.tasks.ui.editors;
 
 import java.util.Collection;
@@ -9,7 +19,11 @@ import org.eclipse.mylyn.context.core.IInteractionElement;
 import org.eclipse.mylyn.monitor.core.InteractionEvent;
 import org.eclipse.mylyn.tasks.core.ITask;
 
+/**
+ * @author Shawn Minto
+ */
 class ContextWrapper implements IInteractionContext {
+
 	private IInteractionContext wrappedContext;
 
 	private final ITask task;
@@ -20,7 +34,7 @@ class ContextWrapper implements IInteractionContext {
 	}
 
 	public boolean isForSameTaskAs(IInteractionContext context) {
-		return context != null && context.getHandleIdentifier().equals(task.getHandleIdentifier());
+		return context != null && isForSameTaskAs(context.getHandleIdentifier());
 	}
 
 	public boolean isForSameTaskAs(String contextHandle) {
