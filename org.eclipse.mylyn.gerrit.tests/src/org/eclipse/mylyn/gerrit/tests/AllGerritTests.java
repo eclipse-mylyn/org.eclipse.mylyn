45bb797219f57704bf83d045b3d60e4c254e2dee
Steffen Pingel <steffen.pingel@tasktop.com>
2012-03-01 15:53:27 +0100

    setup integration tests on slave7
    Bug: 372971
    Change-Id: I61ed15252a1e8c656cd7b36602adfb762bd62a96

--- org.eclipse.mylyn.gerrit.tests/src/org/eclipse/mylyn/gerrit/tests/AllGerritTests.java
+++ org.eclipse.mylyn.gerrit.tests/src/org/eclipse/mylyn/gerrit/tests/AllGerritTests.java
@@ -28,25 +28,32 @@ public class AllGerritTests {
 
 	public static Test suite() {
 		TestSuite suite = new ManagedTestSuite(AllGerritTests.class.getName());
-		addTests(CommonTestUtil.runHeartbeatTestsOnly(), suite);
+		addTests(false, CommonTestUtil.runHeartbeatTestsOnly(), suite);
 		return suite;
 	}
 
 	public static Test suite(boolean defaultOnly) {
 		TestSuite suite = new TestSuite(AllGerritTests.class.getName());
-		addTests(defaultOnly, suite);
+		addTests(false, defaultOnly, suite);
 		return suite;
 	}
 
-	private static void addTests(boolean defaultOnly, TestSuite suite) {
-		suite.addTestSuite(GerritUrlHandlerTest.class);
+	public static Test localSuite() {
+		TestSuite suite = new TestSuite(AllGerritTests.class.getName());
+		addTests(true, CommonTestUtil.runHeartbeatTestsOnly(), suite);
+		return suite;
+	}
 
-		// network tests
-		for (GerritFixture fixture : GerritFixture.ALL) {
-			fixture.createSuite(suite);
-			fixture.add(GerritClientTest.class);
-			fixture.add(GerritConnectorTest.class);
-			fixture.done();
+	private static void addTests(boolean localOnly, boolean defaultOnly, TestSuite suite) {
+		suite.addTestSuite(GerritUrlHandlerTest.class);
+		if (!localOnly) {
+			// network tests
+			for (GerritFixture fixture : GerritFixture.ALL) {
+				fixture.createSuite(suite);
+				fixture.add(GerritClientTest.class);
+				fixture.add(GerritConnectorTest.class);
+				fixture.done();
+			}
 		}
 	}
 
