d5c36fd4dde115857867709788b4447ce28e4929
Carsten Hammer <carsten.hammer@oce.com>
2012-07-13 16:10:46 -0700

    bug 383400: MediaWikiApiImageFetchingStrategy.java does not fetch all images https://bugs.eclipse.org/bugs/show_bug.cgi?id=383400
    Change-Id: I0000000000000000000000000000000000000005

--- org.eclipse.mylyn.wikitext.mediawiki.core/src/org/eclipse/mylyn/internal/wikitext/mediawiki/core/tasks/MediaWikiApiImageFetchingStrategy.java
+++ org.eclipse.mylyn.wikitext.mediawiki.core/src/org/eclipse/mylyn/internal/wikitext/mediawiki/core/tasks/MediaWikiApiImageFetchingStrategy.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2010 David Green and others.
+ * Copyright (c) 2007, 2012 David Green and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -56,7 +56,7 @@ class MediaWikiApiImageFetchingStrategy extends ImageFetchingStrategy {
 			throw new BuildException("please specify @pageName"); //$NON-NLS-1$
 		}
 		if (!pageName.equals(pageName.trim())) {
-			throw new BuildException("@pageName must not have leading or training whitespace"); //$NON-NLS-1$
+			throw new BuildException("@pageName must not have leading or trailing whitespace"); //$NON-NLS-1$
 		}
 
 		String base;
@@ -69,90 +69,101 @@ class MediaWikiApiImageFetchingStrategy extends ImageFetchingStrategy {
 			base += "/"; //$NON-NLS-1$
 		}
 
-		URL apiUrl;
-		try {
-			String queryString = String.format(
-					"action=query&titles=%s&generator=images&prop=imageinfo&iiprop=url&format=xml", URLEncoder.encode(pageName, "UTF-8")); //$NON-NLS-1$ //$NON-NLS-2$
-			apiUrl = new URL(base + "api.php?" + queryString); //$NON-NLS-1$
-		} catch (Exception e) {
-			throw new BuildException("Cannot compose API URL", e); //$NON-NLS-1$
-		}
-
+		ImageFetchingContentHandler contentHandler = new ImageFetchingContentHandler();
+		String gimcontinue = null;
 		Set<String> filenames = new HashSet<String>();
-
 		final SAXParserFactory parserFactory = SAXParserFactory.newInstance();
 		parserFactory.setNamespaceAware(true);
 		parserFactory.setValidating(false);
+		int maxloop = 100;
+		do {
+			contentHandler.setGimcontinue(null);
+			URL apiUrl;
+			try {
+				String queryString = String.format(
+						"action=query&titles=%s&generator=images&prop=imageinfo&iiprop=url&format=xml%s", //$NON-NLS-1$
+						URLEncoder.encode(pageName, "UTF-8"), (gimcontinue == null ? "" : "&gimcontinue=" + URLEncoder.encode(gimcontinue, "UTF-8"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+				apiUrl = new URL(base + "api.php?" + queryString); //$NON-NLS-1$
+			} catch (Exception e) {
+				throw new BuildException("Cannot compose API URL", e); //$NON-NLS-1$
+			}
 
-		Reader input;
-		try {
-			input = new InputStreamReader(new BufferedInputStream(apiUrl.openStream()), "UTF-8"); //$NON-NLS-1$
-		} catch (IOException e) {
-			throw new BuildException(String.format("Cannot contact %s: %s", apiUrl, e.getMessage()), e); //$NON-NLS-1$
-		}
-		try {
-			SAXParser saxParser = parserFactory.newSAXParser();
-			XMLReader xmlReader = saxParser.getXMLReader();
-			xmlReader.setEntityResolver(IgnoreDtdEntityResolver.getInstance());
+			Reader input;
+			try {
 
-			ImageFetchingContentHandler contentHandler = new ImageFetchingContentHandler();
-			xmlReader.setContentHandler(contentHandler);
+				log("Fetching " + apiUrl, Project.MSG_VERBOSE); //$NON-NLS-1$
+
+				input = new InputStreamReader(new BufferedInputStream(apiUrl.openStream()), "UTF-8"); //$NON-NLS-1$
+			} catch (IOException e) {
+				throw new BuildException(String.format("Cannot contact %s: %s", apiUrl, e.getMessage()), e); //$NON-NLS-1$
+			}
 
 			try {
-				xmlReader.parse(new InputSource(input));
-			} catch (IOException e) {
-				throw new BuildException(String.format("Unexpected exception retrieving data from %s", apiUrl), e); //$NON-NLS-1$
-			} finally {
+				SAXParser saxParser = parserFactory.newSAXParser();
+				XMLReader xmlReader = saxParser.getXMLReader();
+				xmlReader.setEntityResolver(IgnoreDtdEntityResolver.getInstance());
+
+				xmlReader.setContentHandler(contentHandler);
+
 				try {
-					input.close();
+					xmlReader.parse(new InputSource(input));
+					gimcontinue = contentHandler.getGimcontinue();
 				} catch (IOException e) {
-					// ignore
-				}
-			}
-			int fileCount = 0;
-			for (Map.Entry<String, String> ent : contentHandler.imageTitleToUrl.entrySet()) {
-				String title = ent.getKey();
-				String imageUrl = ent.getValue();
-				Matcher titleMatcher = imageTitlePattern.matcher(title);
-				if (titleMatcher.matches()) {
-					String name = titleMatcher.group(1);
-					name = name.replace(' ', '_');
-					String qualifiedUrl = base;
-					if (imageUrl.matches("https?://.*")) { //$NON-NLS-1$
-						qualifiedUrl = imageUrl;
-					} else {
-						if (imageUrl.startsWith("/")) { //$NON-NLS-1$
-							qualifiedUrl += imageUrl.substring(0);
-						} else {
-							qualifiedUrl += imageUrl;
-						}
-					}
-
-					log("Fetching " + qualifiedUrl, Project.MSG_INFO); //$NON-NLS-1$
-					Get get = new Get();
-					get.setProject(getProject());
-					get.setLocation(getLocation());
+					throw new BuildException(String.format("Unexpected exception retrieving data from %s", apiUrl), e); //$NON-NLS-1$
+				} finally {
 					try {
-						get.setSrc(new URL(qualifiedUrl));
-					} catch (MalformedURLException e) {
-						log("Skipping " + url + ": " + e.getMessage(), Project.MSG_WARN); //$NON-NLS-1$ //$NON-NLS-2$
-						continue;
+						input.close();
+					} catch (IOException e) {
+						// ignore
 					}
-					get.setDest(new File(dest, name));
-					get.execute();
-
-					filenames.add(name);
-					++fileCount;
-				} else {
-					log(String.format("Unexpected title format: %s", title), Project.MSG_WARN); //$NON-NLS-1$
 				}
+			} catch (SAXException e) {
+				throw new BuildException("Unexpected error in XML content", e); //$NON-NLS-1$
+			} catch (ParserConfigurationException e) {
+				throw new BuildException("Cannot configure SAX parser", e); //$NON-NLS-1$
 			}
-			log("Fetched " + fileCount + " image files for " + pageName, Project.MSG_INFO); //$NON-NLS-1$ //$NON-NLS-2$
-		} catch (SAXException e) {
-			throw new BuildException("Unexpected error in XML content", e); //$NON-NLS-1$
-		} catch (ParserConfigurationException e) {
-			throw new BuildException("Cannot configure SAX parser", e); //$NON-NLS-1$
+
+		} while (gimcontinue != null && maxloop-- > 0);
+		int fileCount = 0;
+		for (Map.Entry<String, String> ent : contentHandler.imageTitleToUrl.entrySet()) {
+			String title = ent.getKey();
+			String imageUrl = ent.getValue();
+			Matcher titleMatcher = imageTitlePattern.matcher(title);
+			if (titleMatcher.matches()) {
+				String name = titleMatcher.group(1);
+				name = name.replace(' ', '_');
+				String qualifiedUrl = base;
+				if (imageUrl.matches("https?://.*")) { //$NON-NLS-1$
+					qualifiedUrl = imageUrl;
+				} else {
+					if (imageUrl.startsWith("/")) { //$NON-NLS-1$
+						qualifiedUrl += imageUrl.substring(0);
+					} else {
+						qualifiedUrl += imageUrl;
+					}
+				}
+
+				log("Fetching " + qualifiedUrl, Project.MSG_INFO); //$NON-NLS-1$
+				Get get = new Get();
+				get.setProject(getProject());
+				get.setLocation(getLocation());
+				try {
+					get.setSrc(new URL(qualifiedUrl));
+				} catch (MalformedURLException e) {
+					log("Skipping " + url + ": " + e.getMessage(), Project.MSG_WARN); //$NON-NLS-1$ //$NON-NLS-2$
+					continue;
+				}
+				get.setDest(new File(dest, name));
+				get.execute();
+
+				filenames.add(name);
+				++fileCount;
+			} else {
+				log(String.format("Unexpected title format: %s", title), Project.MSG_WARN); //$NON-NLS-1$
+			}
 		}
+		log("Fetched " + fileCount + " image files for " + pageName, Project.MSG_INFO); //$NON-NLS-1$ //$NON-NLS-2$
+
 		return filenames;
 	}
 
@@ -174,15 +185,27 @@ class MediaWikiApiImageFetchingStrategy extends ImageFetchingStrategy {
 
 	private class ImageFetchingContentHandler implements ContentHandler {
 
-		final Map<String, String> imageTitleToUrl = new HashMap<String, String>();
+		private final Map<String, String> imageTitleToUrl = new HashMap<String, String>();
 
 		private String currentPage = null;
 
 		private boolean inImageInfo = false;
 
+		private String gimcontinue = null;
+
+		public void setGimcontinue(String gimcontinue) {
+			this.gimcontinue = gimcontinue;
+		}
+
+		public String getGimcontinue() {
+			return gimcontinue;
+		}
+
 		public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException {
 			if ("page".equals(localName)) { //$NON-NLS-1$
 				currentPage = atts.getValue("title"); //$NON-NLS-1$
+			} else if ("images".equals(localName)) { //$NON-NLS-1$
+				gimcontinue = atts.getValue("gimcontinue"); //$NON-NLS-1$
 			} else if ("imageinfo".equals(localName)) { //$NON-NLS-1$
 				inImageInfo = true;
 			} else if (inImageInfo && "ii".equals(localName)) { //$NON-NLS-1$
