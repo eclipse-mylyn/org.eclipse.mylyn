ea40a20f46fa7bf61c9950358fda5ac84acb6dfe
Tomasz Zarna <tzarna@gmail.com>
2012-10-16 19:24:08 +0200

    NPE while activating a task: Restoring of editor state failed
    Bug: 391121
    Change-Id: I252a6138d4c252333a98cc654844b44dc14def48

--- org.eclipse.mylyn.context.ui/src/org/eclipse/mylyn/internal/context/ui/state/EditorStateParticipant.java
+++ org.eclipse.mylyn.context.ui/src/org/eclipse/mylyn/internal/context/ui/state/EditorStateParticipant.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2009 Tasktop Technologies and others.
+ * Copyright (c) 2004, 2012 Tasktop Technologies and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -431,7 +431,7 @@ public class EditorStateParticipant extends ContextStateParticipant {
 					IPath path = ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(pathString)).getLocation();
 					for (IEditorReference existingReference : page.getEditorReferences()) {
 						if (existingReference.getEditorInput() instanceof IPathEditorInput
-								&& (path.equals(((IPathEditorInput) existingReference.getEditorInput()).getPath()))) {
+								&& (((IPathEditorInput) existingReference.getEditorInput()).getPath().equals(path))) {
 							// there is already an editor showing the same file
 							found = true;
 							break;
