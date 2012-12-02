aa90104794d6fbb0a2472cd3da44e1fb007c8832
Steffen Pingel <steffen.pingel@tasktop.com>
2012-12-02 23:00:30 +0100

    bug 395565: [release] plan Mylyn 3.8.3 release
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=395565

--- org.eclipse.mylyn.releng/bin/update-versions.sh
+++ org.eclipse.mylyn.releng/bin/update-versions.sh
@@ -10,10 +10,10 @@ find -name feature.xml | xargs sed -i -e "s/\(mylyn.* version=\"\)$OLD\(\" match
 
 #mvn -Dtycho.mode=maven org.sonatype.tycho:tycho-versions-plugin:set-version -DnewVersion=1.4.1-SNAPSHOT
 
-update 1.0.1 1.0.2
-update 1.7.1 1.7.2
-update 3.8.1 3.8.2
-update 5.4.1 5.4.2
+update 1.0.2 1.0.3
+update 1.7.2 1.7.3
+update 3.8.2 3.8.3
+update 5.4.2 5.4.3
 
  
 #grep Bundle-Version */META-INF/MANIFEST.MF | grep -v 0.9.0 | grep -v 3.7.0 | grep -v 1.5.0
