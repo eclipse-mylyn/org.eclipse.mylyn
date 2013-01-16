d06ff2d5026b525e90a6d0424a8b0e3e431c2b21
Steffen Pingel <steffen.pingel@tasktop.com>
2013-01-16 23:04:33 +0100

    384269: fix updating of feature versions
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=384269

--- org.eclipse.mylyn.releng/bin/update-versions.sh
+++ org.eclipse.mylyn.releng/bin/update-versions.sh
@@ -6,6 +6,8 @@ find -name feature.xml | xargs sed -i -e "s/$OLD.qualifier/$NEW.qualifier/"
 find -name pom.xml | xargs sed -i -e "s/<version>$OLD-SNAPSHOT<\/version>/<version>$NEW-SNAPSHOT<\/version>/"
 
 find -name feature.xml | xargs sed -i -e "s/\(mylyn.* version=\"\)$OLD\(\" match=\"equivalent\".*\)/\1$NEW\2/"
+find -name feature.xml | xargs sed -i -e "s/\(mylyn.* version=\"\)$OLD\(\" match=\"compatible\".*\)/\1$NEW\2/"
+find -name feature.xml | xargs sed -i -e "s/\(mylyn.* version=\"\)$OLD\(\" match=\"greaterOrEqual\".*\)/\1$NEW\2/"
 }
 
 #mvn -Dtycho.mode=maven org.sonatype.tycho:tycho-versions-plugin:set-version -DnewVersion=1.4.1-SNAPSHOT
