e505f4180817c3f4317a7439302ba36ad8b45e43
Miles Parker <milesparker@gmail.com>
2013-06-11 13:18:47 -0700

    410515: cannot add reviewers to merged reviews
    Change-Id: Ia096945c3c8a1afd3cd3126873c3211ba54ccd11
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=410515

--- org.eclipse.mylyn.gerrit.ui/src/org/eclipse/mylyn/internal/gerrit/ui/factories/AddReviewersUiFactory.java
+++ org.eclipse.mylyn.gerrit.ui/src/org/eclipse/mylyn/internal/gerrit/ui/factories/AddReviewersUiFactory.java
@@ -13,7 +13,6 @@ package org.eclipse.mylyn.internal.gerrit.ui.factories;
 
 import org.eclipse.mylyn.internal.gerrit.ui.operations.AddReviewersDialog;
 import org.eclipse.mylyn.reviews.core.model.IReview;
-import org.eclipse.mylyn.reviews.core.model.ReviewStatus;
 import org.eclipse.mylyn.reviews.ui.spi.factories.AbstractUiFactory;
 import org.eclipse.mylyn.reviews.ui.spi.factories.IUiContext;
 
@@ -39,6 +38,6 @@ public class AddReviewersUiFactory extends AbstractUiFactory<IReview> {
 
 	@Override
 	public boolean isExecutable() {
-		return getModelObject().getState() == null || getModelObject().getState() == ReviewStatus.NEW;
+		return true;
 	}
 }
