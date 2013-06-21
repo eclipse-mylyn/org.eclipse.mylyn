ae52150c8445dae4123058280d5db9ac50cd005d
Steffen Pingel <steffen.pingel@tasktop.com>
2013-06-21 15:00:03 +0200

    409767: update versions to 3.10
    Change-Id: I37c5577caf3f3e56a76fb252e6d0def5e8d4b40b
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=409767

--- org.eclipse.mylyn.releng/bin/update-versions.sh
+++ org.eclipse.mylyn.releng/bin/update-versions.sh
@@ -1,21 +1,22 @@
 update() {
 OLD=$1
 NEW=$2
-find -name MANIFEST.MF | xargs sed -i -e "s/Bundle-Version: $OLD.qualifier/Bundle-Version: $NEW.qualifier/"
-find -name feature.xml | xargs sed -i -e "s/$OLD.qualifier/$NEW.qualifier/"
-find -name pom.xml | xargs sed -i -e "s/<version>$OLD-SNAPSHOT<\/version>/<version>$NEW-SNAPSHOT<\/version>/"
+find . -name MANIFEST.MF | xargs sed -i~ -e "s/Bundle-Version: $OLD.qualifier/Bundle-Version: $NEW.qualifier/"
+find . -name feature.xml | xargs sed -i~ -e "s/$OLD.qualifier/$NEW.qualifier/"
+find . -name pom.xml | xargs sed -i~ -e "s/<version>$OLD-SNAPSHOT<\/version>/<version>$NEW-SNAPSHOT<\/version>/"
 
-find -name feature.xml | xargs sed -i -e "s/\(mylyn.* version=\"\)$OLD\(\" match=\"equivalent\".*\)/\1$NEW\2/"
-find -name feature.xml | xargs sed -i -e "s/\(mylyn.* version=\"\)$OLD\(\" match=\"compatible\".*\)/\1$NEW\2/"
-find -name feature.xml | xargs sed -i -e "s/\(mylyn.* version=\"\)$OLD\(\" match=\"greaterOrEqual\".*\)/\1$NEW\2/"
+find . -name feature.xml | xargs sed -i~ -e "s/\(mylyn.* version=\"\)$OLD\(\" match=\"equivalent\".*\)/\1$NEW\2/"
+find . -name feature.xml | xargs sed -i~ -e "s/\(mylyn.* version=\"\)$OLD\(\" match=\"compatible\".*\)/\1$NEW\2/"
+find . -name feature.xml | xargs sed -i~ -e "s/\(mylyn.* version=\"\)$OLD\(\" match=\"greaterOrEqual\".*\)/\1$NEW\2/"
 }
 
 #mvn -Dtycho.mode=maven org.sonatype.tycho:tycho-versions-plugin:set-version -DnewVersion=1.4.1-SNAPSHOT
 
-update 1.0.0 1.1.0
-update 1.7.0 1.8.0
-update 3.8.0 3.9.0
-update 5.4.0 5.5.0
+update 1.1.0 1.2.0
+update 1.8.0 1.9.0
+update 2.0.0 2.0.1
+update 3.9.0 3.10.0
+update 5.5.0 5.6.0
 
  
 #grep Bundle-Version */META-INF/MANIFEST.MF | grep -v 0.9.0 | grep -v 3.7.0 | grep -v 1.5.0
