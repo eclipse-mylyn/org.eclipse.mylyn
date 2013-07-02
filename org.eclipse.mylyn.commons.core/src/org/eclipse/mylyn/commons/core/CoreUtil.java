92e72521d438b82c4dac51daddf8959375a84549
Steffen Pingel <steffen.pingel@tasktop.com>
2013-07-02 15:50:39 +0200

    409767: update discovery URL to 3.10
    Change-Id: Ib0b1a8a18a80f6b83db47b3dd524e73588e9f3f0
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=409767

--- org.eclipse.mylyn.commons.core/src/org/eclipse/mylyn/commons/core/CoreUtil.java
+++ org.eclipse.mylyn.commons.core/src/org/eclipse/mylyn/commons/core/CoreUtil.java
@@ -46,7 +46,7 @@ public class CoreUtil {
 		}
 	}
 
-	private static final String FRAMEWORK_VERSION = "3.9.0"; //$NON-NLS-1$
+	private static final String FRAMEWORK_VERSION = "3.10.0"; //$NON-NLS-1$
 
 	private static final SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
 	static {
