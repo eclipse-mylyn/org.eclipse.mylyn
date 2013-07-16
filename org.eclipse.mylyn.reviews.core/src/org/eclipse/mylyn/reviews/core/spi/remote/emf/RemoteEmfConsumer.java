413b9957771deca41aac80e0318a679b4258b47a
Miles Parker <milesparker@gmail.com>
2013-07-16 15:42:08 -0700

    413124: Deadlock when opening editor
        •Reverts 669a792ecd777e38556d1435f0ce9ccd4f46f557
    
    Change-Id: Id7dd0f3d24c7c1bc3421cb4c46c5af4e426afda0
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=413124

--- org.eclipse.mylyn.reviews.core/src/org/eclipse/mylyn/reviews/core/spi/remote/emf/RemoteEmfConsumer.java
+++ org.eclipse.mylyn.reviews.core/src/org/eclipse/mylyn/reviews/core/spi/remote/emf/RemoteEmfConsumer.java
@@ -106,8 +106,8 @@ public class RemoteEmfConsumer<EParentObjectType extends EObject, EObjectType, L
 
 	RemoteEmfConsumer(
 			AbstractRemoteEmfFactory<EParentObjectType, EObjectType, LocalKeyType, RemoteType, RemoteKeyType, ObjectCurrentType> factory,
-			final EParentObjectType parent, final EObjectType modelObject, LocalKeyType localKey,
-			RemoteType remoteObject, RemoteKeyType remoteKey) {
+			EParentObjectType parent, EObjectType modelObject, LocalKeyType localKey, RemoteType remoteObject,
+			RemoteKeyType remoteKey) {
 		this.parentObject = parent;
 		this.modelObject = modelObject;
 		this.remoteObject = remoteObject;
@@ -121,17 +121,9 @@ public class RemoteEmfConsumer<EParentObjectType extends EObject, EObjectType, L
 			this.localKey = factory.getLocalKey(null, modelObject);
 		}
 		if (modelObject instanceof EObject) {
-			getFactory().getService().modelExec(new Runnable() {
-				public void run() {
-					((EObject) modelObject).eAdapters().add(adapter);
-				}
-			});
+			((EObject) modelObject).eAdapters().add(adapter);
 		} else if (parent != null) {
-			getFactory().getService().modelExec(new Runnable() {
-				public void run() {
-					parent.eAdapters().add(adapter);
-				}
-			});
+			parent.eAdapters().add(adapter);
 		}
 		remoteEmfObservers = new ArrayList<IRemoteEmfObserver<EParentObjectType, EObjectType, LocalKeyType, ObjectCurrentType>>();
 	}
@@ -312,14 +304,10 @@ public class RemoteEmfConsumer<EParentObjectType extends EObject, EObjectType, L
 	@Override
 	public void dispose() {
 		retrieving = false;
-		getFactory().getService().modelExec(new Runnable() {
-			public void run() {
-				parentObject.eAdapters().remove(adapter);
-				if (modelObject instanceof EObject) {
-					((EObject) modelObject).eAdapters().remove(adapter);
-				}
-			}
-		});
+		parentObject.eAdapters().remove(adapter);
+		if (modelObject instanceof EObject) {
+			((EObject) modelObject).eAdapters().remove(adapter);
+		}
 		synchronized (remoteEmfObservers) {
 			remoteEmfObservers.clear();
 		}
