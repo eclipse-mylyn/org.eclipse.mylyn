4ef9f9a67d1156d86b3c82cbc26b4e3473319512
David Green <david.green@tasktop.com>
2013-10-25 18:49:40 -0700

    415140: CorruptIndexException in tasklist index
    Change-Id: Ia0c6ac54f23f90e32d157f5deba34b3d322aa3c4
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=415140

--- org.eclipse.mylyn.tasks.index.core/src/org/eclipse/mylyn/internal/tasks/index/core/TaskListIndex.java
+++ org.eclipse.mylyn.tasks.index.core/src/org/eclipse/mylyn/internal/tasks/index/core/TaskListIndex.java
@@ -54,6 +54,7 @@ import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.LockObtainFailedException;
 import org.apache.lucene.store.NIOFSDirectory;
 import org.apache.lucene.util.Version;
@@ -1118,7 +1119,7 @@ public class TaskListIndex implements ITaskDataManagerListener, ITaskListChangeL
 	}
 
 	private void indexQueuedTasks(SubMonitor monitor) throws CorruptIndexException, LockObtainFailedException,
-			IOException {
+			IOException, CoreException {
 
 		synchronized (reindexQueue) {
 			if (reindexQueue.isEmpty()) {
@@ -1148,8 +1149,16 @@ public class TaskListIndex implements ITaskDataManagerListener, ITaskListChangeL
 					}
 
 					if (writer == null) {
-						writer = new IndexWriter(directory, TaskAnalyzer.instance(), false,
-								IndexWriter.MaxFieldLength.UNLIMITED);
+						try {
+							writer = createIndexWriter(false);
+						} catch (CorruptIndexException e) {
+							rebuildIndex = true;
+							synchronized (reindexQueue) {
+								reindexQueue.clear();
+							}
+							rebuildIndexCompletely(monitor);
+							return;
+						}
 					}
 
 					monitor.setWorkRemaining(workingQueue.size());
@@ -1202,8 +1211,17 @@ public class TaskListIndex implements ITaskDataManagerListener, ITaskListChangeL
 
 		monitor.beginTask(Messages.TaskListIndex_task_rebuilding_index, taskListState.indexableTasks.size());
 		try {
-			final IndexWriter writer = new IndexWriter(directory, TaskAnalyzer.instance(), true,
-					IndexWriter.MaxFieldLength.UNLIMITED);
+			IndexWriter writer;
+			try {
+				writer = createIndexWriter(true);
+			} catch (CorruptIndexException e) {
+				if (directory instanceof FSDirectory) {
+					cleanDirectory(((FSDirectory) directory).getFile());
+					writer = createIndexWriter(true);
+				} else {
+					throw e;
+				}
+			}
 			try {
 
 				for (ITask task : taskListState.indexableTasks) {
@@ -1228,6 +1246,25 @@ public class TaskListIndex implements ITaskDataManagerListener, ITaskListChangeL
 			monitor.done();
 		}
 		return multiStatus;
+	}
+
+	private void cleanDirectory(File file) throws IOException {
+		if (file.exists()) {
+			File[] children = file.listFiles();
+			if (children != null) {
+				for (File child : children) {
+					if (child.isDirectory()) {
+						cleanDirectory(child);
+					}
+					child.delete();
+				}
+			}
+		}
+	}
+
+	protected IndexWriter createIndexWriter(boolean create) throws CorruptIndexException, LockObtainFailedException,
+			IOException {
+		return new IndexWriter(directory, TaskAnalyzer.instance(), create, IndexWriter.MaxFieldLength.UNLIMITED);
 	}
 
 	/**
