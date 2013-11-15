17be84216645ebcea03782cc74bfe74f91c468a2
Frank Becker <Frank@Frank-Becker.de>
2013-11-15 05:02:41 +0100

    414360: create new projects and feature
    Change-Id: Iae16ae2bbfe086ff14111be738110fea800ea3e4
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=414360

---
+++ connector-bugzilla-rest/org.eclipse.mylyn.bugzilla.rest.core/src/org/eclipse/mylyn/internal/bugzilla/rest/core/BugzillaRestCorePlugin.java
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Frank Becker and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Frank Becker - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.mylyn.internal.bugzilla.rest.core;
+
+
+public class BugzillaRestCorePlugin  {
+	public static final String CONNECTOR_KIND = "org.eclipse.mylyn.bugzilla.rest"; //$NON-NLS-1$
+}
