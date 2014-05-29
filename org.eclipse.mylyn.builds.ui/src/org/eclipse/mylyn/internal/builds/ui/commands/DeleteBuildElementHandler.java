08f2fc005314ec78696265cfd7fa9f3dad27399a
Maarten Meijer <mm105@xs4all.nl>
2014-05-29 11:26:31 +0200

    340986: add facility to remove (Hudson) build server from Builds list
    - added handler and menu to plugin.xml
    - added confirmation dialog to DeleteBuildElementHandler as requested
      in earlier Gerrit review 23257
    - updated copyrights
      
    Change-Id: If418c1a3f11653bff5e53ac6993b1bd7a234ebde
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=340986
    Signed-off-by: Maarten Meijer <mm105@xs4all.nl>

--- org.eclipse.mylyn.builds.ui/src/org/eclipse/mylyn/internal/builds/ui/commands/DeleteBuildElementHandler.java
+++ org.eclipse.mylyn.builds.ui/src/org/eclipse/mylyn/internal/builds/ui/commands/DeleteBuildElementHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 Tasktop Technologies and others.
+ * Copyright (c) 2010,2014 Tasktop Technologies and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     Tasktop Technologies - initial API and implementation
+ *     Maarten Meijer       - added confirmation dialog bug#340986
  *******************************************************************************/
 
 package org.eclipse.mylyn.internal.builds.ui.commands;
@@ -16,11 +17,14 @@ import java.util.List;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.mylyn.builds.core.IBuildPlan;
 import org.eclipse.mylyn.builds.core.IBuildServer;
 import org.eclipse.mylyn.internal.builds.ui.BuildsUiInternal;
+import org.eclipse.osgi.util.NLS;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 /**
@@ -33,9 +37,18 @@ public class DeleteBuildElementHandler extends AbstractHandler {
 		if (selection instanceof IStructuredSelection) {
 			Object item = ((IStructuredSelection) selection).getFirstElement();
 			if (item instanceof IBuildServer) {
-				List<IBuildPlan> plans = BuildsUiInternal.getModel().getPlans((IBuildServer) item);
-				BuildsUiInternal.getModel().getPlans().removeAll(plans);
-				BuildsUiInternal.getModel().getServers().remove(item);
+				IBuildServer server = (IBuildServer) item;
+				boolean deleteConfirmed = MessageDialog.openQuestion(
+						PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
+						"Delete Build Server",
+						NLS.bind("Are you sure you want to delete build server \"{0}\"?\n"
+								+ "You will also lose the settings for the builds that you watched.", server.getLabel()));
+
+				if (deleteConfirmed) {
+					List<IBuildPlan> plans = BuildsUiInternal.getModel().getPlans((IBuildServer) item);
+					BuildsUiInternal.getModel().getPlans().removeAll(plans);
+					BuildsUiInternal.getModel().getServers().remove(item);
+				}
 			}
 		}
 		return null;
