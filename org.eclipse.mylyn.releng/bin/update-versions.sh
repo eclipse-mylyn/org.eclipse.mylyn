b8c67685db52cceafdc42f85206a70bbabae50ef
Sam Davis <sam.davis@tasktop.com>
2014-07-02 14:33:35 -0700

    432201: update o.e.m to version 3.13
    Change-Id: I3f3d8d697816b2b1a413a626bb4bac8496a9dae7
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=432201

--- org.eclipse.mylyn.releng/bin/update-versions.sh
+++ org.eclipse.mylyn.releng/bin/update-versions.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 update() {
 OLD=$1
 NEW=$2
@@ -10,13 +12,45 @@ find . -name feature.xml | xargs sed -i~ -e "s/\(mylyn.* version=\"\)$OLD\(\" ma
 find . -name feature.xml | xargs sed -i~ -e "s/\(mylyn.* version=\"\)$OLD\(\" match=\"greaterOrEqual\".*\)/\1$NEW\2/"
 }
 
+update_project() {
+DIR=$1
+OLD=$2
+NEW=$3
+
+cd $DIR
+echo -e "Updating $DIR\t from $OLD to $NEW"
+update $OLD $NEW
+cd ..
+}
+
 #mvn -Dtycho.mode=maven org.sonatype.tycho:tycho-versions-plugin:set-version -DnewVersion=1.4.1-SNAPSHOT
 
-update 1.3.0 1.4.0
-update 2.0.0 2.1.0
-update 2.2.0 2.3.0
-update 3.11.0 3.12.0
-update 5.7.0 5.8.0
+PREV="3.12.0"
+NEXT="3.13.0"
+
+for dir in \
+org.eclipse.mylyn \
+org.eclipse.mylyn.builds \
+org.eclipse.mylyn.commons \
+org.eclipse.mylyn.context \
+org.eclipse.mylyn.docs \
+org.eclipse.mylyn.incubator \
+org.eclipse.mylyn.reviews \
+org.eclipse.mylyn.tasks \
+org.eclipse.mylyn.versions
+do
+	update_project $dir $PREV $NEXT
+done
+
+cd org.eclipse.mylyn.all
+echo pom.xml | xargs sed -i~ -e "s/<version>$PREV-SNAPSHOT<\/version>/<version>$NEXT-SNAPSHOT<\/version>/"
+cd ..
+
+update_project org.eclipse.mylyn.builds 1.4.0 1.5.0
+update_project org.eclipse.mylyn.context 5.8.0 5.9.0 # CDT
+update_project org.eclipse.mylyn.docs 2.1.0 2.2.0
+update_project org.eclipse.mylyn.reviews 2.3.0 2.4.0
+update_project org.eclipse.mylyn.versions 1.4.0 1.5.0
 
  
 #grep Bundle-Version */META-INF/MANIFEST.MF | grep -v 0.9.0 | grep -v 3.7.0 | grep -v 1.5.0
