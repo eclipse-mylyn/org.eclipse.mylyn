1ec9be53211b5394768bd5d59f2acef540777afc
daniel <daniel@clockworkgeek.com>
2014-07-07 23:41:38 +0100

    438968: support markdown in task editor
    Change-Id: Ia9eec29b4453fbb3d90cb5d2f0b4571dfd412e16
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=438968
    Signed-off-by: Daniel Deady <daniel@clockworkgeek.com>

---
+++ org.eclipse.mylyn.wikitext.tasks.ui/src/org/eclipse/mylyn/internal/wikitext/tasks/ui/editor/MarkdownMarkupTaskEditorExtension.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Daniel Deady and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Daniel Deady <daniel@clockworkgeek.com> - Bug 438968
+ *******************************************************************************/
+package org.eclipse.mylyn.internal.wikitext.tasks.ui.editor;
+
+import org.eclipse.mylyn.wikitext.markdown.core.MarkdownLanguage;
+import org.eclipse.mylyn.wikitext.tasks.ui.editor.MarkupTaskEditorExtension;
+
+public class MarkdownMarkupTaskEditorExtension extends MarkupTaskEditorExtension<MarkdownLanguage> {
+
+	public MarkdownMarkupTaskEditorExtension() {
+		setMarkupLanguage(new MarkdownLanguage());
+	}
+
+}
