5376a6f73fc98c27d6e30fe58bdab2de124ff9c9
Sam Davis <sam.davis@tasktop.com>
2014-09-29 10:53:58 -0700

    439453: update versions to 3.14
    Change-Id: I80f947c9cc838819f8170731674b9c36773908c4
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=439453

--- org.eclipse.mylyn.releng/bin/update-versions.sh
+++ org.eclipse.mylyn.releng/bin/update-versions.sh
@@ -1,6 +1,7 @@
 update() {
 OLD=$1
 NEW=$2
+echo "Updating $OLD to $NEW..."
 find . -name MANIFEST.MF | xargs sed -i~ -e "s/Bundle-Version: $OLD.qualifier/Bundle-Version: $NEW.qualifier/"
 find . -name feature.xml | xargs sed -i~ -e "s/$OLD.qualifier/$NEW.qualifier/"
 find . -name pom.xml | xargs sed -i~ -e "s/<version>$OLD-SNAPSHOT<\/version>/<version>$NEW-SNAPSHOT<\/version>/"
@@ -12,10 +13,10 @@ find . -name feature.xml | xargs sed -i~ -e "s/\(mylyn.* version=\"\)$OLD\(\" ma
 
 #mvn -Dtycho.mode=maven org.sonatype.tycho:tycho-versions-plugin:set-version -DnewVersion=1.4.1-SNAPSHOT
 
-update 1.4.0 1.5.0 # builds, versions, org.eclipse.mylyn.commons.identiy/notifications/repositories
-update 2.1.0 2.2.0 # docs
-update 2.3.0 2.4.0 # reviews
-update 3.12.0 3.13.0
-update 5.8.0 5.9.0 # CDT
+update 1.5.0 1.6.0 # builds, versions, org.eclipse.mylyn.commons.identiy/notifications/repositories
+update 2.2.0 2.3.0 # docs
+update 2.4.0 2.5.0 # reviews
+update 3.13.0 3.14.0
+update 5.9.0 5.10.0 # CDT
  
 #grep Bundle-Version */META-INF/MANIFEST.MF | grep -v 0.9.0 | grep -v 3.7.0 | grep -v 1.5.0
