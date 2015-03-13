f081c97f5ddddc84791ae8b8a957eee82ba2a335
Sam Davis <sam.davis@tasktop.com>
2015-03-13 16:40:46 -0700

    462113: update o.e.mylyn version to 3.14.3
    Change-Id: I453ecdebc438308265fe835276fd8204e24776f5
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=462113

--- org.eclipse.mylyn.releng/bin/update-versions.sh
+++ org.eclipse.mylyn.releng/bin/update-versions.sh
@@ -21,11 +21,11 @@ org.eclipse.mylyn/org.eclipse.mylyn-parent/pom.xml
 
 #mvn -Dtycho.mode=maven org.sonatype.tycho:tycho-versions-plugin:set-version -DnewVersion=1.4.1-SNAPSHOT
 
-update 1.6.1  1.6.2  # builds, versions, org.eclipse.mylyn.commons.identiy/notifications/repositories
-update 2.3.1  2.3.2  # docs
-update 2.5.1  2.5.2  # reviews
-update 3.14.1 3.14.2  
-update 5.10.1 5.10.2   # CDT
+update 1.6.2  1.6.3  # builds, versions, org.eclipse.mylyn.commons.identiy/notifications/repositories
+update 2.3.2  2.3.3  # docs
+update 2.5.2  2.5.3  # reviews
+update 3.14.2 3.14.3  
+update 5.10.2 5.10.3   # CDT
 
 # Must do this for first SR on a branch
 #updateSnapshotSitesForSR 3.14
