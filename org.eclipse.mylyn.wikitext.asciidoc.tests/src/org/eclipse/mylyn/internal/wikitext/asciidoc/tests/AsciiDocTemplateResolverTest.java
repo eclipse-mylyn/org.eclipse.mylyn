ac5041a122e3bf17e8100380a9791244f0aa39e2
Max Rydahl Andersen <manderse@redhat.com>
2015-05-17 23:32:42 +0200

    467626: Add basic asciidoc content assist templates
    Also includes initial work for cheatsheet, but disabled
    while waiting for rest of rendering features to be merged in.
    cheatsheet is used for manual testing/verification.
    
    Change-Id: I6fced6efa4c200994e18044590f1809405223886
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=467626
    Signed-off-by: Max Rydahl Andersen <manderse@redhat.com>

---
+++ org.eclipse.mylyn.wikitext.asciidoc.tests/src/org/eclipse/mylyn/internal/wikitext/asciidoc/tests/AsciiDocTemplateResolverTest.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Max Rydahl Andersen and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Max Rydahl Andersen- initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.mylyn.internal.wikitext.asciidoc.tests;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.eclipse.jface.text.templates.Template;
+import org.eclipse.mylyn.internal.wikitext.ui.WikiTextUiPlugin;
+import org.eclipse.mylyn.internal.wikitext.ui.editor.assist.Templates;
+import org.eclipse.mylyn.wikitext.asciidoc.core.AsciiDocLanguage;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Basic test for templates being present and resolving correctly.
+ *
+ * @author Max Rydahl Andersen
+ */
+public class AsciiDocTemplateResolverTest {
+
+	private Templates templates;
+
+	@Before
+	public void setUp() throws Exception {
+		templates = WikiTextUiPlugin.getDefault().getTemplates().get(new AsciiDocLanguage().getName());
+	}
+
+	@Test
+	public void hasTemplates() {
+		assertTrue("Should have non-zero templates", templates.getTemplate().size() != 0);
+	}
+
+	@Test
+	public void hasNoUnresolvedNLSStrings() {
+
+		for (Template template : templates.getTemplate()) {
+			assertFalse(template.getName() + " with " + template.getDescription() + " has unresolved NLS string",
+					template.getDescription().startsWith("%"));
+		}
+
+	}
+}
