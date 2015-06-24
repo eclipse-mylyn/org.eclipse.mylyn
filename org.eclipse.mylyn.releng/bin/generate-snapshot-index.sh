e4a4f26e17926bc481e73872236108e39387c65d
Sam Davis <sam.davis@tasktop.com>
2015-06-24 11:23:03 -0700

    use mirrors for snapshot downloads

--- org.eclipse.mylyn.releng/bin/generate-snapshot-index.sh
+++ org.eclipse.mylyn.releng/bin/generate-snapshot-index.sh
@@ -2,16 +2,15 @@
 
 pushd /home/data/httpd/download.eclipse.org/mylyn/drops > /dev/null
 
-echo "<html><head><meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />" > index.html.temp
-echo "<meta http-equiv="Pragma" content="no-cache" />" >> index.html.temp
-echo "<meta http-equiv="Expires" content="0" /></head>" >> index.html.temp
+echo "<html><head><meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />" > index.html
+echo "<meta http-equiv="Pragma" content="no-cache" />" >> index.html
+echo "<meta http-equiv="Expires" content="0" /></head>" >> index.html
 
-echo "<body><h1>Mylyn Snapshot Builds</h1>" >> index.html.temp
-echo "<p>Weekly builds are production quality, but subject to UI changes.</p><p>" >> index.html.temp
+echo "<body><h1>Mylyn Snapshot Builds</h1>" >> index.html
+echo "<p>Weekly builds are production quality, but subject to UI changes.</p><p>" >> index.html
 for f in `find -name *3.1*.zip -not -name *-api.zip | sort`; do 
-	echo "<a href=\"$f\">${f##.*/}</a><br/>" >> index.html.temp
+	echo "<a href=\"https://www.eclipse.org/downloads/download.php?file=/mylyn/drops${f#\.}\">${f##.*/}</a><br/>" >> index.html
 done
-echo "</p></body><html>" >> index.html.temp
+echo "</p></body><html>" >> index.html
 
-cp index.html.temp index.html
 popd > /dev/null
