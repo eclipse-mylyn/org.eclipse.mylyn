c3774ec86109fd47097b974ee51f2c0d2538305a
Frank Becker <eclipse@frank-becker.de>
2015-07-22 19:04:39 +0200

    414360: create schema for QueryPage (provisional version)
    Change-Id: I01e4b2f02e3670167aa743c55f5287b07db49442
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=414360

---
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/provisional/tasks/ui/wizards/SimpleURLQueryPageSchema.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Frank Becker and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Frank Becker - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.mylyn.internal.provisional.tasks.ui.wizards;
+
+import org.eclipse.mylyn.tasks.core.data.TaskAttribute;
+
+public class SimpleURLQueryPageSchema extends AbstractQueryPageSchema {
+	private static final SimpleURLQueryPageSchema instance = new SimpleURLQueryPageSchema();
+
+	public SimpleURLQueryPageSchema() {
+	}
+
+	public static SimpleURLQueryPageSchema getInstance() {
+		return instance;
+	}
+
+	public final Field wholeUrl = createField("wholeQueryURL", "URL:", TaskAttribute.TYPE_URL);
+}
