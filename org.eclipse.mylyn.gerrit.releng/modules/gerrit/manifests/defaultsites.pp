4e66cdfb659adbc4053e8f121b4a93216a77b90a
Sam Davis <sam.davis@tasktop.com>
2015-08-27 15:42:50 -0700

    473225: support Gerrit 2.11.3
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=473225

--- org.eclipse.mylyn.gerrit.releng/modules/gerrit/manifests/defaultsites.pp
+++ org.eclipse.mylyn.gerrit.releng/modules/gerrit/manifests/defaultsites.pp
@@ -23,7 +23,7 @@ define gerrit::defaultsites ($base = $gerrit::base, $userOwner = $gerrit::userOw
   gerrit { "2.10":
   }
   
-  gerrit { "2.11.2":
+  gerrit { "2.11.3":
   }
   
   /* Sites */
@@ -43,11 +43,11 @@ define gerrit::defaultsites ($base = $gerrit::base, $userOwner = $gerrit::userOw
     require => Gerrit["2.10"],
   }
     
-  gerrit::site { "gerrit-2.11.2":
-    version => "2.11.2",
+  gerrit::site { "gerrit-2.11.3":
+    version => "2.11.3",
     port    => 28211,
     sshport => 29211,
-    require => Gerrit["2.11.2"],
+    require => Gerrit["2.11.3"],
   }
 
 }
