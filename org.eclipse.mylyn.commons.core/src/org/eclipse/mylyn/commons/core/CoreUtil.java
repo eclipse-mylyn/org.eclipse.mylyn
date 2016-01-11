a1035a583df8bb80451f8ed6892fbfd00ee58ac4
Sam Davis <sam.davis@tasktop.com>
2016-01-11 11:19:59 -0800

    479456: update o.e.m.commons versions to 3.19
    Change-Id: I65f3c9dd7f7cb230a70d5e3bc4bb87d0f17ef546
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=479456

--- org.eclipse.mylyn.commons.core/src/org/eclipse/mylyn/commons/core/CoreUtil.java
+++ org.eclipse.mylyn.commons.core/src/org/eclipse/mylyn/commons/core/CoreUtil.java
@@ -46,7 +46,7 @@ public class CoreUtil {
 		}
 	}
 
-	private static final String FRAMEWORK_VERSION = "3.18.0"; //$NON-NLS-1$
+	private static final String FRAMEWORK_VERSION = "3.19.0"; //$NON-NLS-1$
 
 	private static final SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
 	static {
