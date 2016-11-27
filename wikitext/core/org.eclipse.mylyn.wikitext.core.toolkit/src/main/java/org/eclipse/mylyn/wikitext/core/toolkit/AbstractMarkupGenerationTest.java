5e3e608b9fe948299441ea9d6daf800593b22687
David Green <david.green@tasktop.com>
2016-11-27 10:33:17 -0800

    511120: move more core projects to Maven build
    Change-Id: If16c681af0e19991934c5938c1bcb9202473ebc5
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=511120

---
+++ wikitext/core/org.eclipse.mylyn.wikitext.core.toolkit/src/main/java/org/eclipse/mylyn/wikitext/core/toolkit/AbstractMarkupGenerationTest.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright (c) 2007, 2016 David Green and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     David Green - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.mylyn.wikitext.core.toolkit;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.StringWriter;
+import java.io.Writer;
+
+import org.eclipse.mylyn.wikitext.core.parser.MarkupParser;
+import org.eclipse.mylyn.wikitext.core.parser.builder.HtmlDocumentBuilder;
+import org.eclipse.mylyn.wikitext.core.parser.markup.MarkupLanguage;
+import org.junit.Before;
+
+/**
+ * @since 2.0
+ */
+public abstract class AbstractMarkupGenerationTest<L extends MarkupLanguage> {
+
+	protected MarkupParser parser;
+
+	protected L markupLanguage;
+
+	@Before
+	public void initParser() {
+		parser = new MarkupParser();
+		markupLanguage = createMarkupLanguage();
+		parser.setMarkupLanguage(markupLanguage);
+	}
+
+	protected abstract L createMarkupLanguage();
+
+	protected void assertMarkup(String expectedHtml, String markup) {
+		String html = toHtml(markup);
+		assertEquals(expectedHtml, html);
+	}
+
+	protected String toHtml(String markup) {
+		Writer writer = new StringWriter();
+		HtmlDocumentBuilder builder = new HtmlDocumentBuilder(writer);
+		builder.setEmitAsDocument(false);
+		parser.setBuilder(builder);
+		parser.parse(markup);
+
+		return writer.toString();
+	}
+}
---
+++ wikitext/core/org.eclipse.mylyn.wikitext.markdown.core/src/test/java/org/eclipse/mylyn/internal/wikitext/markdown/tests/MarkdownReferenceValidationRuleTest.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2013, 2014 Stefan Seelmann and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Stefan Seelmann - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.mylyn.internal.wikitext.markdown.tests;
+
+import java.util.List;
+
+import org.eclipse.mylyn.internal.wikitext.markdown.core.validation.MarkdownReferenceValidationRule;
+import org.eclipse.mylyn.wikitext.core.validation.MarkupValidator;
+import org.eclipse.mylyn.wikitext.core.validation.ValidationProblem;
+import org.eclipse.mylyn.wikitext.core.validation.ValidationProblem.Severity;
+
+import junit.framework.TestCase;
+
+public class MarkdownReferenceValidationRuleTest extends TestCase {
+
+	private MarkdownReferenceValidationRule rule;
+
+	private MarkupValidator validator;
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		rule = new MarkdownReferenceValidationRule();
+		validator = new MarkupValidator();
+		validator.getRules().add(rule);
+	}
+
+	public void testNoErrorInLocalReferenceToExistingAnchor() {
+		final String markup = "# Header 1\n\n[Link to title](#header-1)";
+		List<ValidationProblem> problems = rule.findProblems(markup, 0, markup.length());
+		
+		assertNotNull(problems);
+		assertTrue(problems.isEmpty());
+	}
+
+	public void testErrorInLocalReferenceToNonExistingAnchor() {
+		final String markup = "# Header 1\n\n[Link to title](#FooBar)";
+		List<ValidationProblem> problems = rule.findProblems(markup, 0, markup.length());
+		
+		assertNotNull(problems);
+		assertEquals(1, problems.size());
+		assertEquals(12, problems.get(0).getOffset());
+		assertEquals(24, problems.get(0).getLength());
+		assertEquals(Severity.ERROR, problems.get(0).getSeverity());
+		assertTrue(problems.get(0).getMessage().contains("FooBar"));
+	}
+
+}
