ac270d5163774de75af3311163230bb00937a5e9
Jaxsun McCarthy Huggan <jaxsun.mccarthy@tasktop.com>
2016-12-18 00:35:35 -0800

    357234: reduce excess vertical space due to remove reviewer controls
    Change-Id: Ie7b635674871a2c20b71a8efe2ebedef1813f612
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=357234
    Signed-off-by: Jaxsun McCarthy Huggan <jaxsun.mccarthy@tasktop.com>

--- org.eclipse.mylyn.gerrit.ui/src/org/eclipse/mylyn/internal/gerrit/ui/factories/GerritReviewerUiFactoryProvider.java
+++ org.eclipse.mylyn.gerrit.ui/src/org/eclipse/mylyn/internal/gerrit/ui/factories/GerritReviewerUiFactoryProvider.java
@@ -18,6 +18,9 @@ import org.eclipse.mylyn.reviews.core.model.IUser;
 import org.eclipse.mylyn.reviews.ui.spi.factories.AbstractUiFactory;
 import org.eclipse.mylyn.reviews.ui.spi.factories.AbstractUiFactoryProvider;
 import org.eclipse.mylyn.reviews.ui.spi.factories.IUiContext;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.forms.widgets.FormToolkit;
 
 public class GerritReviewerUiFactoryProvider extends AbstractUiFactoryProvider<IUser> {
 
@@ -26,6 +29,15 @@ public class GerritReviewerUiFactoryProvider extends AbstractUiFactoryProvider<I
 		List<AbstractUiFactory<IUser>> factories = new ArrayList<AbstractUiFactory<IUser>>();
 		factories.add(new RemoveReviewerUiFactory(context, type));
 		return factories;
+	}
+
+	@Override
+	public Composite createControls(IUiContext context, Composite parent, FormToolkit toolkit, IUser object) {
+		Composite buttonComposite = super.createControls(context, parent, toolkit, object);
+		RowLayout layout = (RowLayout) buttonComposite.getLayout();
+		layout.marginBottom = 0;
+		layout.marginTop = 0;
+		return buttonComposite;
 	}
 
 }
