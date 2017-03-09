883cc541cdd04e96d63e88606f7e4b7479c42668
David Green <david.green@tasktop.com>
2017-03-09 12:42:35 -0800

    511120: improve docbook test assertions
    Change-Id: I23662c1ba2d1421e24a050ea903d3127f71e4a1e
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=511120

--- wikitext/ui/org.eclipse.mylyn.wikitext.ui.tests/src/org/eclipse/mylyn/wikitext/core/parser/util/TextileToDocbookTest.java
+++ wikitext/ui/org.eclipse.mylyn.wikitext.ui.tests/src/org/eclipse/mylyn/wikitext/core/parser/util/TextileToDocbookTest.java
@@ -10,11 +10,13 @@
  *******************************************************************************/
 package org.eclipse.mylyn.wikitext.core.parser.util;
 
+import java.text.MessageFormat;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.eclipse.mylyn.wikitext.parser.util.MarkupToDocbook;
 import org.eclipse.mylyn.wikitext.textile.TextileLanguage;
+import org.eclipse.mylyn.wikitext.toolkit.TestResources;
 
 import junit.framework.TestCase;
 
@@ -35,31 +37,27 @@ public class TextileToDocbookTest extends TestCase {
 	public void testHeader() throws Exception {
 		textileToDocbook.setBookTitle("Test");
 		String book = textileToDocbook.parse("h1. title1\n\nContent para 1\n\nh1. title2\n\nMore content\n");
-
-		
+		assertContent(book);
 	}
 
 	public void testMultipleNestedElements() throws Exception {
 		textileToDocbook.setBookTitle("Test");
 		String book = textileToDocbook.parse("h1. title1\n\nContent para 1\n* a\n* list of\n* items\n");
-
-		
+		assertContent(book);
 	}
 
 	public void testNestedHeaders() throws Exception {
 		textileToDocbook.setBookTitle("Test");
-		String book = textileToDocbook.parse("h1. title1\n\nContent para 1\nh2. title2\n\nContent para2\n\nh1. title3\n\npara3");
-
-		
+		String book = textileToDocbook
+				.parse("h1. title1\n\nContent para 1\nh2. title2\n\nContent para2\n\nh1. title3\n\npara3");
 
 		assertTrue(Pattern.compile("</chapter>\\s*<chapter", Pattern.MULTILINE).matcher(book).find());
 	}
 
 	public void testHeaderWithAcronym() throws Exception {
 		textileToDocbook.setBookTitle("Test");
-		String book = textileToDocbook.parse("h1. TBA(To Be Announced) plus more content in the header\n\nContent para 1");
-
-		
+		String book = textileToDocbook
+				.parse("h1. TBA(To Be Announced) plus more content in the header\n\nContent para 1");
 
 		assertTrue(Pattern.compile("<glossterm>TBA</glossterm>\\s*plus more", Pattern.MULTILINE).matcher(book).find());
 		assertTrue(Pattern.compile("<glossterm>TBA</glossterm>\\s*<glossdef>", Pattern.MULTILINE).matcher(book).find());
@@ -70,16 +68,13 @@ public class TextileToDocbookTest extends TestCase {
 		textileToDocbook.setBookTitle("Test");
 		String book = textileToDocbook.parse("h1. A\n\nsome content\n\nbc. public class Foo {\n}\n");
 
-		
-
 		assertTrue(Pattern.compile("<literallayout>\\s*<code>", Pattern.MULTILINE).matcher(book).find());
 	}
 
 	public void testBlockCodeExtended() throws Exception {
 		textileToDocbook.setBookTitle("Test");
-		String book = textileToDocbook.parse("h1. A\n\nsome content\n\nbc.. \npublic class Foo {\n}\n\n\nsome other content");
-
-		
+		String book = textileToDocbook
+				.parse("h1. A\n\nsome content\n\nbc.. \npublic class Foo {\n}\n\n\nsome other content");
 
 		assertTrue(Pattern.compile("<literallayout>\\s*<code>", Pattern.MULTILINE).matcher(book).find());
 	}
@@ -88,16 +83,14 @@ public class TextileToDocbookTest extends TestCase {
 		textileToDocbook.setBookTitle("Test");
 		String book = textileToDocbook.parse("something[1] with a footnote\n\nfn1. the footnote text");
 
-		
-
 		Matcher matcher = Pattern.compile("<link\\s*linkend=\"(___fn.*?)\">", Pattern.MULTILINE).matcher(book);
 		assertTrue(matcher.find());
 
 		String linkend = matcher.group(1);
 		assertNotNull(linkend);
 
-		assertTrue(Pattern.compile("<para\\s*id=\"" + Pattern.quote(linkend) + "\"\\s+role=\"footnote\">",
-				Pattern.MULTILINE)
+		assertTrue(Pattern
+				.compile("<para\\s*id=\"" + Pattern.quote(linkend) + "\"\\s+role=\"footnote\">", Pattern.MULTILINE)
 				.matcher(book)
 				.find());
 	}
@@ -105,8 +98,6 @@ public class TextileToDocbookTest extends TestCase {
 	public void testImageScaling() throws Exception {
 		textileToDocbook.setBookTitle("Test");
 		String book = textileToDocbook.parse("Here comes an !{width:80%}imageUrl! with more text");
-
-		
 
 		Matcher matcher = Pattern.compile("<imagedata .* scale=\"80\"/>", Pattern.MULTILINE).matcher(book);
 		assertTrue(matcher.find());
@@ -116,8 +107,6 @@ public class TextileToDocbookTest extends TestCase {
 		textileToDocbook.setBookTitle("Test");
 		String book = textileToDocbook.parse("Here comes an !{width:80px}imageUrl! with more text");
 
-		
-
 		Matcher matcher = Pattern.compile("<imagedata .* width=\"80px\"/>", Pattern.MULTILINE).matcher(book);
 		assertTrue(matcher.find());
 	}
@@ -126,10 +115,14 @@ public class TextileToDocbookTest extends TestCase {
 		textileToDocbook.setBookTitle("Test");
 		String book = textileToDocbook.parse("Here comes an !{width:80px;height:90px;}imageUrl! with more text");
 
-		
-
 		Matcher matcher = Pattern.compile("<imagedata .* width=\"80px\"\\s.*?depth=\"90px\"/>", Pattern.MULTILINE)
 				.matcher(book);
 		assertTrue(matcher.find());
+	}
+
+	private void assertContent(String book) {
+		String testName = Thread.currentThread().getStackTrace()[2].getMethodName();
+		String resourceName = MessageFormat.format("{0}_{1}.xml", TextileToDocbookTest.class.getSimpleName(), testName);
+		assertEquals(TestResources.load(TextileToDocbookTest.class, resourceName), book);
 	}
 }
