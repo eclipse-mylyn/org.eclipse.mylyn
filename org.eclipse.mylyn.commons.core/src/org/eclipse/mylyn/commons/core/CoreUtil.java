f503a59a14e639c803c37be342438528b01ae709
Sam Davis <sam.davis@tasktop.com>
2017-04-11 10:51:31 -0700

    505126: update org.eclipse.mylyn.commons versions to 3.23
    Change-Id: I0deb1ce4b576e3402dd11024b248ca7edba921fb
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=505126

--- org.eclipse.mylyn.commons.core/src/org/eclipse/mylyn/commons/core/CoreUtil.java
+++ org.eclipse.mylyn.commons.core/src/org/eclipse/mylyn/commons/core/CoreUtil.java
@@ -46,7 +46,7 @@ public class CoreUtil {
 		}
 	}
 
-	private static final String FRAMEWORK_VERSION = "3.22.0"; //$NON-NLS-1$
+	private static final String FRAMEWORK_VERSION = "3.23.0"; //$NON-NLS-1$
 
 	private static final SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
 	static {
