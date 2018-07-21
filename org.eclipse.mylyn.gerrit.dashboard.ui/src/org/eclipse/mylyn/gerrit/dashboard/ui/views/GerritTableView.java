53858e36a2893781849f0cf9ca5462b8d821edb1
Frank Becker <eclipse@frank-becker.de>
2018-07-21 16:42:57 +0200

    537254: update org.eclipse.license from 1.0.1 to 2.0.0
    Change-Id: I6e1dcc41f38ba81a5fa2038472648184e474b338
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=537254
    Signed-off-by: Frank Becker <eclipse@frank-becker.de>

--- org.eclipse.mylyn.gerrit.dashboard.ui/src/org/eclipse/mylyn/gerrit/dashboard/ui/views/GerritTableView.java
+++ org.eclipse.mylyn.gerrit.dashboard.ui/src/org/eclipse/mylyn/gerrit/dashboard/ui/views/GerritTableView.java
@@ -2,9 +2,9 @@
  * Copyright (c) 2013, 2015 Ericsson
  *
  * All rights reserved. This program and the accompanying materials are
- * made available under the terms of the Eclipse Public License v1.0 which
+ * made available under the terms of the Eclipse Public License v2.0 which
  * accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * http://www.eclipse.org/legal/epl-v20.html
  *
  * Contributors:
  *   Jacques Bouthillier - Initial Implementation of the plug-in
