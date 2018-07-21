edce5c1f75a0fceed4ac6be56cefa77676d50067
Frank Becker <eclipse@frank-becker.de>
2018-07-21 16:45:51 +0200

    537254: update org.eclipse.license from 1.0.1 to 2.0.0
    Change-Id: Ia3213bade19ba8d3055ddcbdff850f4c54b37db4
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=537254

--- org.eclipse.mylyn.bugzilla.tests/src/org/eclipse/mylyn/bugzilla/tests/core/BugzillaAttributeTest.java
+++ org.eclipse.mylyn.bugzilla.tests/src/org/eclipse/mylyn/bugzilla/tests/core/BugzillaAttributeTest.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse @Test
 	public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * http://www.eclipse.org/legal/epl-v20.html
  *
  * Contributors:
  *     Frank Becker - initial API and implementation
--- org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/editors/outline/QuickOutlineDialog.java
+++ org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/editors/outline/QuickOutlineDialog.java
@@ -1,11 +1,13 @@
 /*******************************************************************************
  *  Copyright (c) 2006, 2011 IBM Corporation and others.
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  which accompanies this distribution, and is available at
- *  http://www.eclipse.org/legal/epl-v10.html
  * 
- *  Contributors:
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
  *     IBM Corporation - initial API and implementation
  *     Tasktop Technologies - adapted for Mylyn
  *     Frank Becker - adapted for Mylyn Task Editor
