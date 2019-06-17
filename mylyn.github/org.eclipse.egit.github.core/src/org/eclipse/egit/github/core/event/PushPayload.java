64c53a3b37ba029f81e4188ea7214a60b1b408e6
Aaron S. Hawley <aaron.s.hawley@gmail.com>
2019-06-17 02:33:28 -0400

    Add new fields for GitHub push event
    Fields that were missing:
    
    * After
    * Created, Deleted, Forced
    * Base ref
    * Compare
    * Distinct size
    * Head commit
    * Organization, Pusher, Repository, Sender
    
    https://developer.github.com/v3/activity/events/types/#pushevent
    
    Bug: 521783
    Change-Id: Ibdc22e983aa81ddb638c2fd8ad724ea8957be43f
    Signed-off-by: Aaron S. Hawley <aaron.s.hawley@gmail.com>

--- mylyn.github/org.eclipse.egit.github.core/src/org/eclipse/egit/github/core/event/PushPayload.java
+++ mylyn.github/org.eclipse.egit.github.core/src/org/eclipse/egit/github/core/event/PushPayload.java
@@ -12,9 +12,14 @@
  *******************************************************************************/
 package org.eclipse.egit.github.core.event;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.util.List;
 
 import org.eclipse.egit.github.core.Commit;
+import org.eclipse.egit.github.core.Organization;
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.User;
 
 /**
  * PushEvent payload model class.
@@ -23,15 +28,42 @@ public class PushPayload extends EventPayload {
 
 	private static final long serialVersionUID = -1542484898531583478L;
 
+	private String after;
+
 	private String before;
+
+	private boolean created;
+
+	private boolean deleted;
+
+	private boolean forced;
 
 	private String head;
 
 	private String ref;
 
+	@SerializedName("base_ref")
+	private String baseRef;
+
+	private String compare;
+
 	private int size;
 
+	@SerializedName("distinct_size")
+	private int distinctSize;
+
 	private List<Commit> commits;
+
+	@SerializedName("head_commit")
+	private Commit headCommit;
+
+	private Repository repository;
+
+	private User pusher;
+
+	private Organization organization;
+
+	private User sender;
 
 	/**
 	 * @return before
@@ -46,6 +78,70 @@ public class PushPayload extends EventPayload {
 	 */
 	public PushPayload setBefore(String before) {
 		this.before = before;
+		return this;
+	}
+
+	/**
+	 * @return after
+	 */
+	public String getAfter() {
+		return after;
+	}
+
+	/**
+	 * @param after
+	 * @return this payload
+	 */
+	public PushPayload setAfter(String after) {
+		this.after = after;
+		return this;
+	}
+
+	/**
+	 * @return created
+	 */
+	public boolean isCreated() {
+		return created;
+	}
+
+	/**
+	 * @param created
+	 * @return this PushEvent
+	 */
+	public PushPayload setCreated(boolean created) {
+		this.created = created;
+		return this;
+	}
+
+	/**
+	 * @return deleted
+	 */
+	public boolean isDeleted() {
+		return deleted;
+	}
+
+	/**
+	 * @param deleted
+	 * @return this PushEvent
+	 */
+	public PushPayload setDeleted(boolean deleted) {
+		this.deleted = deleted;
+		return this;
+	}
+
+	/**
+	 * @return forced
+	 */
+	public boolean isForced() {
+		return forced;
+	}
+
+	/**
+	 * @param forced
+	 * @return this PushEvent
+	 */
+	public PushPayload setForced(boolean forced) {
+		this.forced = forced;
 		return this;
 	}
 
@@ -82,6 +178,38 @@ public class PushPayload extends EventPayload {
 	}
 
 	/**
+	 * @return baseRef
+	 */
+	public String getBaseRef() {
+		return baseRef;
+	}
+
+	/**
+	 * @param baseRef
+	 * @return this PushEvent
+	 */
+	public PushPayload setBaseRef(String baseRef) {
+		this.baseRef = baseRef;
+		return this;
+	}
+
+	/**
+	 * @return compare
+	 */
+	public String getCompare() {
+		return compare;
+	}
+
+	/**
+	 * @param compare
+	 * @return this PushEvent
+	 */
+	public PushPayload setCompare(String compare) {
+		this.compare = compare;
+		return this;
+	}
+
+	/**
 	 * @return size
 	 */
 	public int getSize() {
@@ -98,6 +226,22 @@ public class PushPayload extends EventPayload {
 	}
 
 	/**
+	 * @return distinctSize
+	 */
+	public int getDistinctSize() {
+		return distinctSize;
+	}
+
+	/**
+	 * @param distinctSize
+	 * @return this PushEvent
+	 */
+	public PushPayload setDistinctSize(int distinctSize) {
+		this.distinctSize = distinctSize;
+		return this;
+	}
+
+	/**
 	 * @return commits
 	 */
 	public List<Commit> getCommits() {
@@ -110,6 +254,86 @@ public class PushPayload extends EventPayload {
 	 */
 	public PushPayload setCommits(List<Commit> commits) {
 		this.commits = commits;
+		return this;
+	}
+
+	/**
+	 * @return headCommit
+	 */
+	public Commit getHeadCommit() {
+		return headCommit;
+	}
+
+	/**
+	 * @param headCommit
+	 * @return this PushEvent
+	 */
+	public PushPayload setHeadCommit(Commit headCommit) {
+		this.headCommit = headCommit;
+		return this;
+	}
+
+	/**
+	 * @return repository
+	 */
+	public Repository getRepository() {
+		return repository;
+	}
+
+	/**
+	 * @param repository
+	 * @return this PushEvent
+	 */
+	public PushPayload setRepository(Repository repository) {
+		this.repository = repository;
+		return this;
+	}
+
+	/**
+	 * @return pusher
+	 */
+	public User getPusher() {
+		return pusher;
+	}
+
+	/**
+	 * @param pusher
+	 * @return this PushEvent
+	 */
+	public PushPayload setPusher(User pusher) {
+		this.pusher = pusher;
+		return this;
+	}
+
+	/**
+	 * @return organization
+	 */
+	public Organization getOrganization() {
+		return organization;
+	}
+
+	/**
+	 * @param organization
+	 * @return this PushEvent
+	 */
+	public PushPayload setOrganization(Organization organization) {
+		this.organization = organization;
+		return this;
+	}
+
+	/**
+	 * @return sender
+	 */
+	public User getSender() {
+		return sender;
+	}
+
+	/**
+	 * @param sender
+	 * @return this PushEvent
+	 */
+	public PushPayload setSender(User sender) {
+		this.sender = sender;
 		return this;
 	}
 }
