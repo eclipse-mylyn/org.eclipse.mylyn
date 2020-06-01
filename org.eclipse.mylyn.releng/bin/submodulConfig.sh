a38f4d0ed2e4d7c34d8a857406b37993851bf14a
Frank Becker <eclipse@frank-becker.de>
2020-06-01 11:39:50 +0200

    562490: OOMPH: support 2020-06
    Change-Id: I889f8ed61c76ce3451edc78a68f669bf333c0653
    Signed-off-by: Frank Becker <eclipse@frank-becker.de>

---
+++ org.eclipse.mylyn.releng/bin/submodulConfig.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+pwd
+for i in org.eclipse.mylyn*; do
+  cd $i
+  echo Entering \'$i\'
+  fetchnode="`git config --local --get-all remote.origin.fetch | grep "/notes"`"
+  if [ -z "$fetchnode" ]
+  then
+    git config --add remote.origin.fetch 'refs/notes/*:refs/notes/*'
+  fi
+  fetchnode="`git config --local --get remote.origin.push`"
+  if [ -z "$fetchnode" ]
+  then
+    git config --add remote.origin.push 'HEAD:refs/for/master'
+  fi
+  fetchnode="`git config --local --get core.autocrlf`"
+  if [ -z "$fetchnode" ]
+  then
+    git config --add core.autocrlf 'false'
+  fi
+  cd ..
+done
