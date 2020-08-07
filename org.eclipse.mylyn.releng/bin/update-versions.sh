c578027817b0d4e2e830f847ee88cb896e1d5791
Frank Becker <eclipse@frank-becker.de>
2020-08-07 15:28:15 +0200

    565877: update org.eclipse.mylyn versions to 3.25.2
    Change-Id: I4af285854debfb85b298a5662ef7bce9416204e4
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=565877
    Signed-off-by: Frank Becker <eclipse@frank-becker.de>

--- org.eclipse.mylyn.releng/bin/update-versions.sh
+++ org.eclipse.mylyn.releng/bin/update-versions.sh
@@ -21,27 +21,31 @@ org.eclipse.mylyn/org.eclipse.mylyn-parent/pom.xml
 
 #mvn -Dtycho.mode=maven org.sonatype.tycho:tycho-versions-plugin:set-version -DnewVersion=1.4.1-SNAPSHOT
 
-update 1.17.0 1.17.1 # builds, versions, org.eclipse.mylyn.commons.identiy/notifications/repositories
-update 2.15.0 2.16.0 # reviews
-update 3.25.0 3.25.1
-update 5.21.0 5.21.1 # CDT
-bug=563071
-version=3.25
+update 1.17.1 1.17.2 # builds, versions, org.eclipse.mylyn.commons.identiy/notifications/repositories
+update 2.16.0 2.16.1 # reviews
+update 3.25.1 3.25.2
+update 5.21.1 5.21.2 # CDT
+bug=565877
+version=3.25.2
 
 # Must do this for first SR on a branch
 #updateSnapshotSitesForSR 3.25
 
 #grep Bundle-Version */META-INF/MANIFEST.MF | grep -v 0.9.0 | grep -v 3.7.0 | grep -v 1.5.0
 
-# for f in `find . -maxdepth 1 -name "org.eclipse.mylyn*" -not -name "*docs"`
-# do
-# cd $f
-# eval "git commit -a -m \"$bug: update \$(basename \$(pwd)) versions to $version
-# 
-# Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=$bug\""
-# cd ..
-# done
-# 
-# git commit -m "$bug: update `basename $(pwd)` versions to $version
-# 
-# Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=$bug" pom.xml
+for f in `find . -maxdepth 1 -name "org.eclipse.mylyn*" -not -name "*docs" -not -name "*versions"`
+do
+cd $f
+eval "git commit -a -m \"$bug: update \$(basename \$(pwd)) versions to $version
+
+Change-Id: I0000000000000000000000000000000000000000
+Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=$bug
+Signed-off-by: Frank Becker <eclipse@frank-becker.de>\""
+cd ..
+done
+
+git commit -m "$bug: update `basename $(pwd)` versions to $version
+
+Change-Id: I0000000000000000000000000000000000000000
+Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=$bug
+Signed-off-by: Frank Becker <eclipse@frank-becker.de>" pom.xml
