16c9f6e1847442ac525d228bf02ae598b47a5781
Alexander Kurtakov <akurtako@redhat.com>
2021-11-07 11:14:57 +0200

    Remove useless throws UnsupportedEncodingException
    Change-Id: I82d31da1ca1822331dab5ff3e3da712cd5b8c216

--- wikitext/core/org.eclipse.mylyn.wikitext.mediawiki.ant/src/test/java/org/eclipse/mylyn/wikitext/mediawiki/ant/internal/tasks/TestMediaWikiApiImageFetchingStrategy.java
+++ wikitext/core/org.eclipse.mylyn.wikitext.mediawiki.ant/src/test/java/org/eclipse/mylyn/wikitext/mediawiki/ant/internal/tasks/TestMediaWikiApiImageFetchingStrategy.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 Jeremie Bresson and others.
+ * Copyright (c) 2017, 2021 Jeremie Bresson and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -18,7 +18,6 @@ import static java.util.Objects.requireNonNull;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
-import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.util.Map;
 
@@ -35,7 +34,7 @@ public class TestMediaWikiApiImageFetchingStrategy extends MediaWikiApiImageFetc
 	}
 
 	@Override
-	protected Reader createInputReader(URL apiUrl) throws UnsupportedEncodingException, IOException {
+	protected Reader createInputReader(URL apiUrl) throws IOException {
 		String key = apiUrl.toString();
 		if (!serverContent.containsKey(key)) {
 			throw new IllegalStateException("Please define a server content used during the tests for the key: " + key);
