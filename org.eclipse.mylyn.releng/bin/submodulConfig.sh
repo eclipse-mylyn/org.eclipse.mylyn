2f6ac77642e6cb02855b218ddc613a326d9f922e
Frank Becker <eclipse@frank-becker.de>
2022-11-19 19:02:01 +0100

    switch to github

--- org.eclipse.mylyn.releng/bin/submodulConfig.sh
+++ org.eclipse.mylyn.releng/bin/submodulConfig.sh
@@ -2,17 +2,7 @@
 pwd
 for i in org.eclipse.mylyn*; do
   cd $i
-  echo Configure git for gerrit \'$i\'
-  fetchnode="`git config --local --get-all remote.origin.fetch | grep "/notes"`"
-  if [ -z "$fetchnode" ]
-  then
-    git config --add remote.origin.fetch 'refs/notes/*:refs/notes/*'
-  fi
-  fetchnode="`git config --local --get remote.origin.push`"
-  if [ -z "$fetchnode" ]
-  then
-    git config --add remote.origin.push 'HEAD:refs/for/master'
-  fi
+  echo Configure autocrlf for \'$i\'
   fetchnode="`git config --local --get core.autocrlf`"
   if [ -z "$fetchnode" ]
   then
