22c484709bd0195a0d4597c9f0ba895c7c596bb8
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2022-12-04 11:24:21 +0300

    Restore releng for org.eclipse.mylyn.tasks #4
    Migrate org.eclipse.mylyn.tasks.core.tests to a recent mockito
    
    Signed-off-by: Alexander Fedorov <alexander.fedorov@arsysop.ru>

--- org.eclipse.mylyn.tasks.core.tests/src/org/eclipse/mylyn/internal/tasks/core/externalization/LazyTransferListTest.java
+++ org.eclipse.mylyn.tasks.core.tests/src/org/eclipse/mylyn/internal/tasks/core/externalization/LazyTransferListTest.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
  * Copyright (c) 2018 Tasktop Technologies and others.
- * 
+ *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
  * https://www.eclipse.org/legal/epl-2.0
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -13,8 +13,8 @@
 
 package org.eclipse.mylyn.internal.tasks.core.externalization;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.eq;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
