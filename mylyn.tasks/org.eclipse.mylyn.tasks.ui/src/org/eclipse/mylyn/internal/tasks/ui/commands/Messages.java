895607f56a4972b499bbf37f1df80a90a4417fc9
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2023-08-20 11:28:21 +0300

    [#196] Remove "discovery" functionality
    Switch from Mylyn Discovery to Equinox P2 discovery
    
    Signed-off-by: Alexander Fedorov <alexander.fedorov@arsysop.ru>

--- mylyn.tasks/org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/commands/Messages.java
+++ mylyn.tasks/org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/commands/Messages.java
@@ -47,12 +47,6 @@ public class Messages extends NLS {
 
 	public static String RemoteTaskSelectionDialog_Select_a_task_repository;
 
-	public static String ShowTasksConnectorDiscoveryWizardCommandHandler_Install_Connectors;
-
-	public static String ShowTasksConnectorDiscoveryWizardCommandHandler_Notify_when_updates_are_available_Text;
-
-	public static String ShowTasksConnectorDiscoveryWizardCommandHandler_Unable_to_launch_connector_install;
-
 	public static String MarkTaskHandler_MarkTasksReadOperation;
 
 	public static String MarkTaskHandler_MarkTasksUnreadOperation;
