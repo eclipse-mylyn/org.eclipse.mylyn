04e3c7570a9b449fb39b518b94c94445d3c8c5ed
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2023-10-02 08:28:58 +0300

    [#290] Delete Trac-related units
    Delete all Trac-related units and Trac mentioning from other places
    
    Signed-off-by: Alexander Fedorov <alexander.fedorov@arsysop.ru>

--- mylyn.tasks/connectors/trac/org.eclipse.mylyn.trac.releng/scripts/fix-permissions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-USER=`grep www /etc/passwd | sed s/:.*//`
-find var/ -name trac.ini | xargs chown $USER
-find var/ -name db -type d | xargs chown -R $USER
-find var/ -name log -type d | xargs chown -R $USER
-find var/ -name attachments -type d | xargs chown -R $USER
-chown -R $USER var/svn
-chmod 775 bin/*
