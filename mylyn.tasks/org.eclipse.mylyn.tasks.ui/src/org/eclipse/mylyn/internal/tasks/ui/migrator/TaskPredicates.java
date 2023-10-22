824aab10bb1a21ce1dafb57a81204b740d87c0ee
George Lindholm <66577321+gnl42@users.noreply.github.com>
2023-10-22 15:25:46 -0700

    Fix Predicates #287
    Task-Url: https://github.com/eclipse-mylyn/org.eclipse.mylyn/issues/issues/287

--- mylyn.tasks/org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/migrator/TaskPredicates.java
+++ mylyn.tasks/org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/internal/tasks/ui/migrator/TaskPredicates.java
@@ -28,66 +28,31 @@ public class TaskPredicates {
 	}
 
 	public static Predicate<AbstractTaskCategory> containsTask(final AbstractTask task) {
-		return new Predicate<AbstractTaskCategory>() {
-			@Override
-			public boolean test(AbstractTaskCategory category) {
-				return category.contains(task.getHandleIdentifier());
-			}
-		};
+		return category -> category.contains(task.getHandleIdentifier());
 	}
 
 	public static Predicate<ITask> hasTaskKey(final String taskKey) {
-		return new Predicate<ITask>() {
-			@Override
-			public boolean test(ITask task) {
-				return taskKey.equals(task.getTaskKey());
-			}
-		};
+		return task -> taskKey.equals(task.getTaskKey());
 	}
 
 	public static Predicate<ITask> isTaskForConnector(final String kind) {
-		return new Predicate<ITask>() {
-			@Override
-			public boolean test(ITask task) {
-				return kind.equals(task.getConnectorKind());
-			}
-		};
+		return task -> kind.equals(task.getConnectorKind());
 	}
 
 	public static Predicate<IRepositoryQuery> isQueryForRepository(final TaskRepository repository) {
-		return new Predicate<IRepositoryQuery>() {
-			@Override
-			public boolean test(IRepositoryQuery query) {
-				return repository.getConnectorKind().equals(query.getConnectorKind())
-						&& repository.getRepositoryUrl().equals(query.getRepositoryUrl());
-			}
-		};
+		return query -> repository.getConnectorKind().equals(query.getConnectorKind())
+				&& repository.getRepositoryUrl().equals(query.getRepositoryUrl());
 	}
 
 	public static Predicate<IRepositoryQuery> isQueryForConnector(final String kind) {
-		return new Predicate<IRepositoryQuery>() {
-			@Override
-			public boolean test(IRepositoryQuery query) {
-				return kind.equals(query.getConnectorKind());
-			}
-		};
+		return query -> kind.equals(query.getConnectorKind());
 	}
 
 	public static Predicate<RepositoryQuery> isSynchronizing() {
-		return new Predicate<RepositoryQuery>() {
-			@Override
-			public boolean test(RepositoryQuery query) {
-				return query.isSynchronizing();
-			}
-		};
+		return query -> query.isSynchronizing();
 	}
 
 	public static Predicate<AbstractTask> isTaskSynchronizing() {
-		return new Predicate<AbstractTask>() {
-			@Override
-			public boolean test(AbstractTask task) {
-				return task.isSynchronizing();
-			}
-		};
+		return task -> task.isSynchronizing();
 	}
 }
