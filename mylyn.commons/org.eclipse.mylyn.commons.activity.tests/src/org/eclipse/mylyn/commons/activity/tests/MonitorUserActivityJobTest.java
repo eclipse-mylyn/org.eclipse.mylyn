016df4ad2eac8fb0818262e05283f7c45ae18b9e
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2024-02-03 10:21:46 +0300

    [#411] Resolve "light" warnings for Mylyn Commons
    * process `org.eclipse.mylyn.commons.activity.tests`

--- mylyn.commons/org.eclipse.mylyn.commons.activity.tests/src/org/eclipse/mylyn/commons/activity/tests/MonitorUserActivityJobTest.java
+++ mylyn.commons/org.eclipse.mylyn.commons.activity.tests/src/org/eclipse/mylyn/commons/activity/tests/MonitorUserActivityJobTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2011 Tasktop Technologies and others.
+ * Copyright (c) 2010, 2024 Tasktop Technologies and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     Tasktop Technologies - initial API and implementation
+ *     ArSysOp - ongoing support
  *******************************************************************************/
 
 package org.eclipse.mylyn.commons.activity.tests;
@@ -21,6 +22,7 @@ import junit.framework.TestCase;
 /**
  * @author Steffen Pingel
  */
+@SuppressWarnings("nls")
 public class MonitorUserActivityJobTest extends TestCase {
 
 	private StubCallback callback;
