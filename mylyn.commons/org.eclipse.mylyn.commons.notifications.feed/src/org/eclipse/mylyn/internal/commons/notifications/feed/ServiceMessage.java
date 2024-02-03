b8ed61d9643b100b1b92a4d03bdb338c80f9f55e
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2024-02-03 11:06:30 +0300

    [#411] Resolve "light" warnings for Mylyn Commons
    * process `org.eclipse.mylyn.commons.notifications.feed`

--- mylyn.commons/org.eclipse.mylyn.commons.notifications.feed/src/org/eclipse/mylyn/internal/commons/notifications/feed/ServiceMessage.java
+++ mylyn.commons/org.eclipse.mylyn.commons.notifications.feed/src/org/eclipse/mylyn/internal/commons/notifications/feed/ServiceMessage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2015 Tasktop Technologies and others.
+ * Copyright (c) 2010, 2024 Tasktop Technologies and others.
  * 
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -8,6 +8,7 @@
  * SPDX-License-Identifier: EPL-2.0
  *
  *     Tasktop Technologies - initial API and implementation
+ *     ArSysOp - ongoing support
  *******************************************************************************/
 
 package org.eclipse.mylyn.internal.commons.notifications.feed;
@@ -47,7 +48,7 @@ public class ServiceMessage extends AbstractNotification {
 	}
 
 	@Override
-	public Object getAdapter(@SuppressWarnings("rawtypes") Class adapter) {
+	public <T> T getAdapter(Class<T> adapter) {
 		return Platform.getAdapterManager().getAdapter(this, adapter);
 	}
 
