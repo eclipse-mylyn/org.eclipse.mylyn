4af9946ddfb182524a97a18c92431717294553b1
George Lindholm <66577321+gnl42@users.noreply.github.com>
2024-02-04 23:40:02 -0800

    Mylyn Bugzilla Connector: Undo copyright line changes #414
    Task-Url: https://github.com/eclipse-mylyn/org.eclipse.mylyn/issues/414

--- mylyn.tasks/connectors/bugzilla/org.eclipse.mylyn.bugzilla.tests/src/org/eclipse/mylyn/bugzilla/tests/core/BugzillaRepositoryConnectorStandaloneTest.java
+++ mylyn.tasks/connectors/bugzilla/org.eclipse.mylyn.bugzilla.tests/src/org/eclipse/mylyn/bugzilla/tests/core/BugzillaRepositoryConnectorStandaloneTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright © 2004, 2013, 2024 Nathan Hapke and others.
+ * Copyright (c) 2004, 2013 Nathan Hapke and others.
  * 
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
--- mylyn.tasks/connectors/bugzilla/org.eclipse.mylyn.bugzilla.tests/src/org/eclipse/mylyn/bugzilla/tests/core/RepositoryConfigurationTest.java
+++ mylyn.tasks/connectors/bugzilla/org.eclipse.mylyn.bugzilla.tests/src/org/eclipse/mylyn/bugzilla/tests/core/RepositoryConfigurationTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright © 2013, 2016, 2024 Tasktop Technologies and others.
+ * Copyright (c) 2013, 2016 Tasktop Technologies and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
