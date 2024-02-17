c85746727ce55ff9291232871bf6facd52d7505f
Frank Becker <eclipse@frank-becker.de>
2024-02-17 08:17:37 +0100

    adjust header comments

--- mylyn.tasks/connectors/gitlab/org.eclipse.mylyn.gitlab.core.test/src/org/eclipse/mylyn/gitlab/core/test/GitlabTestFixture.java
+++ mylyn.tasks/connectors/gitlab/org.eclipse.mylyn.gitlab.core.test/src/org/eclipse/mylyn/gitlab/core/test/GitlabTestFixture.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright © 2024 frank
+ * Copyright (c) 2024 Frank Becker and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
  * https://www.eclipse.org/legal/epl-v20.html.
  *
  * Contributors:
- *     frank - initial API and implementation
+ *     Frank Becker - initial API and implementation
  *******************************************************************************/
 
 package org.eclipse.mylyn.gitlab.core.test;
