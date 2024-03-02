915798bb456d440df04469da1d005603de60213b
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2024-03-02 11:00:25 +0300

    [#423] Resolve "light" warnings for Mylyn Docs
    * resolve restriction warnings for
    `org.eclipse.mylyn.wikitext.mediawiki.ant.tests`

--- mylyn.docs/wikitext/core/org.eclipse.mylyn.wikitext.mediawiki.ant.tests/src/test/java/org/eclipse/mylyn/wikitext/mediawiki/ant/internal/tasks/WikiToDocTaskTest.java
+++ mylyn.docs/wikitext/core/org.eclipse.mylyn.wikitext.mediawiki.ant.tests/src/test/java/org/eclipse/mylyn/wikitext/mediawiki/ant/internal/tasks/WikiToDocTaskTest.java
@@ -32,7 +32,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
-@SuppressWarnings({ "nls", "restriction" })
+@SuppressWarnings("nls")
 public class WikiToDocTaskTest {
 
 	@Rule
