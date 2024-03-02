090ffdeeec6496564c957bde28e62ef599f88e53
Frank Becker <eclipse@frank-becker.de>
2024-03-02 08:21:20 +0100

    add missing @GitLabTestService

--- mylyn.tasks/connectors/gitlab/org.eclipse.mylyn.gitlab.core.test/src/org/eclipse/mylyn/gitlab/core/test/RestfulGitlabClientTest.java
+++ mylyn.tasks/connectors/gitlab/org.eclipse.mylyn.gitlab.core.test/src/org/eclipse/mylyn/gitlab/core/test/RestfulGitlabClientTest.java
@@ -144,7 +144,7 @@ class RestfulGitlabClientTest {
 		}
 		techUsers.sort(Comparator.naturalOrder());
 		String fName = "testdata/getNamespaces" + (techUsers.size() > 0 ? "_" : "") + String.join("_", techUsers)
-				+ ".json";
+		+ ".json";
 
 		String actual = new GsonBuilder().setPrettyPrinting().create().toJson(resultElement);
 		String expected = IOUtils.toString(CommonTestUtil.getResource(this, fName), Charset.defaultCharset());
@@ -215,6 +215,7 @@ class RestfulGitlabClientTest {
 	}
 
 	@Test
+	@GitLabTestService
 	void testgetConfiguration() throws Exception {
 		GitlabRestClient client = GitlabTestFixture.current().client();
 		GitlabConfiguration configuration = client.getConfiguration(client.getTaskRepository(),
