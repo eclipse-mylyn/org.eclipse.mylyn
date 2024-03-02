2b3627005e499e29894abc8a67c06fa4bd5af7f1
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2024-03-02 10:44:52 +0300

    [#423] Resolve "light" warnings for Mylyn Docs
    * resolve restriction warnings for `org.eclipse.mylyn.wikitext.toolkit`

--- mylyn.docs/wikitext/core/org.eclipse.mylyn.wikitext.toolkit/src/main/java/org/eclipse/mylyn/wikitext/toolkit/RecordingDocumentBuilder.java
+++ mylyn.docs/wikitext/core/org.eclipse.mylyn.wikitext.toolkit/src/main/java/org/eclipse/mylyn/wikitext/toolkit/RecordingDocumentBuilder.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2021 David Green and others.
+ * Copyright (c) 2007, 2024 David Green and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -9,6 +9,7 @@
  *
  * Contributors:
  *     David Green - initial API and implementation
+ *     ArSysOp - ongoing support
  *******************************************************************************/
 package org.eclipse.mylyn.wikitext.toolkit;
 
@@ -22,6 +23,7 @@ import org.eclipse.mylyn.wikitext.util.LocatorImpl;
  * @author David Green
  * @since 3.0
  */
+@SuppressWarnings("restriction")
 public class RecordingDocumentBuilder extends DocumentBuilder {
 
 	public static class Event {
