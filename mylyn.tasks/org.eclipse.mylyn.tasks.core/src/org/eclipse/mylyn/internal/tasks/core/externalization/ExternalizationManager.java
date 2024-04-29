8e4617aab2c7f45041c4296c786fc6c98e1eb4a6
George Lindholm <66577321+gnl42@users.noreply.github.com>
2024-04-29 00:37:13 -0700

    SubProgressMonitor #500
    Task-Url: https://github.com/eclipse-mylyn/org.eclipse.mylyn/issues/500

--- mylyn.tasks/org.eclipse.mylyn.tasks.core/src/org/eclipse/mylyn/internal/tasks/core/externalization/ExternalizationManager.java
+++ mylyn.tasks/org.eclipse.mylyn.tasks.core/src/org/eclipse/mylyn/internal/tasks/core/externalization/ExternalizationManager.java
@@ -1,14 +1,15 @@
 /*******************************************************************************
  * Copyright (c) 2004, 2010 Tasktop Technologies and others.
- * 
+ *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
  * https://www.eclipse.org/legal/epl-2.0
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
  *     Tasktop Technologies - initial API and implementation
+ *     See git history
  *******************************************************************************/
 
 package org.eclipse.mylyn.internal.tasks.core.externalization;
@@ -27,7 +28,7 @@ import org.eclipse.core.runtime.MultiStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.SafeRunner;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.SubProgressMonitor;
+import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.core.runtime.jobs.ISchedulingRule;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.mylyn.commons.core.StatusHandler;
@@ -224,7 +225,7 @@ public class ExternalizationManager {
 									monitor.setTaskName(MessageFormat.format(Messages.ExternalizationManager_Saving_X,
 											participant.getDescription()));
 									participant.execute(context,
-											new SubProgressMonitor(monitor, IProgressMonitor.UNKNOWN));
+											SubMonitor.convert(monitor, IProgressMonitor.UNKNOWN));
 								} catch (CoreException e) {
 									StatusHandler.log(new Status(IStatus.WARNING, ITasksCoreConstants.ID_PLUGIN,
 											"Save failed for " + participant.getDescription(), e)); //$NON-NLS-1$
