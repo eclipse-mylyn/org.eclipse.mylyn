be9796c72804ed214ca5f4212058db3f36ee2882
Michael Bangas <michael.bangas@vector.com>
2025-04-22 13:58:54 +0200

    add all svgs to org.eclipse.mylyn.tasks.ui and org.eclipse.mylyn.wikitext.ui
    This commit adds SVGs for all icons in the bundles `org.eclipse.mylyn.tasks.ui` and `org.eclipse.mylyn.wikitext.ui` except for the following as these are not available as SVG yet:
    
    org.eclipse.mylyn.wikitext.ui/org.eclipse.mylyn.internal.wikitext.ui.util.resources/missing-image.svg

--- mylyn.tasks/org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/tasks/ui/TasksUiImages.java
+++ mylyn.tasks/org.eclipse.mylyn.tasks.ui/src/org/eclipse/mylyn/tasks/ui/TasksUiImages.java
@@ -47,60 +47,60 @@ public class TasksUiImages {
 
 	// Tasks and Task List elements
 
-	public static final ImageDescriptor TASK = create(TOOL, "task.png"); //$NON-NLS-1$
+	public static final ImageDescriptor TASK = create(TOOL, "task.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor TASK_COMPLETE = create(TOOL, "task-complete.png"); //$NON-NLS-1$
+	public static final ImageDescriptor TASK_COMPLETE = create(TOOL, "task-complete.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor TASK_INCOMPLETE = create(TOOL, "task-incomplete.png"); //$NON-NLS-1$
+	public static final ImageDescriptor TASK_INCOMPLETE = create(TOOL, "task-incomplete.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor TASK_COMPLETED = create(TOOL, "task-completed.png"); //$NON-NLS-1$
+	public static final ImageDescriptor TASK_COMPLETED = create(TOOL, "task-completed.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor TASK_NOTES = create(TOOL, "task-notes.png"); //$NON-NLS-1$
+	public static final ImageDescriptor TASK_NOTES = create(TOOL, "task-notes.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor TASK_NEW = create(TOOL, "task-new.png"); //$NON-NLS-1$
+	public static final ImageDescriptor TASK_NEW = create(TOOL, "task-new.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor TASK_NEW_SUB = create(TOOL, "sub-task-new.png"); //$NON-NLS-1$
+	public static final ImageDescriptor TASK_NEW_SUB = create(TOOL, "sub-task-new.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor TASK_REPOSITORY_HISTORY = create(TOOL, "task-repository-history.png"); //$NON-NLS-1$
+	public static final ImageDescriptor TASK_REPOSITORY_HISTORY = create(TOOL, "task-repository-history.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor TASK_REMOTE = create(TOOL, "task-remote.png"); //$NON-NLS-1$
+	public static final ImageDescriptor TASK_REMOTE = create(TOOL, "task-remote.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor TASK_WORKING_SET = create(TOOL, "open-task.png"); //$NON-NLS-1$
+	public static final ImageDescriptor TASK_WORKING_SET = create(TOOL, "open-task.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor TASKS_VIEW = create(VIEW, "task-list.png"); //$NON-NLS-1$
+	public static final ImageDescriptor TASKS_VIEW = create(VIEW, "task-list.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor TASK_ATTACHMENT_PATCH = create(OBJ, "attachment-patch.png"); //$NON-NLS-1$
+	public static final ImageDescriptor TASK_ATTACHMENT_PATCH = create(OBJ, "attachment-patch.svg"); //$NON-NLS-1$
 
 	/**
 	 * @since 3.21
 	 */
-	public static final ImageDescriptor TASK_OWNED = create(TOOL, "task-owned.png"); //$NON-NLS-1$
+	public static final ImageDescriptor TASK_OWNED = create(TOOL, "task-owned.svg"); //$NON-NLS-1$
 
 	/**
 	 * @since 3.7
 	 */
-	public static final ImageDescriptor FILTER_OBSOLETE_SMALL = create(TOOL_SMALL, "file-delete-line-12x12.png"); //$NON-NLS-1$
+	public static final ImageDescriptor FILTER_OBSOLETE_SMALL = create(TOOL_SMALL, "file-delete-line-12x12.svg"); //$NON-NLS-1$
 
 	/**
 	 * @since 3.7
 	 */
-	public static final ImageDescriptor FILE_NEW_SMALL = create(TOOL_SMALL, "file-new-12x12.png"); //$NON-NLS-1$
+	public static final ImageDescriptor FILE_NEW_SMALL = create(TOOL_SMALL, "file-new-12x12.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor TASK_RETRIEVE = create(TOOL, "task-retrieve.png"); //$NON-NLS-1$
+	public static final ImageDescriptor TASK_RETRIEVE = create(TOOL, "task-retrieve.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor TASK_REPOSITORY = create(TOOL, "task-repository.png"); //$NON-NLS-1$
+	public static final ImageDescriptor TASK_REPOSITORY = create(TOOL, "task-repository.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor TASK_REPOSITORY_NEW = create(TOOL, "task-repository-new.png"); //$NON-NLS-1$
+	public static final ImageDescriptor TASK_REPOSITORY_NEW = create(TOOL, "task-repository-new.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor CATEGORY = create(TOOL, "category.png"); //$NON-NLS-1$
+	public static final ImageDescriptor CATEGORY = create(TOOL, "category.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor CATEGORY_NEW = create(TOOL, "category-new.png"); //$NON-NLS-1$
+	public static final ImageDescriptor CATEGORY_NEW = create(TOOL, "category-new.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor CATEGORY_UNCATEGORIZED = create(TOOL, "category-archive.png"); //$NON-NLS-1$
+	public static final ImageDescriptor CATEGORY_UNCATEGORIZED = create(TOOL, "category-archive.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor QUERY = create(TOOL, "query.png"); //$NON-NLS-1$
+	public static final ImageDescriptor QUERY = create(TOOL, "query.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor QUERY_NEW = create(TOOL, "query-new.png"); //$NON-NLS-1$
+	public static final ImageDescriptor QUERY_NEW = create(TOOL, "query-new.svg"); //$NON-NLS-1$
 
 	/**
 	 * @since 3.2
@@ -108,75 +108,75 @@ public class TasksUiImages {
 	public static final ImageDescriptor QUERY_OFFLINE = ImageDescriptor.createWithFlags(TasksUiImages.QUERY,
 			SWT.IMAGE_GRAY);
 
-	public static final ImageDescriptor QUERY_UNMATCHED = create(TOOL, "query-unmatched.png"); //$NON-NLS-1$
+	public static final ImageDescriptor QUERY_UNMATCHED = create(TOOL, "query-unmatched.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor REPOSITORY = create(VIEW, "repositories.png"); //$NON-NLS-1$
+	public static final ImageDescriptor REPOSITORY = create(VIEW, "repositories.svg"); //$NON-NLS-1$
 
 	public static final ImageDescriptor REPOSITORY_OFFLINE = ImageDescriptor.createWithFlags(TasksUiImages.REPOSITORY,
 			SWT.IMAGE_GRAY);
 
 	public static final ImageDescriptor REPOSITORY_SYNCHRONIZE_SMALL = create(TOOL_SMALL,
-			"repository-synchronize-small.png"); //$NON-NLS-1$
+			"repository-synchronize-small.svg"); //$NON-NLS-1$
 
 	/**
 	 * @since 3.2
 	 */
 	public static final ImageDescriptor REPOSITORY_UPDATE_CONFIGURATION = create(TOOL,
-			"repository-synchronize-attributes.png"); //$NON-NLS-1$
+			"repository-synchronize-attributes.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor REPOSITORY_SYNCHRONIZE = create(TOOL, "repository-synchronize.png"); //$NON-NLS-1$
+	public static final ImageDescriptor REPOSITORY_SYNCHRONIZE = create(TOOL, "repository-synchronize.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor REPOSITORY_SUBMIT = create(TOOL, "repository-submit.png"); //$NON-NLS-1$
+	public static final ImageDescriptor REPOSITORY_SUBMIT = create(TOOL, "repository-submit.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor REPOSITORY_SMALL = create(OBJ, "repository-small.png"); //$NON-NLS-1$
+	public static final ImageDescriptor REPOSITORY_SMALL = create(OBJ, "repository-small.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor REPOSITORY_NEW = create(TOOL, "repository-new.png"); //$NON-NLS-1$
+	public static final ImageDescriptor REPOSITORY_NEW = create(TOOL, "repository-new.svg"); //$NON-NLS-1$
 
 	/**
 	 * @since 3.1
 	 */
-	public static final ImageDescriptor REPOSITORY_VALIDATE = create(OBJ, "resource_obj.png"); //$NON-NLS-1$
+	public static final ImageDescriptor REPOSITORY_VALIDATE = create(OBJ, "resource_obj.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor REPOSITORIES_VIEW = create(VIEW, "repositories.png"); //$NON-NLS-1$
+	public static final ImageDescriptor REPOSITORIES_VIEW = create(VIEW, "repositories.svg"); //$NON-NLS-1$
 
 	// Context and activation
 
-	public static final ImageDescriptor CONTEXT_ACTIVE = create(TOOL, "task-active.png"); //$NON-NLS-1$
+	public static final ImageDescriptor CONTEXT_ACTIVE = create(TOOL, "task-active.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor CONTEXT_ACTIVE_CENTERED = create(TOOL, "task-active-centered.png"); //$NON-NLS-1$
+	public static final ImageDescriptor CONTEXT_ACTIVE_CENTERED = create(TOOL, "task-active-centered.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor CONTEXT_INACTIVE_EMPTY = create(TOOL, "task-inactive.png"); //$NON-NLS-1$
+	public static final ImageDescriptor CONTEXT_INACTIVE_EMPTY = create(TOOL, "task-inactive.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor CONTEXT_INACTIVE = create(TOOL, "task-context.png"); //$NON-NLS-1$
+	public static final ImageDescriptor CONTEXT_INACTIVE = create(TOOL, "task-context.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor CONTEXT_FOCUS = create(VIEW, "focus.png"); //$NON-NLS-1$
+	public static final ImageDescriptor CONTEXT_FOCUS = create(VIEW, "focus.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor CONTEXT_ATTACH = create(TOOL, "context-attach.png"); //$NON-NLS-1$
+	public static final ImageDescriptor CONTEXT_ATTACH = create(TOOL, "context-attach.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor CONTEXT_RETRIEVE = create(TOOL, "context-retrieve.png"); //$NON-NLS-1$
+	public static final ImageDescriptor CONTEXT_RETRIEVE = create(TOOL, "context-retrieve.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor CONTEXT_TRANSFER = create(TOOL, "context-transfer.png"); //$NON-NLS-1$
+	public static final ImageDescriptor CONTEXT_TRANSFER = create(TOOL, "context-transfer.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor CONTEXT_CLEAR = create(TOOL, "context-clear.png"); //$NON-NLS-1$
+	public static final ImageDescriptor CONTEXT_CLEAR = create(TOOL, "context-clear.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor CONTEXT_HISTORY_PREVIOUS = create(TOOL, "navigate-previous.png"); //$NON-NLS-1$
+	public static final ImageDescriptor CONTEXT_HISTORY_PREVIOUS = create(TOOL, "navigate-previous.svg"); //$NON-NLS-1$
 
 	@Deprecated
-	public static final ImageDescriptor CONTEXT_HISTORY_PREVIOUS_PAUSE = create(TOOL, "navigate-previous-pause.png"); //$NON-NLS-1$
+	public static final ImageDescriptor CONTEXT_HISTORY_PREVIOUS_PAUSE = create(TOOL, "navigate-previous-pause.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor CONTEXT_HISTORY_PREVIOUS_ACTIVE = create(TOOL, "navigate-previous-active.png"); //$NON-NLS-1$
+	public static final ImageDescriptor CONTEXT_HISTORY_PREVIOUS_ACTIVE = create(TOOL, "navigate-previous-active.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor CONTEXT_HISTORY_NEXT = create(TOOL, "navigate-next.png"); //$NON-NLS-1$
+	public static final ImageDescriptor CONTEXT_HISTORY_NEXT = create(TOOL, "navigate-next.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor CONTEXT_CAPTURE_PAUSE = create(TOOL, "capture-pause.png"); //$NON-NLS-1$
+	public static final ImageDescriptor CONTEXT_CAPTURE_PAUSE = create(TOOL, "capture-pause.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor CONTEXT_ADD = create(TOOL, "context-add.png"); //$NON-NLS-1$
+	public static final ImageDescriptor CONTEXT_ADD = create(TOOL, "context-add.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor CONTEXT_COPY = create(TOOL, "context-transfer.png"); //$NON-NLS-1$
+	public static final ImageDescriptor CONTEXT_COPY = create(TOOL, "context-transfer.svg"); //$NON-NLS-1$
 
 	// Comments and collaboration
 
-	public static final ImageDescriptor COMMENT = create(TOOL, "comment.png"); //$NON-NLS-1$
+	public static final ImageDescriptor COMMENT = create(TOOL, "comment.svg"); //$NON-NLS-1$
 
 	public static final ImageDescriptor COMMENT_SORT_DOWN = create(TOOL, "sort-down.gif"); //$NON-NLS-1$
 
@@ -186,58 +186,58 @@ public class TasksUiImages {
 
 	public static final ImageDescriptor COMMENT_SORT_UP_GRAY = create(TOOL, "sort-up-gray.gif"); //$NON-NLS-1$
 
-	public static final ImageDescriptor COMMENT_REPLY = create(TOOL, "reply.png"); //$NON-NLS-1$
+	public static final ImageDescriptor COMMENT_REPLY = create(TOOL, "reply.svg"); //$NON-NLS-1$
 
 	/**
 	 * @since 3.1
 	 */
-	public static final ImageDescriptor COMMENT_REPLY_SMALL = create(TOOL_SMALL, "reply.png"); //$NON-NLS-1$
+	public static final ImageDescriptor COMMENT_REPLY_SMALL = create(TOOL_SMALL, "reply.svg"); //$NON-NLS-1$
 
 	// Wizard banners
 
-	public static final ImageDescriptor BANNER_REPOSITORY = create(WIZBAN, "banner-repository.gif"); //$NON-NLS-1$
+	public static final ImageDescriptor BANNER_REPOSITORY = create(WIZBAN, "banner-repository.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor BANNER_REPOSITORY_SETTINGS = create(WIZBAN, "banner-repository-settings.gif"); //$NON-NLS-1$
+	public static final ImageDescriptor BANNER_REPOSITORY_SETTINGS = create(WIZBAN, "banner-repository-settings.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor BANNER_REPOSITORY_CONTEXT = create(WIZBAN, "banner-repository-context.gif"); //$NON-NLS-1$
+	public static final ImageDescriptor BANNER_REPOSITORY_CONTEXT = create(WIZBAN, "banner-repository-context.svg"); //$NON-NLS-1$
 
-	public static final ImageDescriptor BANNER_WORKING_SET = create(WIZBAN, "workset_wiz.png"); //$NON-NLS-1$
-
-	/**
-	 * @since 3.2
-	 */
-	public static final ImageDescriptor PRESENTATION_CATEGORIZED = create(TOOL, "presentation-categorized.png"); //$NON-NLS-1$
+	public static final ImageDescriptor BANNER_WORKING_SET = create(WIZBAN, "workset_wiz.svg"); //$NON-NLS-1$
 
 	/**
 	 * @since 3.2
 	 */
-	public static final ImageDescriptor PRESENTATION_SCHEDULED = create(TOOL, "presentation-scheduled.png"); //$NON-NLS-1$
+	public static final ImageDescriptor PRESENTATION_CATEGORIZED = create(TOOL, "presentation-categorized.svg"); //$NON-NLS-1$
 
 	/**
 	 * @since 3.2
 	 */
-	public static final ImageDescriptor BANNER_REPORT_BUG = create(WIZBAN, "bug-wizard.gif"); //$NON-NLS-1$
+	public static final ImageDescriptor PRESENTATION_SCHEDULED = create(TOOL, "presentation-scheduled.svg"); //$NON-NLS-1$
+
+	/**
+	 * @since 3.2
+	 */
+	public static final ImageDescriptor BANNER_REPORT_BUG = create(WIZBAN, "bug-wizard.svg"); //$NON-NLS-1$
 
 	/**
 	 * @since 3.7
 	 */
 
-	public static final ImageDescriptor LOCK_CLOSE = create(TOOL_SMALL, "lock.png"); //$NON-NLS-1$
+	public static final ImageDescriptor LOCK_CLOSE = create(TOOL_SMALL, "lock.svg"); //$NON-NLS-1$
 
 	/**
 	 * @since 3.7
 	 */
-	public static final ImageDescriptor LOCK_OPEN = create(TOOL_SMALL, "unlock.png"); //$NON-NLS-1$
+	public static final ImageDescriptor LOCK_OPEN = create(TOOL_SMALL, "unlock.svg"); //$NON-NLS-1$
 
 	/**
 	 * @since 3.8
 	 */
-	public static final ImageDescriptor IMAGE_CAPTURE_SMALL = create(TOOL_SMALL, "image_capture.png"); //$NON-NLS-1$
+	public static final ImageDescriptor IMAGE_CAPTURE_SMALL = create(TOOL_SMALL, "image_capture.svg"); //$NON-NLS-1$
 
 	/**
 	 * @since 3.10
 	 */
-	public static final ImageDescriptor NOTES = create(OVERLAY, "overlay-notes.png"); //$NON-NLS-1$
+	public static final ImageDescriptor NOTES = create(OVERLAY, "overlay-notes.svg"); //$NON-NLS-1$
 
 	public static Image getImageForPriority(PriorityLevel priorityLevel) {
 		if (priorityLevel == null) {
