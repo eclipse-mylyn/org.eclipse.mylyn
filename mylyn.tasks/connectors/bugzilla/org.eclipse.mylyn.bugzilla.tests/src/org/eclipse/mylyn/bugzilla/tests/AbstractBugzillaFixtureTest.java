acc8510bb1a0f4a79ed6477f3e85b77f9384c900
George Lindholm <66577321+gnl42@users.noreply.github.com>
2025-09-28 02:07:04 -0700

    51: Migrate to JUnit 5
    Task-Url: https://github.com/eclipse-mylyn/org.eclipse.mylyn/issues/51

--- mylyn.tasks/connectors/bugzilla/org.eclipse.mylyn.bugzilla.tests/src/org/eclipse/mylyn/bugzilla/tests/AbstractBugzillaFixtureTest.java
+++ mylyn.tasks/connectors/bugzilla/org.eclipse.mylyn.bugzilla.tests/src/org/eclipse/mylyn/bugzilla/tests/AbstractBugzillaFixtureTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2025 george
+ * Copyright (c) 2025 George Lindholm
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
